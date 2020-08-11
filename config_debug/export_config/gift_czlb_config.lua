83e1955920b855fda1e02ca8558b961738b223f0
duchao <duchao@qq.com>
2020-08-11 14:02:26 +0800

    增加季卡月卡

--- config_debug/export_config/gift_czlb_config.lua
+++ config_debug/export_config/gift_czlb_config.lua
@@ -7,6 +7,9 @@ return {
 			id = 1,
 			title = "每日6元",
 			gift_ids = {10257,10258,10259,},
+			pay_name = {"60万","1","最高10万","60万","2","最高12万","60万","3","最高15万",},
+			icon_img = {"ty_icon_jb_6y","3dby_btn_sd","fkfl_btn_bx","ty_icon_jb_15y","3dby_btn_sd","fkfl_btn_bx2","ty_icon_jb_30y","3dby_btn_sd","fkfl_btn_bx3",},
+			tips = {"60万金币","锁定*1","最高10万金币","60万金币","锁定*2","最高12万金币","60万金币","锁定*3","最高15万金币",},
 		},
 		[2]=
 		{
@@ -14,6 +17,9 @@ return {
 			id = 2,
 			title = "每日48元",
 			gift_ids = {10260,10261,10262,},
+			pay_name = {"480万","2","最高80万","480万","3","最高96万","480万","4","最高120万",},
+			icon_img = {"ty_icon_jb_48y","3dby_btn_sd","fkfl_btn_bx","ty_icon_jb_98y","3dby_btn_sd","fkfl_btn_bx2","ty_icon_jb_198y","3dby_btn_sd","fkfl_btn_bx3",},
+			tips = {"480万金币","锁定*2","最高80万金币","480万金币","锁定*3","最高96万金币","480万金币","锁定*4","最高120万金币",},
 		},
 		[3]=
 		{
@@ -21,6 +27,9 @@ return {
 			id = 3,
 			title = "每日98元",
 			gift_ids = {10263,10264,10265,},
+			pay_name = {"980万","3","最高170万","980万","4","最高200万","980万","5","最高248万",},
+			icon_img = {"ty_icon_jb_98y","3dby_btn_sd","fkfl_btn_bx","ty_icon_jb_198y","3dby_btn_sd","fkfl_btn_bx2","ty_icon_jb_498y","3dby_btn_sd","fkfl_btn_bx3",},
+			tips = {"980万金币","锁定*3","最高170万金币","980万金币","锁定*4","最高200万金币","980万金币","锁定*5","最高248万金币",},
 		},
 		[4]=
 		{
@@ -28,6 +37,9 @@ return {
 			id = 4,
 			title = "每日198元",
 			gift_ids = {10266,10267,10268,},
+			pay_name = {"1980万","4","最高330万","1980万","5","最高400万","1980万","6","最高500万",},
+			icon_img = {"ty_icon_jb_498y","3dby_btn_sd","fkfl_btn_bx","ty_icon_jb_998y","3dby_btn_sd","fkfl_btn_bx2","ty_icon_jb_2498y","3dby_btn_sd","fkfl_btn_bx3",},
+			tips = {"1980万金币","锁定*4","最高330万金币","1980万金币","锁定*5","最高400万金币","1980万金币","锁定*6","最高500万金币",},
 		},
 	},
 	help_info=
