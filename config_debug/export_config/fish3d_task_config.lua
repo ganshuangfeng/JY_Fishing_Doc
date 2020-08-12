1319b0f19780aac77e78dc10a2664a5389bfd8b5
denglu <denglu@qq.com>
2020-08-12 15:42:23 +0800

    挑战任务修改

---
+++ config_debug/export_config/fish3d_task_config.lua
@@ -0,0 +1,329 @@
+return {
+	Sheet1=
+	{
+		[1]=
+		{
+			line = 1,
+			id = 20300,
+			fish_id = 15,
+			name = "渔场2",
+			desc = "使用任意炮捕获",
+			sort = 1,
+		},
+		[2]=
+		{
+			line = 2,
+			id = 20301,
+			fish_id = 16,
+			name = "渔场2",
+			desc = "使用任意炮捕获",
+			sort = 2,
+		},
+		[3]=
+		{
+			line = 3,
+			id = 20302,
+			fish_id = 17,
+			name = "渔场2",
+			desc = "使用任意炮捕获",
+			sort = 3,
+		},
+		[4]=
+		{
+			line = 4,
+			id = 20303,
+			fish_id = 18,
+			name = "渔场2",
+			desc = "使用任意炮捕获",
+			sort = 4,
+		},
+		[5]=
+		{
+			line = 5,
+			id = 20304,
+			fish_id = 20,
+			name = "渔场2",
+			desc = "使用任意炮捕获",
+			sort = 5,
+		},
+		[6]=
+		{
+			line = 6,
+			id = 20305,
+			fish_id = 22,
+			name = "渔场2",
+			desc = "使用任意炮捕获",
+			sort = 6,
+		},
+		[7]=
+		{
+			line = 7,
+			id = 20306,
+			fish_id = 23,
+			name = "渔场2",
+			desc = "使用任意炮捕获",
+			sort = 7,
+		},
+		[8]=
+		{
+			line = 8,
+			id = 20307,
+			fish_id = 26,
+			name = "渔场2",
+			desc = "使用任意炮捕获",
+			sort = 8,
+		},
+		[9]=
+		{
+			line = 9,
+			id = 20308,
+			fish_id = 27,
+			name = "渔场2",
+			desc = "使用任意炮捕获",
+			sort = 9,
+		},
+		[10]=
+		{
+			line = 10,
+			id = 20400,
+			fish_id = 15,
+			name = "渔场3",
+			desc = "使用任意炮捕获",
+			sort = 10,
+		},
+		[11]=
+		{
+			line = 11,
+			id = 20401,
+			fish_id = 16,
+			name = "渔场3",
+			desc = "使用任意炮捕获",
+			sort = 11,
+		},
+		[12]=
+		{
+			line = 12,
+			id = 20402,
+			fish_id = 17,
+			name = "渔场3",
+			desc = "使用任意炮捕获",
+			sort = 12,
+		},
+		[13]=
+		{
+			line = 13,
+			id = 20403,
+			fish_id = 18,
+			name = "渔场3",
+			desc = "使用任意炮捕获",
+			sort = 13,
+		},
+		[14]=
+		{
+			line = 14,
+			id = 20404,
+			fish_id = 20,
+			name = "渔场3",
+			desc = "使用任意炮捕获",
+			sort = 14,
+		},
+		[15]=
+		{
+			line = 15,
+			id = 20405,
+			fish_id = 22,
+			name = "渔场3",
+			desc = "使用任意炮捕获",
+			sort = 15,
+		},
+		[16]=
+		{
+			line = 16,
+			id = 20406,
+			fish_id = 23,
+			name = "渔场3",
+			desc = "使用任意炮捕获",
+			sort = 16,
+		},
+		[17]=
+		{
+			line = 17,
+			id = 20407,
+			fish_id = 26,
+			name = "渔场3",
+			desc = "使用任意炮捕获",
+			sort = 17,
+		},
+		[18]=
+		{
+			line = 18,
+			id = 20408,
+			fish_id = 27,
+			name = "渔场3",
+			desc = "使用任意炮捕获",
+			sort = 18,
+		},
+		[19]=
+		{
+			line = 19,
+			id = 20500,
+			fish_id = 15,
+			name = "渔场4",
+			desc = "使用任意炮捕获",
+			sort = 19,
+		},
+		[20]=
+		{
+			line = 20,
+			id = 20501,
+			fish_id = 16,
+			name = "渔场4",
+			desc = "使用任意炮捕获",
+			sort = 20,
+		},
+		[21]=
+		{
+			line = 21,
+			id = 20502,
+			fish_id = 17,
+			name = "渔场4",
+			desc = "使用任意炮捕获",
+			sort = 21,
+		},
+		[22]=
+		{
+			line = 22,
+			id = 20503,
+			fish_id = 18,
+			name = "渔场4",
+			desc = "使用任意炮捕获",
+			sort = 22,
+		},
+		[23]=
+		{
+			line = 23,
+			id = 20504,
+			fish_id = 20,
+			name = "渔场4",
+			desc = "使用任意炮捕获",
+			sort = 23,
+		},
+		[24]=
+		{
+			line = 24,
+			id = 20505,
+			fish_id = 22,
+			name = "渔场4",
+			desc = "使用任意炮捕获",
+			sort = 24,
+		},
+		[25]=
+		{
+			line = 25,
+			id = 20506,
+			fish_id = 23,
+			name = "渔场4",
+			desc = "使用任意炮捕获",
+			sort = 25,
+		},
+		[26]=
+		{
+			line = 26,
+			id = 20507,
+			fish_id = 26,
+			name = "渔场4",
+			desc = "使用任意炮捕获",
+			sort = 26,
+		},
+		[27]=
+		{
+			line = 27,
+			id = 20508,
+			fish_id = 27,
+			name = "渔场4",
+			desc = "使用任意炮捕获",
+			sort = 27,
+		},
+		[28]=
+		{
+			line = 28,
+			id = 20600,
+			fish_id = 15,
+			name = "渔场5",
+			desc = "使用任意炮捕获",
+			sort = 28,
+		},
+		[29]=
+		{
+			line = 29,
+			id = 20601,
+			fish_id = 16,
+			name = "渔场5",
+			desc = "使用任意炮捕获",
+			sort = 29,
+		},
+		[30]=
+		{
+			line = 30,
+			id = 20602,
+			fish_id = 17,
+			name = "渔场5",
+			desc = "使用任意炮捕获",
+			sort = 30,
+		},
+		[31]=
+		{
+			line = 31,
+			id = 20603,
+			fish_id = 18,
+			name = "渔场5",
+			desc = "使用任意炮捕获",
+			sort = 31,
+		},
+		[32]=
+		{
+			line = 32,
+			id = 20604,
+			fish_id = 20,
+			name = "渔场5",
+			desc = "使用任意炮捕获",
+			sort = 32,
+		},
+		[33]=
+		{
+			line = 33,
+			id = 20605,
+			fish_id = 22,
+			name = "渔场5",
+			desc = "使用任意炮捕获",
+			sort = 33,
+		},
+		[34]=
+		{
+			line = 34,
+			id = 20606,
+			fish_id = 23,
+			name = "渔场5",
+			desc = "使用任意炮捕获",
+			sort = 34,
+		},
+		[35]=
+		{
+			line = 35,
+			id = 20607,
+			fish_id = 26,
+			name = "渔场5",
+			desc = "使用任意炮捕获",
+			sort = 35,
+		},
+		[36]=
+		{
+			line = 36,
+			id = 20608,
+			fish_id = 27,
+			name = "渔场5",
+			desc = "使用任意炮捕获",
+			sort = 36,
+		},
+	},
+}
