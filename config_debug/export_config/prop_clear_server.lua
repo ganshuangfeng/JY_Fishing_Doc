d28c4b9009c319fda546abb9596accebadf5d9bb
denglu <denglu@qq.com>
2020-09-01 11:06:38 +0800

    道具清理配置

---
+++ config_debug/export_config/prop_clear_server.lua
@@ -0,0 +1,57 @@
+return {
+	player_prop=
+	{
+		[1]=
+		{
+			id = 1,
+			prop_type = "prop_sunshine",
+			start_time = 1591054200,
+			recycle_time = 1591635600,
+		},
+		[2]=
+		{
+			id = 2,
+			prop_type = "prop_carnation",
+			start_time = 1588633200,
+			recycle_time = 1589216400,
+		},
+		[3]=
+		{
+			id = 3,
+			prop_type = "prop_shovel",
+			start_time = 1588028400,
+			recycle_time = 1588611600,
+		},
+		[4]=
+		{
+			id = 4,
+			prop_type = "prop_love",
+			start_time = 1589844600,
+			recycle_time = 1590422400,
+		},
+		[5]=
+		{
+			id = 5,
+			prop_type = "prop_fish_drop_act_0",
+			start_time = 1598310000,
+			recycle_time = 1598889599,
+		},
+		[6]=
+		{
+			id = 6,
+			prop_type = "prop_mfcjq",
+			start_time = 1599523200,
+			recycle_time = 1600099199,
+		},
+		[7]=
+		{
+			id = 7,
+			prop_type = "prop_xycd_sunshine",
+			start_time = 1599523200,
+			recycle_time = 1600099199,
+		},
+	},
+	player_asset=
+	{
+	},
+}
