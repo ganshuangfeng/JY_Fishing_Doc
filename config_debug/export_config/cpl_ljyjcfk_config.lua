79faa1ba33bf4d66ae1a44e27010d0770678149d
denglu <denglu@qq.com>
2020-09-10 13:55:58 +0800

    VIVO渠道调整

--- config_debug/export_config/cpl_ljyjcfk_config.lua
+++ config_debug/export_config/cpl_ljyjcfk_config.lua
@@ -10,6 +10,7 @@ return {
 			show_hb = 10,
 			hb = {5,8,10},
 			hb_image = "ty_icon_flq1",
+			key = "actp_own_task_p_xyxyj_nor",
 		},
 		[2]=
 		{
@@ -20,6 +21,7 @@ return {
 			show_hb = 50,
 			hb = {30,40,50},
 			hb_image = "ty_icon_flq1",
+			key = "actp_own_task_p_xyxyj_nor",
 		},
 		[3]=
 		{
@@ -30,6 +32,7 @@ return {
 			show_hb = 100,
 			hb = {50,80,100},
 			hb_image = "ty_icon_flq2",
+			key = "actp_own_task_p_xyxyj_nor",
 		},
 		[4]=
 		{
@@ -40,6 +43,7 @@ return {
 			show_hb = 200,
 			hb = {150,180,200},
 			hb_image = "ty_icon_flq2",
+			key = "actp_own_task_p_xyxyj_nor",
 		},
 		[5]=
 		{
@@ -50,6 +54,7 @@ return {
 			show_hb = 500,
 			hb = {300,400,500},
 			hb_image = "ty_icon_flq3",
+			key = "actp_own_task_p_xyxyj_nor",
 		},
 		[6]=
 		{
@@ -60,6 +65,7 @@ return {
 			show_hb = 1000,
 			hb = {800,900,1000},
 			hb_image = "ty_icon_flq3",
+			key = "actp_own_task_p_xyxyj_nor",
 		},
 		[7]=
 		{
@@ -70,6 +76,7 @@ return {
 			show_hb = 5000,
 			hb = {4500,4800,5000},
 			hb_image = "ty_icon_flq4",
+			key = "actp_own_task_p_xyxyj_nor",
 		},
 		[8]=
 		{
@@ -80,6 +87,7 @@ return {
 			show_hb = 10000,
 			hb = {8000,9000,10000},
 			hb_image = "ty_icon_flq4",
+			key = "actp_own_task_p_xyxyj_nor",
 		},
 		[9]=
 		{
@@ -90,6 +98,106 @@ return {
 			show_hb = 20000,
 			hb = {15000,18000,20000},
 			hb_image = "ty_icon_flq5",
+			key = "actp_own_task_p_xyxyj_nor",
+		},
+		[10]=
+		{
+			id = 10,
+			task = 1000057,
+			total = 1000000,
+			task_name = "小游戏累计赢金100万",
+			show_hb = 10,
+			hb = {"5000金币","8福利券","10福利券"},
+			hb_image = "ty_icon_flq1",
+			key = "actp_own_task_p_xyxyj_cps",
+		},
+		[11]=
+		{
+			id = 11,
+			task = 1000057,
+			total = 5000000,
+			task_name = "小游戏累计赢金500万",
+			show_hb = 50,
+			hb = {"3万金币","40福利券","50福利券"},
+			hb_image = "ty_icon_flq1",
+			key = "actp_own_task_p_xyxyj_cps",
+		},
+		[12]=
+		{
+			id = 12,
+			task = 1000057,
+			total = 10000000,
+			task_name = "小游戏累计赢金1000万",
+			show_hb = 100,
+			hb = {"5万金币","80福利券","100福利券"},
+			hb_image = "ty_icon_flq2",
+			key = "actp_own_task_p_xyxyj_cps",
+		},
+		[13]=
+		{
+			id = 13,
+			task = 1000057,
+			total = 20000000,
+			task_name = "小游戏累计赢金2000万",
+			show_hb = 200,
+			hb = {"15万金币","180福利券","200福利券"},
+			hb_image = "ty_icon_flq2",
+			key = "actp_own_task_p_xyxyj_cps",
+		},
+		[14]=
+		{
+			id = 14,
+			task = 1000057,
+			total = 50000000,
+			task_name = "小游戏累计赢金5000万",
+			show_hb = 500,
+			hb = {"30万金币","400福利券","500福利券"},
+			hb_image = "ty_icon_flq3",
+			key = "actp_own_task_p_xyxyj_cps",
+		},
+		[15]=
+		{
+			id = 15,
+			task = 1000057,
+			total = 100000000,
+			task_name = "小游戏累计赢金1亿",
+			show_hb = 1000,
+			hb = {"80万金币","900福利券","1000福利券"},
+			hb_image = "ty_icon_flq3",
+			key = "actp_own_task_p_xyxyj_cps",
+		},
+		[16]=
+		{
+			id = 16,
+			task = 1000057,
+			total = 500000000,
+			task_name = "小游戏累计赢金5亿",
+			show_hb = 5000,
+			hb = {"450万金币","4800福利券","5000福利券"},
+			hb_image = "ty_icon_flq4",
+			key = "actp_own_task_p_xyxyj_cps",
+		},
+		[17]=
+		{
+			id = 17,
+			task = 1000057,
+			total = 1000000000,
+			task_name = "小游戏累计赢金10亿",
+			show_hb = 10000,
+			hb = {"800万金币","9000福利券","1万福利券"},
+			hb_image = "ty_icon_flq4",
+			key = "actp_own_task_p_xyxyj_cps",
+		},
+		[18]=
+		{
+			id = 18,
+			task = 1000057,
+			total = 2000000000,
+			task_name = "小游戏累计赢金20亿",
+			show_hb = 20000,
+			hb = {"1500万金币","1.8万福利券","2万福利券"},
+			hb_image = "ty_icon_flq5",
+			key = "actp_own_task_p_xyxyj_cps",
 		},
 	},
 	helpinfo=
