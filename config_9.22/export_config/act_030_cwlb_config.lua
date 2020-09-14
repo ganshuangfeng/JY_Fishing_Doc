bef5702c807cbbbc7ff769890e8c8d3b8f2b1922
duchao <duchao@qq.com>
2020-09-14 15:48:57 +0800

    配置增加

---
+++ config_9.22/export_config/act_030_cwlb_config.lua
@@ -0,0 +1,110 @@
+return {
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			icon = "ty_icon_flq2",
+			name = "12000金币",
+			asset_type = "jing_bi",
+			value = 12000,
+		},
+		[2]=
+		{
+			line = 2,
+			icon = "ty_icon_flq2",
+			name = "15福利券",
+			asset_type = "shop_gold_sum",
+			value = 15,
+		},
+		[3]=
+		{
+			line = 3,
+			icon = "ty_icon_flq2",
+			name = "20000金币",
+			asset_type = "jing_bi",
+			value = 20000,
+		},
+		[4]=
+		{
+			line = 4,
+			icon = "ty_icon_flq2",
+			name = "30福利券",
+			asset_type = "shop_gold_sum",
+			value = 30,
+		},
+		[5]=
+		{
+			line = 5,
+			icon = "ty_icon_flq2",
+			name = "50000金币",
+			asset_type = "jing_bi",
+			value = 50000,
+		},
+		[6]=
+		{
+			line = 6,
+			icon = "ty_icon_flq2",
+			name = "100元话费",
+			asset_type = "prop_web_chip_huafei",
+			value = 10000,
+		},
+	},
+	gift=
+	{
+		[1]=
+		{
+			line = 1,
+			gift_id = 10311,
+			name = "6元礼包",
+			order = 1,
+			pay_icon = {"ty_icon_jb_30y","ty_icon_yxb_3","com_award_icon_dhq",},
+			pay_name = {"60万","3000","抽奖券*1",},
+		},
+		[2]=
+		{
+			line = 2,
+			gift_id = 10312,
+			name = "20元礼包",
+			order = 2,
+			pay_icon = {"ty_icon_jb_48y","ty_icon_yxb_2","com_award_icon_dhq",},
+			pay_name = {"200万","1万","抽奖券*4",},
+		},
+		[3]=
+		{
+			line = 3,
+			gift_id = 10313,
+			name = "48元礼包",
+			order = 3,
+			pay_icon = {"ty_icon_jb_50y","ty_icon_yxb_2","com_award_icon_dhq",},
+			pay_name = {"480万","3万","抽奖券*10",},
+		},
+		[4]=
+		{
+			line = 4,
+			gift_id = 10314,
+			name = "98元礼包",
+			order = 4,
+			pay_icon = {"ty_icon_jb_98y","ty_icon_yxb_4","com_award_icon_dhq",},
+			pay_name = {"980万","10万","抽奖券*20",},
+		},
+		[5]=
+		{
+			line = 5,
+			gift_id = 10315,
+			name = "198元礼包",
+			order = 5,
+			pay_icon = {"ty_icon_jb_198y","ty_icon_yxb_5","com_award_icon_dhq",},
+			pay_name = {"1980万","50万","抽奖券*30",},
+		},
+		[6]=
+		{
+			line = 6,
+			gift_id = 10316,
+			name = "498元礼包",
+			order = 6,
+			pay_icon = {"ty_icon_jb_498y","ty_icon_yxb_5","com_award_icon_dhq",},
+			pay_name = {"4980万","200万","抽奖券*50",},
+		},
+	},
+}
