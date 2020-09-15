87c9324d7fb905b19e987e27f31dc063c7beda6f
duchao <duchao@qq.com>
2020-09-15 10:28:44 +0800

    小游戏福利

---
+++ config_debug/export_config/activity_030_xyxfl_config.lua
@@ -0,0 +1,95 @@
+return {
+	base=
+	{
+		[1]=
+		{
+			index = 1,
+			icon = "yxfl_bg_1",
+			name = "小游戏福利",
+		},
+	},
+	tge=
+	{
+		tge1=
+		{
+			tge = "tge1",
+			name = "小游戏福利",
+			on_off = 1,
+			is_show = 1,
+			order = 1,
+		},
+	},
+	tge1=
+	{
+		[1]=
+		{
+			id = 1,
+			total = 1,
+			task_name = "水果消消乐使用1万及以上档次，出现1次幸运时刻",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[2]=
+		{
+			id = 2,
+			total = 3,
+			task_name = "水浒消消乐使用1万及以上档次，累计召唤3个英雄",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[3]=
+		{
+			id = 3,
+			total = 3,
+			task_name = "财神消消乐使用1万及以上档次，出现3次天女散花",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[4]=
+		{
+			id = 4,
+			total = 1,
+			task_name = "敲敲乐普通模式中砸中1次金猪",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[5]=
+		{
+			id = 5,
+			total = 2,
+			task_name = "敲敲乐财神模式中连续砸出2次财神",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[6]=
+		{
+			id = 6,
+			total = 2,
+			task_name = "苹果大战中连胜2局",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[7]=
+		{
+			id = 7,
+			total = 1,
+			task_name = "在苹果大战中种出1次金苹果",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+	},
+}
