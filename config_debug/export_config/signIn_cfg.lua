b0b16c413cec71ece118addfb29d8a5f147aa36e
duchao <duchao@qq.com>
2020-09-29 09:30:48 +0800

    配置同步

---
+++ config_debug/export_config/signIn_cfg.lua
@@ -0,0 +1,106 @@
+return {
+	week=
+	{
+		[1]=
+		{
+			id = 1,
+			img = "ty_icon_jb_18y",
+			info = "金币x10000",
+			vip = 1,
+			vipinfo = "金币x20000",
+		},
+		[2]=
+		{
+			id = 2,
+			img = "ty_icon_jb_18y",
+			info = "金币x15000",
+		},
+		[3]=
+		{
+			id = 3,
+			img = "ty_icon_jb_30y",
+			info = "金币x20000",
+			vip = 2,
+			vipinfo = "金币x40000",
+		},
+		[4]=
+		{
+			id = 4,
+			img = "ty_icon_jb_30y",
+			info = "金币x25000",
+		},
+		[5]=
+		{
+			id = 5,
+			img = "ty_icon_jb_50y",
+			info = "金币x30000",
+			vip = 3,
+			vipinfo = "金币x60000",
+		},
+		[6]=
+		{
+			id = 6,
+			img = "ty_icon_jb_50y",
+			info = "金币x35000",
+		},
+		[7]=
+		{
+			id = 7,
+			img = "ty_icon_jb_198y",
+			info = "金币x50000",
+		},
+	},
+	month=
+	{
+		[1]=
+		{
+			line = 1,
+			id = 1,
+			img = "ty_icon_flq2",
+			info = "10福利券",
+			day = 3,
+		},
+		[2]=
+		{
+			line = 2,
+			id = 2,
+			img = "ty_icon_jb_50y",
+			info = "金币x20000",
+			day = 7,
+		},
+		[3]=
+		{
+			line = 3,
+			id = 3,
+			img = "ty_icon_flq3",
+			info = "30福利券",
+			day = 14,
+		},
+		[4]=
+		{
+			line = 4,
+			id = 4,
+			img = "ty_icon_jb_198y",
+			info = "金币x50000",
+			day = 21,
+		},
+		[5]=
+		{
+			line = 5,
+			id = 5,
+			img = "3dby_icon_p1",
+			info = "七日雷霆幻影",
+			day = "M",
+			key = "cpl_notcjj",
+		},
+		[6]=
+		{
+			line = 6,
+			id = 5,
+			img = "ty_icon_flq3",
+			info = "100福利券",
+			day = "M",
+			key = "cpl_cjj",
+		},
+	},
+}
