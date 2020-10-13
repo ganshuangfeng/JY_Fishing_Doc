4e271d2ca8bf386b688bc49a933198bf05bf8eaa
duchao <duchao@qq.com>
2020-10-13 15:42:02 +0800

    提交

---
+++ config_10.13/export_config/prop_clear_server.lua
@@ -0,0 +1,120 @@
+return {
+	player_prop=
+	{
+		[1]=
+		{
+			id = 1,
+			prop_type = "prop_sunshine",
+			start_time = 1591054200,
+			recycle_time = 1591635600,
+		},
+		[2]=
+		{
+			id = 2,
+			prop_type = "prop_carnation",
+			start_time = 1588633200,
+			recycle_time = 1589216400,
+		},
+		[3]=
+		{
+			id = 3,
+			prop_type = "prop_shovel",
+			start_time = 1588028400,
+			recycle_time = 1588611600,
+		},
+		[4]=
+		{
+			id = 4,
+			prop_type = "prop_love",
+			start_time = 1589844600,
+			recycle_time = 1590422400,
+		},
+		[5]=
+		{
+			id = 5,
+			prop_type = "prop_fish_drop_act_0",
+			start_time = 1598310000,
+			recycle_time = 1598889599,
+		},
+		[6]=
+		{
+			id = 6,
+			prop_type = "prop_mfcjq",
+			start_time = 1599523200,
+			recycle_time = 1603123199,
+		},
+		[7]=
+		{
+			id = 7,
+			prop_type = "prop_xycd_sunshine",
+			start_time = 1599523200,
+			recycle_time = 1600099199,
+		},
+		[8]=
+		{
+			id = 8,
+			prop_type = "prop_brass_hammer_1",
+			start_time = 1602547200,
+			recycle_time = 1603123199,
+		},
+		[9]=
+		{
+			id = 9,
+			prop_type = "prop_silver_hammer_1",
+			start_time = 1602547200,
+			recycle_time = 1603123199,
+		},
+		[10]=
+		{
+			id = 10,
+			prop_type = "prop_gold_hammer_1",
+			start_time = 1602547200,
+			recycle_time = 1603123199,
+		},
+		[11]=
+		{
+			id = 11,
+			prop_type = "prop_brass_hammer_2",
+			start_time = 1602547200,
+			recycle_time = 1603123199,
+		},
+		[12]=
+		{
+			id = 12,
+			prop_type = "prop_silver_hammer_2",
+			start_time = 1602547200,
+			recycle_time = 1603123199,
+		},
+		[13]=
+		{
+			id = 13,
+			prop_type = "prop_gold_hammer_2",
+			start_time = 1602547200,
+			recycle_time = 1603123199,
+		},
+		[14]=
+		{
+			id = 14,
+			prop_type = "prop_brass_hammer_3",
+			start_time = 1602547200,
+			recycle_time = 1603123199,
+		},
+		[15]=
+		{
+			id = 15,
+			prop_type = "prop_silver_hammer_3",
+			start_time = 1602547200,
+			recycle_time = 1603123199,
+		},
+		[16]=
+		{
+			id = 16,
+			prop_type = "prop_gold_hammer_3",
+			start_time = 1602547200,
+			recycle_time = 1603123199,
+		},
+	},
+	player_ext_status=
+	{
+	},
+}
