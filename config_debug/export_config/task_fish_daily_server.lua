ec7ca7750bc847d0ac925dbb46456d4e3e14ea3f
duchao <duchao@qq.com>
2020-10-13 14:33:31 +0800

    tijiao

--- config_debug/export_config/task_fish_daily_server.lua
+++ config_debug/export_config/task_fish_daily_server.lua
@@ -1903,9 +1903,9 @@ return {
 			condition_value = {1,2,3,4,5},
 			judge_type = 2,
 		},
-		[24]=
+		[23]=
 		{
-			id = 24,
+			id = 23,
 			condition_id = 1023,
 			condition_name = "base_fish_id",
 			condition_value = {18,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49},
