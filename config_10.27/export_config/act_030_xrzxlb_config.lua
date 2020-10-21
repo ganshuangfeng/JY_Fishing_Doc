5729c77d22a9f07c37bc140ae192bcfe02cf58e7
xusj <xusj@qq.com>
2020-10-21 11:16:45 +0800

    10.27 新人专享礼包

---
+++ config_10.27/export_config/act_030_xrzxlb_config.lua
@@ -0,0 +1,50 @@
+return {
+	Sheet1=
+	{
+		[1]=
+		{
+			line = 1,
+			gift_id = 10338,
+			price = 20,
+			name = "新手宝箱",
+			jl1_image = "ty_icon_jb_498y",
+			jl1_number = "200万",
+			jl2_image = "3dby_btn_bd",
+			jl2_number = 5,
+			jl3_image = "3dby_btn_sd",
+			jl3_number = 5,
+			box_image = "xrzxlb_iocn_xrbx",
+			show_image = "xrzxlb_1000",
+		},
+		[2]=
+		{
+			line = 2,
+			gift_id = 10339,
+			price = 58,
+			name = "高级宝箱",
+			jl1_image = "ty_icon_jb_998y",
+			jl1_number = "580万",
+			jl2_image = "3dby_btn_bd",
+			jl2_number = 10,
+			jl3_image = "3dby_btn_sd",
+			jl3_number = 10,
+			box_image = "xrzxlb_iocn_gjbx",
+			show_image = "xrzxlb_3000",
+		},
+		[3]=
+		{
+			line = 3,
+			gift_id = 10340,
+			price = 128,
+			name = "豪华宝箱",
+			jl1_image = "ty_icon_jb_2498y",
+			jl1_number = "1280万",
+			jl2_image = "3dby_btn_bd",
+			jl2_number = 15,
+			jl3_image = "3dby_btn_sd",
+			jl3_number = 15,
+			box_image = "xrzxlb_iocn_hhbx",
+			show_image = "xrzxlb_6400",
+		},
+	},
+}
