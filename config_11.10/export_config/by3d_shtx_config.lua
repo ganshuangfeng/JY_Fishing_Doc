5fbe31765f79398cfe46f8f0d899bee56996c5c2
duchao <duchao@qq.com>
2020-10-29 11:50:57 +0800

    提交

---
+++ config_11.10/export_config/by3d_shtx_config.lua
@@ -0,0 +1,25 @@
+return {
+	task_info=
+	{
+		[1]=
+		{
+			line = 1,
+			task_id = 15001,
+			task_icon = "ty_icon_jb",
+			text = "捕鱼获得金币10万",
+			total = 10000000,
+			random = 0,
+			award_img = {"ty_icon_jb","ty_icon_yb_1","3dby_btn_sd"},
+			award_txt = {"金币*100","鱼币*1000","锁定*1"},
+		},
+	},
+	extra_award=
+	{
+		[1]=
+		{
+			id = 1,
+			extra_award_img = {"3dby_btn_sd",},
+			extra_award_txt = {"锁定*1",},
+		},
+	},
+}
