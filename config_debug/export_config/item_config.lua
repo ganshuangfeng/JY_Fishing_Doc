226a2968518f26760768e8095ac2ebacef34cee8
xusj <xusj@qq.com>
2020-10-30 17:02:31 +0800

    同步

--- config_debug/export_config/item_config.lua
+++ config_debug/export_config/item_config.lua
@@ -2217,18 +2217,5 @@ return {
 			name = "豪华宝箱",
 			desc = "新人专享豪华宝箱",
 		},
-		[169]=
-		{
-			id = 169,
-			item_id = 169,
-			item_key = "task_1000114",
-			lose_time = -1,
-			is_local_icon = 1,
-			image = "ty_icon_hytb",
-			is_show_bag = 0,
-			order = 33,
-			name = "积分",
-			desc = "新人七天乐积分",
-		},
 	},
 }
