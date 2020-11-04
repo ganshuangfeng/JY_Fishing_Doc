4b93528ddee0be92fe81eb5a8113d5482706f214
xusj <xusj@qq.com>
2020-11-04 15:50:21 +0800

    聚划算显示设置

---
+++ config_11.17/export_config/act_035_jhs_config.lua
@@ -0,0 +1,75 @@
+return {
+	Sheet1=
+	{
+		[1]=
+		{
+			line = 1,
+			price = 2498,
+			shop_id = {10381,10382,10383,},
+			first_buy = {24980万,749万,},
+			twoce_buy = {24980万,1498.8万,},
+			ten_min = {249.8万,},
+			image = "ty_icon_jb_2498y",
+		},
+		[2]=
+		{
+			line = 2,
+			price = 998,
+			shop_id = {10378,10379,10380,},
+			first_buy = {9980万,299万,},
+			twoce_buy = {9980万,598.8万,},
+			ten_min = {99.8万,},
+			image = "ty_icon_jb_998y",
+		},
+		[3]=
+		{
+			line = 3,
+			price = 498,
+			shop_id = {10375,10376,10377,},
+			first_buy = {4980万,149万,},
+			twoce_buy = {4980万,298.8万,},
+			ten_min = {49.8万,},
+			image = "ty_icon_jb_498y",
+		},
+		[4]=
+		{
+			line = 4,
+			price = 198,
+			shop_id = {10372,10373,10374,},
+			first_buy = {1980万,59万,},
+			twoce_buy = {1980万,118.8万,},
+			ten_min = {19.8万,},
+			image = "ty_icon_jb_198y",
+		},
+		[5]=
+		{
+			line = 5,
+			price = 98,
+			shop_id = {10369,10370,10371,},
+			first_buy = {980万,29万,},
+			twoce_buy = {980万,58.8万,},
+			ten_min = {9.8万,},
+			image = "ty_icon_jb_98y",
+		},
+		[6]=
+		{
+			line = 6,
+			price = 38,
+			shop_id = {10366,10367,10368,},
+			first_buy = {380万,11万,},
+			twoce_buy = {380万,22.8万,},
+			ten_min = {3.8万,},
+			image = "ty_icon_jb_30y",
+		},
+		[7]=
+		{
+			line = 7,
+			price = 8,
+			shop_id = {10363,10364,10365,},
+			first_buy = {80万,2.4万,},
+			twoce_buy = {80万,4.8万,},
+			ten_min = {0.8万,},
+			image = "ty_icon_jb_18y",
+		},
+	},
+}
