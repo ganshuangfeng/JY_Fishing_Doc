ccab2b6948237223d98620310f80366cd08ea4c7
xusj <xusj@qq.com>
2020-11-19 19:22:15 +0800

    开启

--- config_debug/export_config/game_module_config.lua
+++ config_debug/export_config/game_module_config.lua
@@ -2823,9 +2823,9 @@ return {
 			key = "act_034_ldfd",
 			desc = "立冬福袋",
 			lua = "Act_034_LDFDManager",
-			is_on_off = 0,
-			enable = 0,
-			state = 0,
+			is_on_off = 1,
+			enable = 1,
+			state = 1,
 		},
 		[289]=
 		{
