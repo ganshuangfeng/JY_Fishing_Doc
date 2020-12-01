f6425407f7efb5a7086a0c9632083773cc89e342
duchao <duchao@qq.com>
2020-12-01 19:33:06 +0800

    tijiao

---
+++ config_debug/export_config/fish_3d_gun_overflow_award.lua
@@ -0,0 +1,88 @@
+return {
+	barrel=
+	{
+		[1]=
+		{
+			id = 1,
+			name = "小钢炮",
+			second_award = 1,
+			max_award = 15000,
+		},
+		[2]=
+		{
+			id = 2,
+			name = "雷霆幻影",
+			second_award = 1,
+			max_award = 80000,
+		},
+		[3]=
+		{
+			id = 3,
+			name = "闪耀风暴",
+			second_award = 1,
+			max_award = 15000,
+		},
+		[4]=
+		{
+			id = 4,
+			name = "金币使者",
+			second_award = 1,
+			max_award = 15000,
+		},
+		[5]=
+		{
+			id = 5,
+			name = "核能风暴",
+			second_award = 1,
+			max_award = 15000,
+		},
+		[6]=
+		{
+			id = 6,
+			name = "死灵之光",
+			second_award = 1,
+			max_award = 15000,
+		},
+		[7]=
+		{
+			id = 7,
+			name = "神龙之力",
+			second_award = 1,
+			max_award = 15000,
+		},
+	},
+	bed=
+	{
+		[1]=
+		{
+			id = 1,
+			name = "青柠",
+			second_award = 1,
+			max_award = 10000,
+		},
+		[2]=
+		{
+			id = 2,
+			name = "绿芒",
+			second_award = 1,
+			max_award = 10000,
+		},
+		[3]=
+		{
+			id = 3,
+			name = "寒霜",
+			second_award = 1,
+			max_award = 10000,
+		},
+		[4]=
+		{
+			id = 4,
+			name = "金焰",
+			second_award = 1,
+			max_award = 10000,
+		},
+	},
+	head_frame=
+	{
+	},
+}
