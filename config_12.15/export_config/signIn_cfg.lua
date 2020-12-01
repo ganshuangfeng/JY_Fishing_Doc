34ddcfa461908d92c5aee267f166f25cf55a79ed
duchao <duchao@qq.com>
2020-12-01 15:53:39 +0800

    提交

---
+++ config_12.15/export_config/signIn_cfg.lua
@@ -0,0 +1,169 @@
+return {
+	week=
+	{
+		[1]=
+		{
+			id = 1,
+			img = "ty_icon_jb_18y",
+			info = "金币x10000",
+			vip = 1,
+			vipinfo = "金币x20000",
+			vip_desc = "VIP1双倍",
+			key = "signin_cpl",
+		},
+		[2]=
+		{
+			id = 2,
+			img = "ty_icon_jb_18y",
+			info = "金币x15000",
+			key = "signin_cpl",
+		},
+		[3]=
+		{
+			id = 3,
+			img = "ty_icon_jb_30y",
+			info = "金币x20000",
+			vip = 2,
+			vipinfo = "金币x40000",
+			vip_desc = "VIP2三倍",
+			key = "signin_cpl",
+		},
+		[4]=
+		{
+			id = 4,
+			img = "ty_icon_jb_30y",
+			info = "金币x25000",
+			key = "signin_cpl",
+		},
+		[5]=
+		{
+			id = 5,
+			img = "ty_icon_jb_50y",
+			info = "金币x30000",
+			vipinfo = "金币x60000",
+			key = "signin_cpl",
+		},
+		[6]=
+		{
+			id = 6,
+			img = "ty_icon_jb_50y",
+			info = "金币x35000",
+			vip = 3,
+			vip_desc = "VIP3三倍",
+			key = "signin_cpl",
+		},
+		[7]=
+		{
+			id = 7,
+			img = "ty_icon_jb_198y",
+			info = "金币x50000",
+			vip = 5,
+			vip_desc = "VIP5三倍",
+			key = "signin_cpl",
+		},
+		[8]=
+		{
+			id = 8,
+			img = "ty_icon_jb_18y",
+			info = "金币x10000",
+			vip = 1,
+			vipinfo = "金币x20000",
+			vip_desc = "VIP1双倍",
+		},
+		[9]=
+		{
+			id = 9,
+			img = "ty_icon_jb_18y",
+			info = "金币x15000",
+		},
+		[10]=
+		{
+			id = 10,
+			img = "ty_icon_jb_30y",
+			info = "金币x20000",
+			vip = 2,
+			vipinfo = "金币x40000",
+			vip_desc = "VIP2双倍",
+		},
+		[11]=
+		{
+			id = 11,
+			img = "ty_icon_jb_30y",
+			info = "金币x25000",
+		},
+		[12]=
+		{
+			id = 12,
+			img = "ty_icon_jb_50y",
+			info = "金币x30000",
+			vip = 3,
+			vipinfo = "金币x60000",
+			vip_desc = "VIP3双倍",
+		},
+		[13]=
+		{
+			id = 13,
+			img = "ty_icon_jb_50y",
+			info = "金币x35000",
+		},
+		[14]=
+		{
+			id = 14,
+			img = "ty_icon_jb_198y",
+			info = "金币x50000",
+		},
+	},
+	month=
+	{
+		[1]=
+		{
+			line = 1,
+			id = 1,
+			img = "ty_icon_flq2",
+			info = "10福利券",
+			day = 3,
+		},
+		[2]=
+		{
+			line = 2,
+			id = 2,
+			img = "ty_icon_jb_50y",
+			info = "金币x20000",
+			day = 7,
+		},
+		[3]=
+		{
+			line = 3,
+			id = 3,
+			img = "ty_icon_flq3",
+			info = "30福利券",
+			day = 14,
+		},
+		[4]=
+		{
+			line = 4,
+			id = 4,
+			img = "ty_icon_jb_198y",
+			info = "金币x50000",
+			day = 21,
+		},
+		[5]=
+		{
+			line = 5,
+			id = 5,
+			img = "3dby_icon_p1",
+			info = "七日雷霆幻影",
+			day = "M",
+			key = "cpl_notcjj",
+		},
+		[6]=
+		{
+			line = 6,
+			id = 5,
+			img = "ty_icon_flq3",
+			info = "100福利券",
+			day = "M",
+			key = "cpl_cjj",
+		},
+	},
+}
