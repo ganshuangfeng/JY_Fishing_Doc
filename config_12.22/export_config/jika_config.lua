2b71ecbc7e1bc2e73979295deed9e5ab29c28ff7
xusj <xusj@qq.com>
2020-12-09 13:50:51 +0800

    12.22优化 季卡

---
+++ config_12.22/export_config/jika_config.lua
@@ -0,0 +1,157 @@
+return {
+	base=
+	{
+		[1]=
+		{
+			Index = 1,
+			award_id = 1,
+			award_text = "40万鱼币",
+			award_image = "com_icon_yb",
+		},
+		[2]=
+		{
+			Index = 2,
+			award_id = 9,
+			award_text = "1.98万福利券",
+			award_image = "ls_icon_hb1",
+		},
+		[3]=
+		{
+			Index = 3,
+			award_id = 3,
+			award_text = "45万鱼币",
+			award_image = "com_award_icon_yb1",
+		},
+		[4]=
+		{
+			Index = 4,
+			award_id = 4,
+			award_text = "50万金币",
+			award_image = "pay_icon_gold3",
+		},
+		[5]=
+		{
+			Index = 5,
+			award_id = 5,
+			award_text = "60万鱼币",
+			award_image = "com_award_icon_yb1",
+		},
+		[6]=
+		{
+			Index = 6,
+			award_id = 6,
+			award_text = "话费碎片*100",
+			award_image = "com_award_icon_hfsp",
+		},
+		[7]=
+		{
+			Index = 7,
+			award_id = 7,
+			award_text = "500万鱼币",
+			award_image = "com_award_icon_yb2",
+		},
+		[8]=
+		{
+			Index = 8,
+			award_id = 8,
+			award_text = "1000万金币",
+			award_image = "pay_icon_gold8",
+		},
+		[9]=
+		{
+			Index = 9,
+			award_id = 2,
+			award_text = "30万鱼币",
+			award_image = "com_icon_yb",
+		},
+	},
+	base_cjj=
+	{
+		[1]=
+		{
+			Index = 1,
+			award_id = 1,
+			award_text = "40万小游戏币",
+			award_image = "ty_icon_yxb_2",
+		},
+		[2]=
+		{
+			Index = 2,
+			award_id = 9,
+			award_text = "1.98万福利券",
+			award_image = "ls_icon_hb1",
+		},
+		[3]=
+		{
+			Index = 3,
+			award_id = 3,
+			award_text = "45万小游戏币",
+			award_image = "ty_icon_yxb_4",
+		},
+		[4]=
+		{
+			Index = 4,
+			award_id = 4,
+			award_text = "50万金币",
+			award_image = "pay_icon_gold3",
+		},
+		[5]=
+		{
+			Index = 5,
+			award_id = 5,
+			award_text = "60万小游戏币",
+			award_image = "ty_icon_yxb_4",
+		},
+		[6]=
+		{
+			Index = 6,
+			award_id = 6,
+			award_text = "话费碎片*100",
+			award_image = "com_award_icon_hfsp",
+		},
+		[7]=
+		{
+			Index = 7,
+			award_id = 7,
+			award_text = "500万小游戏币",
+			award_image = "ty_icon_yxb_5",
+		},
+		[8]=
+		{
+			Index = 8,
+			award_id = 8,
+			award_text = "1000万金币",
+			award_image = "pay_icon_gold8",
+		},
+		[9]=
+		{
+			Index = 9,
+			award_id = 2,
+			award_text = "30万小游戏币",
+			award_image = "ty_icon_yxb_3",
+		},
+	},
+	help_info=
+	{
+		[1]=
+		{
+			id = 1,
+			text = "1.季卡礼包最高可获得<color=#BD1C1CFF>9亿</color>金币价值奖励；",
+		},
+		[2]=
+		{
+			id = 2,
+			text = "2.季卡礼包购买后立即获得880万金币和90张抽奖券；",
+		},
+		[3]=
+		{
+			id = 3,
+			text = "3.抽奖券每天可使用1张，若当天未使用也会扣除一张，请及时使用。",
+		},
+		[4]=
+		{
+			id = 4,
+			text = "4.若抽完90张抽奖券获得道具的金币价值加上880万金币之和小于1980万，则联系客服可再领取1980万金币。",
+		},
+	},
+}
