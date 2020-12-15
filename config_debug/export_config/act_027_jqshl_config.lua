828a370a294e480f26f7c9a74bcd5240441aa0c9
duchao <duchao@qq.com>
2020-12-15 17:17:33 +0800

    优化模板火动

---
+++ config_debug/export_config/act_027_jqshl_config.lua
@@ -0,0 +1,236 @@
+return {
+	task=
+	{
+		[1]=
+		{
+			index = 1,
+			task_level = 1,
+			type = 0,
+			task_name = "累计抽取福利券3",
+			image = "ty_icon_jb_6y",
+			text = "1000金币",
+		},
+		[2]=
+		{
+			index = 2,
+			task_level = 2,
+			type = 0,
+			task_name = "累计抽取福利券6",
+			image = "ty_icon_jb_15y",
+			text = "2000金币",
+		},
+		[3]=
+		{
+			index = 3,
+			task_level = 1,
+			type = 1,
+			task_name = "累计充值3元",
+			image = "ty_icon_flq2",
+			text = "10福利券",
+		},
+		[4]=
+		{
+			index = 4,
+			task_level = 3,
+			type = 0,
+			task_name = "累计抽取福利券10",
+			image = "ty_icon_flq1",
+			text = "2福利券",
+		},
+		[5]=
+		{
+			index = 5,
+			task_level = 4,
+			type = 0,
+			task_name = "累计抽取福利券20",
+			image = "ty_icon_flq1",
+			text = "5福利券",
+		},
+		[6]=
+		{
+			index = 6,
+			task_level = 2,
+			type = 1,
+			task_name = "累计充值10元",
+			image = "ty_icon_flq3",
+			text = "35福利券",
+		},
+		[7]=
+		{
+			index = 7,
+			task_level = 5,
+			type = 0,
+			task_name = "累计抽取福利券50",
+			image = "ty_icon_flq2",
+			text = "15福利券",
+		},
+		[8]=
+		{
+			index = 8,
+			task_level = 6,
+			type = 0,
+			task_name = "累计抽取福利券100",
+			image = "ty_icon_flq2",
+			text = "25福利券",
+		},
+		[9]=
+		{
+			index = 9,
+			task_level = 3,
+			type = 1,
+			task_name = "累计充值48元",
+			image = "ty_icon_flq3",
+			text = "180福利券",
+		},
+		[10]=
+		{
+			index = 10,
+			task_level = 7,
+			type = 0,
+			task_name = "累计抽取福利券300",
+			image = "ty_icon_flq3",
+			text = "100福利券",
+		},
+		[11]=
+		{
+			index = 11,
+			task_level = 8,
+			type = 0,
+			task_name = "累计抽取福利券600",
+			image = "ty_icon_flq3",
+			text = "150福利券",
+		},
+		[12]=
+		{
+			index = 12,
+			task_level = 4,
+			type = 1,
+			task_name = "累计充值198元",
+			image = "ty_icon_flq3",
+			text = "750福利券",
+		},
+		[13]=
+		{
+			index = 13,
+			task_level = 9,
+			type = 0,
+			task_name = "累计抽取福利券1200",
+			image = "ty_icon_flq3",
+			text = "300福利券",
+		},
+		[14]=
+		{
+			index = 14,
+			task_level = 10,
+			type = 0,
+			task_name = "累计抽取福利券2000",
+			image = "ty_icon_flq3",
+			text = "400福利券",
+		},
+		[15]=
+		{
+			index = 15,
+			task_level = 5,
+			type = 1,
+			task_name = "累计充值500元",
+			image = "ty_icon_flq4",
+			text = "1500福利券",
+		},
+		[16]=
+		{
+			index = 16,
+			task_level = 11,
+			type = 0,
+			task_name = "累计抽取福利券4000",
+			image = "ty_icon_flq4",
+			text = "1000福利券",
+		},
+		[17]=
+		{
+			index = 17,
+			task_level = 12,
+			type = 0,
+			task_name = "累计抽取福利券8000",
+			image = "ty_icon_flq4",
+			text = "2000福利券",
+		},
+		[18]=
+		{
+			index = 18,
+			task_level = 13,
+			type = 0,
+			task_name = "累计抽取福利券1.5万",
+			image = "ty_icon_flq4",
+			text = "3500福利券",
+		},
+		[19]=
+		{
+			index = 19,
+			task_level = 6,
+			type = 1,
+			task_name = "累计充值2498元",
+			image = "ty_icon_flq5",
+			text = "10000福利券",
+		},
+		[20]=
+		{
+			index = 20,
+			task_level = 14,
+			type = 0,
+			task_name = "累计抽取福利券5万",
+			image = "ty_icon_flq5",
+			text = "15000福利券",
+		},
+		[21]=
+		{
+			index = 21,
+			task_level = 15,
+			type = 0,
+			task_name = "累计抽取福利券10万",
+			image = "ty_icon_flq5",
+			text = "32000福利券",
+		},
+	},
+	other_data=
+	{
+		[1]=
+		{
+			line = 1,
+			cz_permiss = "actp_own_task_p_027_mfflq",
+			ljyj_permiss = "actp_own_task_p_027_mfflq",
+			cz_task_id = 1000120,
+			taskid = 1000119,
+			end_time = 1607356799,
+			sta_time = 1606780800,
+			act_time = "活动时间:12月1日08:00-12月7日23:59:59",
+		},
+	},
+	help_Info=
+	{
+		[1]=
+		{
+			line = 1,
+			content = "1.活动时间：12月1日8:00-12月7日23:59:59。",
+		},
+		[2]=
+		{
+			line = 2,
+			content = "2.所有任务奖励必须先领取前一个任务的奖励后才能领取，完成任务越多，领取福利券奖励越多哦。",
+		},
+		[3]=
+		{
+			line = 3,
+			content = "3.累计抽取福利券统计范围包括所有游戏。",
+		},
+		[4]=
+		{
+			line = 4,
+			content = "4.累计充值任务只记录游戏商城中的充值，不包含带有“推荐必买”和“活动”标签的商品。",
+		},
+		[5]=
+		{
+			line = 5,
+			content = "5.每日0点重置所有任务，请抓紧时间完成任务，未领取的奖励视为自动放弃",
+		},
+	},
+}
---
+++ config_debug/export_config/act_027_mfflq_config.lua
@@ -0,0 +1,14 @@
+return {
+	data=
+	{
+		[1]=
+		{
+			line = 1,
+			gift_id = 10290,
+			box_exchange_id = 40,
+			award_index = 11731,
+			permission = "actp_buy_gift_bag_10290",
+			act_time = "活动时间：10月13日08:00—10月19日23:59:59",
+		},
+	},
+}
