c865020598e3115db4074fb5856b03e7141a3aa9
duchao <duchao@qq.com>
2020-12-29 17:25:37 +0800

    配置同步

---
+++ config_1.12/export_config/activity_042_hlfl_config.lua
@@ -0,0 +1,192 @@
+return {
+	base=
+	{
+		[1]=
+		{
+			index = 1,
+			icon = "hlqd_xycfl_bg_1",
+			name = "欢乐福利",
+		},
+	},
+	tge=
+	{
+		tge1=
+		{
+			tge = "tge1",
+			name = "欢乐福利",
+			on_off = 1,
+			is_show = 1,
+			order = 1,
+			condi_key = "actp_own_task_p_hljnh_046_hlfl_nor",
+		},
+		tge2=
+		{
+			tge = "tge2",
+			name = "欢乐福利",
+			on_off = 1,
+			is_show = 1,
+			order = 1,
+			condi_key = "actp_own_task_p_hljnh_046_hlfl_cjj",
+		},
+	},
+	tge1=
+	{
+		[1]=
+		{
+			id = 1,
+			task = 1000175,
+			total = 3,
+			task_name = "在3D捕鱼中使用3次锁定卡",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[2]=
+		{
+			id = 2,
+			task = 1000176,
+			total = 3,
+			task_name = "在3D捕鱼中使用3次超级火力卡",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[3]=
+		{
+			id = 3,
+			task = 1000177,
+			total = 3,
+			task_name = "在3D捕鱼中使用3次双倍奖励卡",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[4]=
+		{
+			id = 4,
+			task = 1000178,
+			total = 3,
+			task_name = "在3D捕鱼深海探险当前层数+3",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[5]=
+		{
+			id = 5,
+			task = 1000179,
+			total = 1,
+			task_name = "在3D捕鱼中使用1000及以上炮倍捕获1条话费鱼",
+			level = 1,
+			item = {"jing_bi",},
+			count = {20000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[6]=
+		{
+			id = 6,
+			task = 1000180,
+			total = 1,
+			task_name = "水果消消乐使用1万及以上档次，出现1次幸运时刻",
+			level = 1,
+			item = {"jing_bi",},
+			count = {20000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[7]=
+		{
+			id = 7,
+			task = 1000181,
+			total = 3,
+			task_name = "水浒消消乐使用1万及以上档次，累计召唤3次英雄",
+			level = 1,
+			item = {"jing_bi",},
+			count = {20000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[8]=
+		{
+			id = 8,
+			task = 1000182,
+			total = 3,
+			task_name = "苹果大战中种出1次金苹果",
+			level = 1,
+			item = {"jing_bi",},
+			count = {20000,},
+			gotoUI = {"game_MiniGame",},
+		},
+	},
+	tge2=
+	{
+		[1]=
+		{
+			id = 1,
+			task = 1000183,
+			total = 2,
+			task_name = "在冲金鸡财神模式中连胜2局",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[2]=
+		{
+			id = 2,
+			task = 1000184,
+			total = 5,
+			task_name = "在冲金鸡财神模式中累胜5局",
+			level = 1,
+			item = {"jing_bi",},
+			count = {20000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[3]=
+		{
+			id = 3,
+			task = 1000185,
+			total = 100000,
+			task_name = "在龙王争霸中累计赢金10万",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[4]=
+		{
+			id = 4,
+			task = 1000186,
+			total = 300000,
+			task_name = "在龙王争霸中累计赢金30万",
+			level = 1,
+			item = {"jing_bi",},
+			count = {20000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[5]=
+		{
+			id = 5,
+			task = 1000187,
+			total = 2,
+			task_name = "在苹果大战中连胜2局",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[6]=
+		{
+			id = 6,
+			task = 1000188,
+			total = 1,
+			task_name = "在苹果大战中种出1次金苹果",
+			level = 1,
+			item = {"jing_bi",},
+			count = {20000,},
+			gotoUI = {"game_MiniGame",},
+		},
+	},
+}
