ca5e929909c66b7ef148f1b5bcdc2129e7043055
denglu <denglu@qq.com>
2020-12-30 17:08:47 +0800

    修改

---
+++ config_1.12/export_config/act_ty_lb1_config.lua
@@ -0,0 +1,125 @@
+return {
+	platform_channel=
+	{
+		[1]=
+		{
+			line = 1,
+			permission = "actp_no_cjj_gej_exchange",
+			item_key = {"prop_hqjnh_xybox","prop_hqjnh_ssbox","prop_hqjnh_csbox"},
+			box_exchange_id = {70,71,72},
+			config = "config_normal",
+		},
+		[2]=
+		{
+			line = 2,
+			permission = "actp_cjj_gej_exchange",
+			item_key = {"prop_hqjnh_xybox","prop_hqjnh_ssbox","prop_hqjnh_csbox"},
+			box_exchange_id = {70,71,72},
+			config = "config_cjj",
+		},
+	},
+	other_data=
+	{
+		[1]=
+		{
+			line = 1,
+			sta_t = 1610409600,
+			end_t = 1610985599,
+			xg_desc = "每个礼包每日限购1次",
+		},
+	},
+	config_normal=
+	{
+		[1]=
+		{
+			id = 1,
+			title = "5020万金币",
+			jg = "498元",
+			tips = "活动期间可开启，最高可得2000福利券！",
+			bx_img = "gelb_btn_bx3",
+			shop_id = 10428,
+			jl_img = {"3dby_btn_jb","ty_lb1_icon_box2_3"},
+			jl_number = {"x10","传说x15"},
+		},
+		[2]=
+		{
+			id = 2,
+			title = "2010万金币",
+			jg = "198元",
+			tips = "活动期间可开启，最高可得2000福利券！",
+			bx_img = "gelb_btn_bx3",
+			shop_id = 10429,
+			jl_img = {"3dby_btn_jb","ty_lb1_icon_box2_3"},
+			jl_number = {"x6","传说x5"},
+		},
+		[3]=
+		{
+			id = 3,
+			title = "996万金币",
+			jg = "98元",
+			tips = "活动期间可开启，最高可得1000福利券！",
+			bx_img = "gelb_btn_bx2",
+			shop_id = 10430,
+			jl_img = {"3dby_btn_jb","ty_lb1_icon_4"},
+			jl_number = {"x4","史诗x3"},
+		},
+		[4]=
+		{
+			id = 4,
+			title = "488万金币",
+			jg = "48元",
+			tips = "活动期间可开启，最高可得500福利券！",
+			bx_img = "gelb_btn_bx1",
+			shop_id = 10431,
+			jl_img = {"3dby_btn_jb","ty_lb1_icon_3"},
+			jl_number = {"x2","稀有x3"},
+		},
+	},
+	config_cjj=
+	{
+		[1]=
+		{
+			id = 1,
+			title = "5000万金币",
+			jg = "498元",
+			tips = "活动期间可开启，最高可得2000福利券！",
+			bx_img = "gelb_btn_bx3",
+			shop_id = 10432,
+			jl_img = {"zpg_icon_yg","ty_lb1_icon_box2_3"},
+			jl_number = {"x10","传说x15"},
+		},
+		[2]=
+		{
+			id = 2,
+			title = "2000万金币",
+			jg = "198元",
+			tips = "活动期间可开启，最高可得2000福利券！",
+			bx_img = "gelb_btn_bx3",
+			shop_id = 10433,
+			jl_img = {"zpg_icon_yg","ty_lb1_icon_box2_3"},
+			jl_number = {"x6","传说x5"},
+		},
+		[3]=
+		{
+			id = 3,
+			title = "996万金币",
+			jg = "98元",
+			tips = "活动期间可开启，最高可得1000福利券！",
+			bx_img = "gelb_btn_bx2",
+			shop_id = 10434,
+			jl_img = {"zpg_icon_shui","ty_lb1_icon_4"},
+			jl_number = {"x4","史诗x3"},
+		},
+		[4]=
+		{
+			id = 4,
+			title = "488万金币",
+			jg = "48元",
+			tips = "活动期间可开启，最高可得500福利券！",
+			bx_img = "gelb_btn_bx1",
+			shop_id = 10435,
+			jl_img = {"zpg_icon_shui","ty_lb1_icon_3"},
+			jl_number = {"x2","稀有x3"},
+		},
+	},
+}
