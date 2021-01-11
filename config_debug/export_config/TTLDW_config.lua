529f5848efb524f4e2a1d5a77a549832200369e6
duchao <duchao@qq.com>
2021-01-11 11:50:01 +0800

    弹弹乐权限

---
+++ config_debug/export_config/TTLDW_config.lua
@@ -0,0 +1,77 @@
+return {
+	auto=
+	{
+		[1]=
+		{
+			line = 1,
+			dw = 1,
+			min = 5000,
+			max = 10000,
+			key = "tantanle_dc_1",
+		},
+		[2]=
+		{
+			line = 2,
+			dw = 2,
+			min = 10000,
+			max = 50000,
+			key = "tantanle_dc_2",
+		},
+		[3]=
+		{
+			line = 3,
+			dw = 3,
+			min = 50000,
+			max = 100000,
+			key = "tantanle_dc_3",
+		},
+		[4]=
+		{
+			line = 4,
+			dw = 4,
+			min = 100000,
+			max = 500000,
+			key = "tantanle_dc_4",
+		},
+		[5]=
+		{
+			line = 5,
+			dw = 5,
+			min = 500000,
+			max = 1000000,
+			key = "tantanle_dc_5",
+		},
+		[6]=
+		{
+			line = 6,
+			dw = 6,
+			min = 1000000,
+			max = 5000000,
+			key = "tantanle_dc_6",
+		},
+		[7]=
+		{
+			line = 7,
+			dw = 7,
+			min = 5000000,
+			max = 10000000,
+			key = "tantanle_dc_7",
+		},
+		[8]=
+		{
+			line = 8,
+			dw = 8,
+			min = 10000000,
+			max = 20000000,
+			key = "tantanle_dc_8",
+		},
+		[9]=
+		{
+			line = 9,
+			dw = 9,
+			min = 20000000,
+			max = 20000000,
+			key = "tantanle_dc_9",
+		},
+	},
+}
