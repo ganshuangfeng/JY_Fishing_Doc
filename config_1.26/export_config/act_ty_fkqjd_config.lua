495ecb039f50d1a16d8704bfd017c0b0a440af92
xusj <xusj@qq.com>
2021-01-12 11:43:54 +0800

    疯狂敲金蛋用表

---
+++ config_1.26/export_config/act_ty_fkqjd_config.lua
@@ -0,0 +1,86 @@
+return {
+	platform_or_channel_or_level=
+	{
+		[1]=
+		{
+			line = 1,
+			permission = "sdasd",
+			gift_config = "gift_config1",
+			eggs_config = "eggs_config1",
+		},
+		[2]=
+		{
+			line = 2,
+			gift_config = "gift_config2",
+			eggs_config = "eggs_config2",
+		},
+		[3]=
+		{
+			line = 3,
+			gift_config = "gift_config3",
+			eggs_config = "eggs_config3",
+		},
+	},
+	other_data=
+	{
+		[1]=
+		{
+			line = 1,
+			sta_t = 1608593400,
+			end_t = 1609171199,
+			btm_txt = {"每日0点重置所有金蛋和清除未使用的锤子,请及时敲蛋!"},
+			help_txt = {"1.活动时间：1月26日8:00-2月1日23:59:59。","2.同一个金蛋5锤必破，前4锤均有机会敲破金蛋拿走奖励。","3.所有金蛋每日0点重置，同时清除所有未使用的锤子，请及时敲蛋。",},
+		},
+	},
+	gift_config1=
+	{
+		[1]=
+		{
+			line = 1,
+			gift_id = 4543,
+			price = 18,
+			award_txt = {"x1800000","x1",},
+			award_img = {"",""},
+			award_tips = {"",""},
+		},
+		[2]=
+		{
+			line = 2,
+			price = 30,
+			award_txt = {"x3000000","x1",},
+			award_tips = {"",""},
+		},
+		[3]=
+		{
+			line = 3,
+			price = 48,
+			award_txt = {"x4800000","x1",},
+			award_tips = {"",""},
+		},
+	},
+	eggs_config1=
+	{
+		[1]=
+		{
+			line = 1,
+			task_id = 343,
+			item_key = "sdas",
+			eggs_img = {"","","","",""},
+			must_be_broken = 5,
+			award_txt = 500000,
+			eggs_tip = {"",},
+		},
+		[2]=
+		{
+			line = 2,
+			must_be_broken = 5,
+			award_txt = 700000,
+		},
+		[3]=
+		{
+			line = 3,
+			must_be_broken = 5,
+			award_txt = 1000000,
+		},
+	},
+}
