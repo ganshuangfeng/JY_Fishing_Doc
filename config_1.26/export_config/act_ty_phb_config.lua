5c9b07db920951c6d97be5a3f2a5af33dd3412ee
xusj <xusj@qq.com>
2021-01-12 11:39:10 +0800

    阳光榜单

--- config_1.26/export_config/act_ty_phb_config.lua
+++ config_1.26/export_config/act_ty_phb_config.lua
@@ -5,9 +5,9 @@ return {
 		{
 			line = 1,
 			image = "act_ty_by_drop_7",
-			rank_type = "khqd_001_lzphb_rank",
-			s_time = 1611014400,
-			e_time = 1611590399,
+			rank_type = "drswn_002_ygbd_rank",
+			s_time = 1611590400,
+			e_time = 1612195199,
 			award = "award_config",
 		},
 	},
