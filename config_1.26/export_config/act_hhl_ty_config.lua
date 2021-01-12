ecb4f51d15c0ed75f541f958d3d64014af9aec40
xusj <xusj@qq.com>
2021-01-12 10:55:44 +0800

    兑换奖励用表

---
+++ config_1.26/export_config/act_hhl_ty_config.lua
@@ -0,0 +1,293 @@
+return {
+	config_infor=
+	{
+		[1]=
+		{
+			line = 1,
+			condiy_key = "cpl_notcjj",
+			shop_config = "config",
+			config = "Info",
+			change_type = 19,
+			item_key = "prop_fish_drop_act_0",
+			GotoUI = {"game_Fishing3DHall",},
+			help = "help_info",
+			beginTime = 1611014400,
+			endTime = 1611590399,
+			top_desc = "所有游戏有几率得灵珠，玩高倍场可得更多灵珠！",
+			btm_desc = "充值商城中每日首次购买金币，必得宝箱灵珠！",
+		},
+		[2]=
+		{
+			line = 2,
+			condiy_key = "cpl_cjj",
+			shop_config = "config_cjj",
+			config = "Info_cjj",
+			change_type = 20,
+			item_key = "prop_fish_drop_act_0",
+			GotoUI = {"game_MiniGame",},
+			help = "help_info_cjj",
+			beginTime = 1611014400,
+			endTime = 1611590399,
+			top_desc = "所有游戏有几率得灵珠，玩高倍场可得更多灵珠！",
+			btm_desc = "充值商城中每日首次购买金币，必得宝箱灵珠！",
+		},
+	},
+	Info=
+	{
+		[1]=
+		{
+			ID = 1,
+			award_name = "坚果大礼包",
+			award_image = "dhhl_swjl_1",
+			item_cost_text = 15000,
+			tips = "坚果大礼包",
+			type = 1,
+		},
+		[2]=
+		{
+			ID = 2,
+			award_name = "高档电热水袋",
+			award_image = "dhhl_swjl_2",
+			item_cost_text = 8000,
+			tips = "高档电热水袋",
+			type = 1,
+		},
+		[3]=
+		{
+			ID = 3,
+			award_name = "花生酥1斤",
+			award_image = "dhhl_swjl_3",
+			item_cost_text = 5000,
+			tips = "花生酥1斤",
+			type = 1,
+		},
+		[4]=
+		{
+			ID = 4,
+			award_name = "2000万金币",
+			award_image = "pay_icon_gold9",
+			item_cost_text = 24000,
+			type = 0,
+		},
+		[5]=
+		{
+			ID = 5,
+			award_name = "高级核弹",
+			award_image = "dhhl_icon_gjhd",
+			item_cost_text = 12000,
+			type = 0,
+		},
+		[6]=
+		{
+			ID = 6,
+			award_name = "中级核弹",
+			award_image = "dhhl_icon_zjhd",
+			item_cost_text = 6000,
+			type = 0,
+		},
+		[7]=
+		{
+			ID = 7,
+			award_name = "200万金币",
+			award_image = "pay_icon_gold7",
+			item_cost_text = 2400,
+			type = 0,
+		},
+		[8]=
+		{
+			ID = 8,
+			award_name = "初级核弹",
+			award_image = "dhhl_icon_cjhd",
+			item_cost_text = 1200,
+			type = 0,
+		},
+	},
+	Info_cjj=
+	{
+		[1]=
+		{
+			ID = 1,
+			award_name = "坚果大礼包",
+			award_image = "dhhl_swjl_1",
+			item_cost_text = 15000,
+			tips = "坚果大礼包",
+			type = 1,
+		},
+		[2]=
+		{
+			ID = 2,
+			award_name = "高档电热水袋",
+			award_image = "dhhl_swjl_2",
+			item_cost_text = 8000,
+			tips = "高档电热水袋",
+			type = 1,
+		},
+		[3]=
+		{
+			ID = 3,
+			award_name = "花生酥1斤",
+			award_image = "dhhl_swjl_3",
+			item_cost_text = 5000,
+			tips = "花生酥1斤",
+			type = 1,
+		},
+		[4]=
+		{
+			ID = 4,
+			award_name = "2000万金币",
+			award_image = "pay_icon_gold9",
+			item_cost_text = 24000,
+			type = 0,
+		},
+		[5]=
+		{
+			ID = 5,
+			award_name = "1000万金币",
+			award_image = "pay_icon_gold8",
+			item_cost_text = 12000,
+			type = 0,
+		},
+		[6]=
+		{
+			ID = 6,
+			award_name = "小金锤",
+			award_image = "dhhl_icon_jc",
+			item_cost_text = 6000,
+			type = 0,
+		},
+		[7]=
+		{
+			ID = 7,
+			award_name = "200万金币",
+			award_image = "pay_icon_gold7",
+			item_cost_text = 2400,
+			type = 0,
+		},
+		[8]=
+		{
+			ID = 8,
+			award_name = "小银锤",
+			award_image = "dhhl_icon_yc",
+			item_cost_text = 1200,
+			type = 0,
+		},
+	},
+	config_cjj=
+	{
+		[1000217]=
+		{
+			task_id  = 1000217,
+			shop_id = {2,85},
+			icon_txt = 45,
+		},
+		[1000218]=
+		{
+			task_id  = 1000218,
+			shop_id = {3,86},
+			icon_txt = 90,
+		},
+		[1000219]=
+		{
+			task_id  = 1000219,
+			shop_id = {4,87},
+			icon_txt = 150,
+		},
+		[1000220]=
+		{
+			task_id  = 1000220,
+			shop_id = {5,88},
+			icon_txt = 290,
+		},
+		[1000221]=
+		{
+			task_id  = 1000221,
+			shop_id = {111,10189},
+			icon_txt = 590,
+		},
+		[1000222]=
+		{
+			task_id  = 1000222,
+			shop_id = {6,89},
+			icon_txt = 1490,
+		},
+		[1000223]=
+		{
+			task_id  = 1000223,
+			shop_id = {108,10044},
+			icon_txt = 2990,
+		},
+		[1000224]=
+		{
+			task_id  = 1000224,
+			shop_id = {110,10045},
+			icon_txt = 7490,
+		},
+	},
+	config=
+	{
+		[1000217]=
+		{
+			task_id  = 1000217,
+			shop_id = {2,85},
+			icon_txt = 45,
+		},
+		[1000218]=
+		{
+			task_id  = 1000218,
+			shop_id = {3,86},
+			icon_txt = 90,
+		},
+		[1000219]=
+		{
+			task_id  = 1000219,
+			shop_id = {4,87},
+			icon_txt = 150,
+		},
+		[1000220]=
+		{
+			task_id  = 1000220,
+			shop_id = {5,88},
+			icon_txt = 290,
+		},
+		[1000221]=
+		{
+			task_id  = 1000221,
+			shop_id = {111,10189},
+			icon_txt = 590,
+		},
+		[1000222]=
+		{
+			task_id  = 1000222,
+			shop_id = {6,89},
+			icon_txt = 1490,
+		},
+		[1000223]=
+		{
+			task_id  = 1000223,
+			shop_id = {108,10044},
+			icon_txt = 2990,
+		},
+		[1000224]=
+		{
+			task_id  = 1000224,
+			shop_id = {110,10045},
+			icon_txt = 7490,
+		},
+	},
+	help_info=
+	{
+		[1]=
+		{
+			line = 1,
+			desc = {"1.活动时间：1月19日8:00-1月25日23:59:59。","2.活动期间，玩所有游戏都有几率获得灵珠道具，灵珠可用于兑换奖励，高倍场可获得更多灵珠哦！","3.活动结束后，所有灵珠将统一清除，请及时进行奖励兑换","4.实物奖励请关注公众号《鲸鱼初纪元》联系在线客服领取","5.实物图片仅供参考，请以实际发出的奖励为准",},
+		},
+	},
+	help_info_cjj=
+	{
+		[1]=
+		{
+			line = 1,
+			desc = {"1.活动时间：1月19日8:00-1月25日23:59:59。","2.活动期间，玩所有游戏都有几率获得灵珠道具，灵珠可用于兑换奖励，高倍场可获得更多灵珠哦！","3.活动结束后，所有灵珠将统一清除，请及时进行奖励兑换","4.实物奖励请关注公众号《彩云新世界》联系在线客服领取","5.实物图片仅供参考，请以实际发出的奖励为准",},
+		},
+	},
+}
