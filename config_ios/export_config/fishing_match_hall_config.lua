b83fafc83dfca4a5f87e95f5771965cfc2673fcc
duchao <duchao@qq.com>
2021-01-13 19:25:05 +0800

    ios提审相关配置提交

---
+++ config_ios/export_config/fishing_match_hall_config.lua
@@ -0,0 +1,30 @@
+return {
+	game=
+	{
+		[1]=
+		{
+			id = 1,
+			game_key = "pms",
+			game_name = {"bs_btn_pmtzs_01","bs_btn_pmtzs_02"},
+			order = 1,
+			isOnOff = 1,
+		},
+		[2]=
+		{
+			id = 2,
+			game_key = "qys",
+			game_name = {"bs_btn_pmtzs_01","bs_btn_pmtzs_02"},
+			order = 2,
+			isOnOff = 0,
+		},
+		[3]=
+		{
+			id = 3,
+			game_key = "djs",
+			game_name = {"bs_btn_pmtzs_01","bs_btn_pmtzs_02"},
+			order = 3,
+			tag = "VIP",
+			isOnOff = 0,
+		},
+	},
+}
