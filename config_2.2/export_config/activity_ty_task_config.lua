84f6eb0cc6323c3d8ec2891938f4a6b4e4675e43
xusj <xusj@qq.com>
2021-02-03 12:11:42 +0800

    换表

---
+++ config_2.2/export_config/activity_ty_task_config.lua
@@ -0,0 +1,243 @@
+return {
+	base=
+	{
+		[1]=
+		{
+			index = 1,
+			key = "act_khfl_044",
+			icon = "khfl_bg_1",
+			name = "新年福利",
+			tges = 1,
+		},
+		[2]=
+		{
+			index = 2,
+			key = "act_khfl_044_cjj",
+			icon = "khfl_bg_1",
+			name = "新年福利_cjj",
+			tges = 2,
+		},
+	},
+	tge=
+	{
+		[1]=
+		{
+			tge_index = 1,
+			name = "新年福利",
+			on_off = 1,
+			is_show = 1,
+			order = 1,
+			task_index = {1,2,3,4,5,6,7,8,9},
+		},
+		[2]=
+		{
+			tge_index = 2,
+			name = "新年福利_cjj",
+			on_off = 1,
+			is_show = 1,
+			order = 1,
+			task_index = {10,11,12,13,14,15,16,17,18},
+		},
+	},
+	task=
+	{
+		[1]=
+		{
+			id = 1,
+			task = 1000240,
+			total = 3,
+			task_name = "在3D捕鱼中使用3次双倍奖励卡",
+			level = 1,
+			item = {"jing_bi",},
+			count = {8000,},
+			gotoUI = {"game_FishingHall",},
+		},
+		[2]=
+		{
+			id = 2,
+			task = 1000241,
+			total = 3,
+			task_name = "在3D捕鱼中使用3次超级火力卡",
+			level = 1,
+			item = {"jing_bi",},
+			count = {8000,},
+			gotoUI = {"game_FishingHall",},
+		},
+		[3]=
+		{
+			id = 3,
+			task = 1000242,
+			total = 1,
+			task_name = "在3D捕鱼深海探险当前层数+1",
+			level = 1,
+			item = {"jing_bi",},
+			count = {5000,},
+			gotoUI = {"game_FishingHall",},
+		},
+		[4]=
+		{
+			id = 4,
+			task = 1000243,
+			total = 3,
+			task_name = "在3D捕鱼深海探险当前层数+3",
+			level = 1,
+			item = {"jing_bi",},
+			count = {8000,},
+			gotoUI = {"game_FishingHall",},
+		},
+		[5]=
+		{
+			id = 5,
+			task = 1000244,
+			total = 1,
+			task_name = "在3D捕鱼海底宝藏中击杀1条深海狂鲨",
+			level = 1,
+			item = {"jing_bi",},
+			count = {15000,},
+			gotoUI = {"game_FishingHall",},
+		},
+		[6]=
+		{
+			id = 6,
+			task = 1000245,
+			total = 3,
+			task_name = "在3D捕鱼海底宝藏中击3条深海狂鲨",
+			level = 1,
+			item = {"jing_bi",},
+			count = {500000,},
+			gotoUI = {"game_FishingHall",},
+		},
+		[7]=
+		{
+			id = 7,
+			task = 1000246,
+			total = 50,
+			task_name = "在3D捕鱼中通过击杀话费鱼累计获得50话费碎片",
+			level = 1,
+			item = {"jing_bi",},
+			count = {8000,},
+			gotoUI = {"game_FishingHall",},
+		},
+		[8]=
+		{
+			id = 8,
+			task = 1000247,
+			total = 150,
+			task_name = "在3D捕鱼中通过击杀话费鱼累计获得150话费碎片",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_FishingHall",},
+		},
+		[9]=
+		{
+			id = 9,
+			task = 1000248,
+			total = 300,
+			task_name = "在3D捕鱼中通过击杀话费鱼累计获得300话费碎片",
+			level = 1,
+			item = {"jing_bi",},
+			count = {15000,},
+			gotoUI = {"game_FishingHall",},
+		},
+		[10]=
+		{
+			id = 10,
+			task = 1000249,
+			total = 300000,
+			task_name = "在西游消消乐中累计赢金30万",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[11]=
+		{
+			id = 11,
+			task = 1000250,
+			total = 800000,
+			task_name = "在西游消消乐中累计赢金80万",
+			level = 1,
+			item = {"jing_bi",},
+			count = {15000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[12]=
+		{
+			id = 12,
+			task = 1000251,
+			total = 1500000,
+			task_name = "在西游消消乐中累计赢金150万",
+			level = 1,
+			item = {"jing_bi",},
+			count = {20000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[13]=
+		{
+			id = 13,
+			task = 1000252,
+			total = 300000,
+			task_name = "在龙王争霸中累计赢金30万",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[14]=
+		{
+			id = 14,
+			task = 1000253,
+			total = 800000,
+			task_name = "在龙王争霸中累计赢金80万",
+			level = 1,
+			item = {"jing_bi",},
+			count = {15000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[15]=
+		{
+			id = 15,
+			task = 1000254,
+			total = 1500000,
+			task_name = "在龙王争霸中累计赢金150万",
+			level = 1,
+			item = {"jing_bi",},
+			count = {20000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[16]=
+		{
+			id = 16,
+			task = 1000255,
+			total = 2,
+			task_name = "在苹果大战中连胜2局",
+			level = 1,
+			item = {"jing_bi",},
+			count = {10000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[17]=
+		{
+			id = 17,
+			task = 1000256,
+			total = 800000,
+			task_name = "在苹果大战中累计纯赢80万",
+			level = 1,
+			item = {"jing_bi",},
+			count = {15000,},
+			gotoUI = {"game_MiniGame",},
+		},
+		[18]=
+		{
+			id = 18,
+			task = 1000257,
+			total = 1,
+			task_name = "苹果大战中种出1次金苹果",
+			level = 1,
+			item = {"jing_bi",},
+			count = {20000,},
+			gotoUI = {"game_MiniGame",},
+		},
+	},
+}
