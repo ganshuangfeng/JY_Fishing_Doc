0eee66bb24a12705e85a7028a13cff147b93cd56
xusj <xusj@qq.com>
2021-02-09 15:17:55 +0800

    3.2 女神礼包用表

---
+++ config_3.2/export_config/act_ty_lb1_config.lua
@@ -0,0 +1,120 @@
+return {
+	platform_channel=
+	{
+		[1]=
+		{
+			line = 1,
+			permission = "actp_no_cjj_gej_exchange",
+			config = "config_normal",
+		},
+		[2]=
+		{
+			line = 2,
+			permission = "actp_cjj_gej_exchange",
+			config = "config_cjj",
+		},
+	},
+	other_data=
+	{
+		[1]=
+		{
+			line = 1,
+			sta_t = 1612828800,
+			end_t = 1613404799,
+		},
+	},
+	config_normal=
+	{
+		[1]=
+		{
+			id = 1,
+			title = "5200万金币",
+			jg = "498元",
+			tips = {"双倍奖励卡，请在3D捕鱼中使用","万能字符，可替代“新年快乐”任意字符"},
+			bx_img = "ty_lb1_btn_bx3",
+			shop_id = 10465,
+			jl_img = {"3dby_btn_jb","ty_lb1_wnz"},
+			jl_number = {"x10","x280"},
+		},
+		[2]=
+		{
+			id = 2,
+			title = "2060万金币",
+			jg = "198元",
+			tips = {"双倍奖励卡，请在3D捕鱼中使用","万能字符，可替代“新年快乐”任意字符"},
+			bx_img = "ty_lb1_btn_bx3",
+			shop_id = 10466,
+			jl_img = {"3dby_btn_jb","ty_lb1_wnz"},
+			jl_number = {"x6","x180"},
+		},
+		[3]=
+		{
+			id = 3,
+			title = "1020万金币",
+			jg = "98元",
+			tips = {"双倍奖励卡，请在3D捕鱼中使用","万能字符，可替代“新年快乐”任意字符"},
+			bx_img = "ty_lb1_btn_bx2",
+			shop_id = 10467,
+			jl_img = {"3dby_btn_jb","ty_lb1_wnz"},
+			jl_number = {"x4","x88"},
+		},
+		[4]=
+		{
+			id = 4,
+			title = "500万金币",
+			jg = "48元",
+			tips = {"双倍奖励卡，请在3D捕鱼中使用","万能字符，可替代“新年快乐”任意字符"},
+			bx_img = "ty_lb1_btn_bx1",
+			shop_id = 10468,
+			jl_img = {"3dby_btn_jb","ty_lb1_wnz"},
+			jl_number = {"x2","x40"},
+		},
+	},
+	config_cjj=
+	{
+		[1]=
+		{
+			id = 1,
+			title = "5200万金币",
+			jg = "498元",
+			tips = {"请在苹果大战中使用","万能字符，可替代“新年快乐”任意字符"},
+			bx_img = "ty_lb1_btn_bx3",
+			shop_id = 10469,
+			jl_img = {"zpg_icon_yg","ty_lb1_wnz"},
+			jl_number = {"x1","x240"},
+		},
+		[2]=
+		{
+			id = 2,
+			title = "2060万金币",
+			jg = "198元",
+			tips = {"请在苹果大战中使用","万能字符，可替代“新年快乐”任意字符"},
+			bx_img = "ty_lb1_btn_bx3",
+			shop_id = 10470,
+			jl_img = {"zpg_icon_yg","ty_lb1_wnz"},
+			jl_number = {"x1","x140"},
+		},
+		[3]=
+		{
+			id = 3,
+			title = "1020万金币",
+			jg = "98元",
+			tips = {"请在苹果大战中使用","万能字符，可替代“新年快乐”任意字符"},
+			bx_img = "ty_lb1_btn_bx2",
+			shop_id = 10471,
+			jl_img = {"zpg_icon_shui","ty_lb1_wnz"},
+			jl_number = {"x4","x60"},
+		},
+		[4]=
+		{
+			id = 4,
+			title = "500万金币",
+			jg = "48元",
+			tips = {"请在苹果大战中使用","万能字符，可替代“新年快乐”任意字符"},
+			bx_img = "ty_lb1_btn_bx1",
+			shop_id = 10472,
+			jl_img = {"zpg_icon_shui","ty_lb1_wnz"},
+			jl_number = {"x2","x30"},
+		},
+	},
+}
