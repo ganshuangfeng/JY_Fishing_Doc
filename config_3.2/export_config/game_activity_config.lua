3dbebc7162d546c6ba532dbbaa0930ff91da4d8a
xusj <xusj@qq.com>
2021-02-09 17:10:21 +0800

    2.16 新游福利 权限

--- config_3.2/export_config/game_activity_config.lua
+++ config_3.2/export_config/game_activity_config.lua
@@ -339,6 +339,7 @@ return {
 			title = "新游福利",
 			order = 2,
 			is_on_off = 1,
+			condi_key = "actp_own_task_p_041_xyxfl",
 			beginTime = 1613433600,
 			endTime = 1614009599,
 			showType = "prefab",
