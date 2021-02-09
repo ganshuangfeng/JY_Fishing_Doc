bb4580dd99a3dc5a93cc5851f9afa894ad1c0a5d
xusj <xusj@qq.com>
2021-02-09 14:34:53 +0800

    3.2 玫瑰榜单 用表

---
+++ config_3.2/export_config/act_ty_phb_config.lua
@@ -0,0 +1,87 @@
+return {
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			is_on_off = 1,
+			ID = 1,
+			item_key = "prop_fish_drop_act_0",
+			rank_type = "gdn_004_jzbd_rank",
+			s_time = 1612828800,
+			e_time = 1613404799,
+			award = {1,2,3,4,5,6,7},
+			path = "act_003_dlphb",
+			condiy_key = "actp_no_cjj_gej_exchange",
+		},
+		[2]=
+		{
+			line = 2,
+			is_on_off = 1,
+			ID = 2,
+			item_key = "prop_fish_drop_act_1",
+			rank_type = "ycs_005_jybb_rank",
+			s_time = 1613433600,
+			e_time = 1614009599,
+			award = {1,2,3,4,5,6,7},
+			path = "act_004_dlphb",
+		},
+		[3]=
+		{
+			line = 3,
+			is_on_off = 1,
+			ID = 3,
+			item_key = "prop_fish_drop_act_2",
+			rank_type = "nyx_006_yxbd_rank",
+			s_time = 1614038400,
+			e_time = 1614614399,
+			award = {1,2,3,4,5,6,7},
+			path = "act_005_dlphb",
+		},
+	},
+	award_config=
+	{
+		[1]=
+		{
+			ID = 1,
+			award = 100000,
+			limit = {1,1,},
+		},
+		[2]=
+		{
+			ID = 2,
+			award = 30000,
+			limit = {2,2},
+		},
+		[3]=
+		{
+			ID = 3,
+			award = 10000,
+			limit = {3,3,},
+		},
+		[4]=
+		{
+			ID = 4,
+			award = 3000,
+			limit = {4,6,},
+		},
+		[5]=
+		{
+			ID = 5,
+			award = 2000,
+			limit = {7,10,},
+		},
+		[6]=
+		{
+			ID = 6,
+			award = 1000,
+			limit = {11,15,},
+		},
+		[7]=
+		{
+			ID = 7,
+			award = 500,
+			limit = {16,20,},
+		},
+	},
+}
