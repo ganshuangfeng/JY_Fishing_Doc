02cae2d71bacf18aa7fd993ccf29d250d3325d3e
xusj <xusj@qq.com>
2021-02-10 09:42:19 +0800

    同步配置

--- config_3.2/export_config/game_module_config_cjj.lua
+++ config_3.2/export_config/game_module_config_cjj.lua
@@ -2665,9 +2665,9 @@ return {
 			key = "cpl_ljyjcfk",
 			desc = "小游戏累计赢金抽福卡",
 			lua = "CPL_LJYJCFKManager",
-			is_on_off = 0,
-			enable = 0,
-			state = 0,
+			is_on_off = 1,
+			enable = 1,
+			state = 1,
 		},
 		[273]=
 		{
