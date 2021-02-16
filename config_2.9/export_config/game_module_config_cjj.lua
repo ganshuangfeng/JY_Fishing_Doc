c2ce6b9f7f28c8efc585d183b79e82d910839aa5
xusj <xusj@qq.com>
2021-02-16 09:33:56 +0800

    关闭 免费福利券

--- config_2.9/export_config/game_module_config_cjj.lua
+++ config_2.9/export_config/game_module_config_cjj.lua
@@ -2635,9 +2635,9 @@ return {
 			key = "act_027_mfflq",
 			desc = "免费福利券",
 			lua = "Act_027_MFFLQManager",
-			is_on_off = 1,
-			enable = 1,
-			state = 1,
+			is_on_off = 0,
+			enable = 0,
+			state = 0,
 		},
 		[270]=
 		{
