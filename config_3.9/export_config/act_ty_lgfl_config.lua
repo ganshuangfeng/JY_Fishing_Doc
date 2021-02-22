a411e679546cf88ac568c903dc3999bea0b4ec34
xusj <xusj@qq.com>
2021-02-22 15:52:45 +0800

    3.9 感恩回馈用表

---
+++ config_3.9/export_config/act_ty_lgfl_config.lua
@@ -0,0 +1,105 @@
+return {
+	base=
+	{
+		[1]=
+		{
+			index = 1,
+			icon = "sxlgfl_bg_1",
+			name = "连购返利",
+		},
+	},
+	tge=
+	{
+		tge1=
+		{
+			tge = "tge1",
+			name = "连购返利",
+			on_off = 1,
+			is_show = 1,
+			order = 1,
+		},
+	},
+	tge1=
+	{
+		[1]=
+		{
+			id = 1,
+			task = 21397,
+			total = 3,
+			task_name = "每天在游戏中累计购买3次2498元",
+			level = 1,
+			item = {"jing_bi",},
+			count = {980000,},
+			N_chose_1_img = {"activity_icon_gift179_tyj","activity_icon_gift181_ktb","activity_icon_gift198_bht"},
+			N_chose_1_text = {"太阳眼镜","空调被","美的风扇",},
+			gotoUI = {"shop_bay","jing_bi",},
+			remarks = "实物奖励三选一",
+		},
+		[2]=
+		{
+			id = 2,
+			task = 21398,
+			total = 3,
+			task_name = "每天在游戏中累计购买3次998元",
+			level = 1,
+			item = {"jing_bi",},
+			count = {480000,},
+			N_chose_1_img = {"activity_icon_gift44","activity_icon_gift199_bht","activity_icon_gift182_ysm"},
+			N_chose_1_text = {"雨伞","矿泉水","鸭舌帽",},
+			gotoUI = {"shop_bay","jing_bi",},
+			remarks = "实物奖励三选一",
+		},
+		[3]=
+		{
+			id = 3,
+			task = 21399,
+			total = 3,
+			task_name = "每天在游戏中累计购买3次498元",
+			level = 1,
+			item = {"jing_bi",},
+			count = {480000,},
+			gotoUI = {"shop_bay","jing_bi",},
+		},
+		[4]=
+		{
+			id = 4,
+			task = 21400,
+			total = 3,
+			task_name = "每天在游戏中累计购买3次198元",
+			level = 1,
+			item = {"jing_bi",},
+			count = {180000,},
+			gotoUI = {"shop_bay","jing_bi",},
+		},
+		[5]=
+		{
+			id = 5,
+			task = 21401,
+			total = 3,
+			task_name = "每天在游戏中累计购买3次98元",
+			level = 1,
+			item = {"jing_bi",},
+			count = {80000,},
+			gotoUI = {"shop_bay","jing_bi",},
+		},
+	},
+	helpinfo=
+	{
+		[1]=
+		{
+			index = 1,
+			text = "1.活动时间：7月21日7:30-7月27日23:59:59",
+			button_image = "xycd_btn_hdgz",
+		},
+		[2]=
+		{
+			index = 2,
+			text = "2.活动任务每天0点重置，未领取的奖励视为自动放弃",
+		},
+		[3]=
+		{
+			index = 3,
+			text = "3.活动中的图片仅作为参考，请以实际发出的奖励为准；",
+		},
+	},
+}
