33219a830ac95bf8a6ed709a7135976a4adce7e5
xusj <xusj@qq.com>
2021-02-23 13:34:54 +0800

    3.2 女神节礼物 用表

---
+++ config_3.2/export_config/act_hhl_ty_config.lua
@@ -0,0 +1,229 @@
+return {
+	config_infor=
+	{
+		[1]=
+		{
+			line = 1,
+			condiy_key = "cpl_notcjj",
+			shop_config = {1,2,3,4,5,6,7,8},
+			config = {1,2,3,4,5,6,7,8},
+			change_type = 39,
+			cur_path = "act_003_hhl_",
+			item_key = "prop_fish_drop_act_2",
+			GotoUI = {"game_Fishing3DHall",},
+			help = "help_info",
+			beginTime = 1614038400,
+			endTime = 1614614399,
+		},
+		[2]=
+		{
+			line = 2,
+			condiy_key = "cpl_cjj",
+			shop_config = {1,2,3,4,5,6,7,8},
+			config = {9,10,11,12,13,14,15,16},
+			change_type = 40,
+			cur_path = "act_003_hhl_",
+			item_key = "prop_fish_drop_act_2",
+			GotoUI = {"game_MiniGame",},
+			help = "help_info_cjj",
+			beginTime = 1614038400,
+			endTime = 1614614399,
+		},
+	},
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			award_name = "徐福记礼盒",
+			award_image = "swjl_1",
+			item_cost_text = 15000,
+			tips = "新年礼盒",
+			type = 1,
+		},
+		[2]=
+		{
+			line = 2,
+			award_name = "琥珀核桃4罐",
+			award_image = "swjl_2",
+			item_cost_text = 8000,
+			tips = "核桃花生奶16盒",
+			type = 1,
+		},
+		[3]=
+		{
+			line = 3,
+			award_name = "高档纯棉浴巾",
+			award_image = "swjl_3",
+			item_cost_text = 5000,
+			tips = "金龙鱼大米5斤",
+			type = 1,
+		},
+		[4]=
+		{
+			line = 4,
+			award_name = "2000万金币",
+			award_image = "pay_icon_gold9",
+			item_cost_text = 24000,
+			type = 0,
+		},
+		[5]=
+		{
+			line = 5,
+			award_name = "高级核弹*1",
+			award_image = "icon_gjhd",
+			item_cost_text = 12000,
+			type = 0,
+		},
+		[6]=
+		{
+			line = 6,
+			award_name = "中级核弹*1",
+			award_image = "icon_zjhd",
+			item_cost_text = 6000,
+			type = 0,
+		},
+		[7]=
+		{
+			line = 7,
+			award_name = "200万金币",
+			award_image = "pay_icon_gold7",
+			item_cost_text = 2400,
+			type = 0,
+		},
+		[8]=
+		{
+			line = 8,
+			award_name = "初级核弹*1",
+			award_image = "icon_cjhd",
+			item_cost_text = 1200,
+			type = 0,
+		},
+		[9]=
+		{
+			line = 9,
+			award_name = "徐福记礼盒",
+			award_image = "swjl_1",
+			item_cost_text = 15000,
+			tips = "新年礼盒",
+			type = 1,
+		},
+		[10]=
+		{
+			line = 10,
+			award_name = "琥珀核桃4罐",
+			award_image = "swjl_2",
+			item_cost_text = 8000,
+			tips = "核桃花生奶16盒",
+			type = 1,
+		},
+		[11]=
+		{
+			line = 11,
+			award_name = "高档纯棉浴巾",
+			award_image = "swjl_3",
+			item_cost_text = 5000,
+			tips = "金龙鱼大米5斤",
+			type = 1,
+		},
+		[12]=
+		{
+			line = 12,
+			award_name = "2000万金币",
+			award_image = "pay_icon_gold9",
+			item_cost_text = 24000,
+			type = 0,
+		},
+		[13]=
+		{
+			line = 13,
+			award_name = "1000万金币",
+			award_image = "pay_icon_gold8",
+			item_cost_text = 12000,
+			type = 0,
+		},
+		[14]=
+		{
+			line = 14,
+			award_name = "小金锤*5",
+			award_image = "icon_jc",
+			item_cost_text = 6000,
+			type = 0,
+		},
+		[15]=
+		{
+			line = 15,
+			award_name = "200万金币",
+			award_image = "pay_icon_gold7",
+			item_cost_text = 2400,
+			type = 0,
+		},
+		[16]=
+		{
+			line = 16,
+			award_name = "小银锤*10",
+			award_image = "icon_yc",
+			item_cost_text = 1200,
+			type = 0,
+		},
+	},
+	shop_config=
+	{
+		[1]=
+		{
+			ID = 1,
+			task_id  = 1000291,
+			shop_id = {2,85},
+			icon_txt = 45,
+		},
+		[2]=
+		{
+			ID = 2,
+			task_id  = 1000292,
+			shop_id = {3,86},
+			icon_txt = 90,
+		},
+		[3]=
+		{
+			ID = 3,
+			task_id  = 1000293,
+			shop_id = {4,87},
+			icon_txt = 150,
+		},
+		[4]=
+		{
+			ID = 4,
+			task_id  = 1000294,
+			shop_id = {5,88},
+			icon_txt = 290,
+		},
+		[5]=
+		{
+			ID = 5,
+			task_id  = 1000295,
+			shop_id = {111,10189},
+			icon_txt = 590,
+		},
+		[6]=
+		{
+			ID = 6,
+			task_id  = 1000296,
+			shop_id = {6,89},
+			icon_txt = 1490,
+		},
+		[7]=
+		{
+			ID = 7,
+			task_id  = 1000297,
+			shop_id = {108,10044},
+			icon_txt = 2990,
+		},
+		[8]=
+		{
+			ID = 8,
+			task_id  = 1000298,
+			shop_id = {110,10045},
+			icon_txt = 7490,
+		},
+	},
+}
