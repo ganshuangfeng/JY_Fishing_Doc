858da9ebeee90d45a414b7f49d37076014a04c2c
xusj <xusj@qq.com>
2021-02-23 10:31:05 +0800

    打开act_ty_task

--- config_2.9/export_config/game_module_config_cjj.lua
+++ config_2.9/export_config/game_module_config_cjj.lua
@@ -2465,9 +2465,9 @@ return {
 			key = "act_ty_task",
 			desc = "通用活动模板1",
 			lua = "ActivityTaskManager",
-			is_on_off = 0,
-			enable = 0,
-			state = 0,
+			is_on_off = 1,
+			enable = 1,
+			state = 1,
 		},
 		[253]=
 		{
