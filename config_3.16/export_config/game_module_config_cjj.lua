9fea5109979ae96fc6d2bab1b6395b7bc9aef134
xusj <xusj@qq.com>
2021-03-02 17:17:49 +0800

    提交

--- config_3.16/export_config/game_module_config_cjj.lua
+++ config_3.16/export_config/game_module_config_cjj.lua
@@ -3070,9 +3070,9 @@ return {
 			id = 313,
 			key = "act_ty_by_hhl_style/act_001_hhl",
 			desc = "换好礼皮肤",
-			is_on_off = 0,
-			enable = 0,
-			state = 0,
+			is_on_off = 1,
+			enable = 1,
+			state = 1,
 		},
 		[314]=
 		{
