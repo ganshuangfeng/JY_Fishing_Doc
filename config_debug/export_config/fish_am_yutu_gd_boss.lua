69882b37a7f45bdc32b96769f45da232ec93cf94
duchao <duchao@qq.com>
2021-03-10 10:12:18 +0800

    奥秘配置合并

---
+++ config_debug/export_config/fish_am_yutu_gd_boss.lua
@@ -0,0 +1,74 @@
+return {
+	image=
+	{
+		[1]=
+		{
+			line = 1,
+			id = 1,
+			time_frame = {90,590,1090,1590,2090,2590,3090,3590,},
+			fish = {10046,},
+			path = {34,95,},
+			path_type = "random",
+			dead_force_create_index = 1,
+			dead_force_create_delay = 30,
+			image_type = "normal",
+		},
+		[2]=
+		{
+			line = 2,
+			id = 2,
+			time_frame = {90,410,730,1050,1370,1690,2010,2330,2650,2970,3290,},
+			fish = {10047,},
+			path = {34,95,},
+			path_type = "random",
+			dead_force_create_index = 2,
+			dead_force_create_delay = 80,
+			image_type = "normal",
+		},
+		[3]=
+		{
+			line = 3,
+			id = 3,
+			time_frame = {100,600,1100,1600,2100,2600,3100,3600,},
+			fish = {10048,},
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,},
+			path_type = "random",
+			dead_force_create_index = 3,
+			dead_force_create_delay = 30,
+			image_type = "normal",
+		},
+		[4]=
+		{
+			line = 4,
+			id = 4,
+			time_frame = {90,390,690,990,1290,1590,1290,1590,1890,2190,2490,},
+			fish = {10049,},
+			path = {11001,},
+			image_type = "normal",
+		},
+		[5]=
+		{
+			line = 5,
+			id = 4,
+			time_frame = {90,390,690,990,1290,1590,1290,1590,1890,2190,2490,},
+			fish = {10049,},
+			path = {11004,},
+		},
+		[6]=
+		{
+			line = 6,
+			id = 4,
+			time_frame = {240,540,840,1140,1740,2040,2340,2640,2940,3240,3540,},
+			fish = {10049,},
+			path = {11002,},
+		},
+		[7]=
+		{
+			line = 7,
+			id = 4,
+			time_frame = {240,540,840,1140,1740,2040,2340,2640,2940,3240,3540,},
+			fish = {10049,},
+			path = {11003,},
+		},
+	},
+}
