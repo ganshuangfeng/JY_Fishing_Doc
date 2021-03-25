81917b2b5af4792e59fc934dab892f03fdcce182
denglu <denglu@qq.com>
2021-03-25 10:48:54 +0800

    修改

--- config_4.6/export_config/permission_server_config.lua
+++ config_4.6/export_config/permission_server_config.lua
@@ -6644,8 +6644,8 @@ return {
 		{
 			id = 371,
 			enable = 1,
-			key = "actp_rank_ymkh_010_wxphb_rank",
-			name = "五星排行榜--非cjj",
+			key = "actp_rank_ltqf_012_fqdr_rank",
+			name = "福气达人--非cjj",
 			condi = 393,
 			is_dynamic_codi = 1,
 			is_dynamic_cancel = 1,
