2277577e4fe4f1ab7f050884b2ae8783648f1a34
denglu <denglu@qq.com>
2021-03-30 14:55:09 +0800

    前端配置，道具清理

---
+++ config_4.6/export_config/prop_clear_server.lua
@@ -0,0 +1,386 @@
+return {
+	player_prop=
+	{
+		[1]=
+		{
+			id = 1,
+			prop_type = "prop_sunshine",
+			start_time = 1591054200,
+			recycle_time = 1591635600,
+		},
+		[2]=
+		{
+			id = 2,
+			prop_type = "prop_carnation",
+			start_time = 1588633200,
+			recycle_time = 1589216400,
+		},
+		[3]=
+		{
+			id = 3,
+			prop_type = "prop_shovel",
+			start_time = 1588028400,
+			recycle_time = 1588611600,
+		},
+		[4]=
+		{
+			id = 4,
+			prop_type = "prop_love",
+			start_time = 1589844600,
+			recycle_time = 1590422400,
+		},
+		[5]=
+		{
+			id = 5,
+			prop_type = "prop_fish_drop_act_0",
+			start_time = 1617667200,
+			recycle_time = 1618243199,
+		},
+		[6]=
+		{
+			id = 6,
+			prop_type = "prop_mfcjq",
+			start_time = 1599523200,
+			recycle_time = 1603123199,
+		},
+		[7]=
+		{
+			id = 7,
+			prop_type = "prop_xycd_sunshine",
+			start_time = 1599523200,
+			recycle_time = 1600099199,
+		},
+		[8]=
+		{
+			id = 8,
+			prop_type = "prop_brass_hammer_1",
+			start_time = 1615248000,
+			recycle_time = 1615823999,
+		},
+		[9]=
+		{
+			id = 9,
+			prop_type = "prop_silver_hammer_1",
+			start_time = 1615248000,
+			recycle_time = 1615823999,
+		},
+		[10]=
+		{
+			id = 10,
+			prop_type = "prop_gold_hammer_1",
+			start_time = 1615248000,
+			recycle_time = 1615823999,
+		},
+		[11]=
+		{
+			id = 11,
+			prop_type = "prop_brass_hammer_2",
+			start_time = 1615248000,
+			recycle_time = 1615823999,
+		},
+		[12]=
+		{
+			id = 12,
+			prop_type = "prop_silver_hammer_2",
+			start_time = 1615248000,
+			recycle_time = 1615823999,
+		},
+		[13]=
+		{
+			id = 13,
+			prop_type = "prop_gold_hammer_2",
+			start_time = 1615248000,
+			recycle_time = 1615823999,
+		},
+		[14]=
+		{
+			id = 14,
+			prop_type = "prop_brass_hammer_3",
+			start_time = 1615248000,
+			recycle_time = 1615823999,
+		},
+		[15]=
+		{
+			id = 15,
+			prop_type = "prop_silver_hammer_3",
+			start_time = 1615248000,
+			recycle_time = 1615823999,
+		},
+		[16]=
+		{
+			id = 16,
+			prop_type = "prop_gold_hammer_3",
+			start_time = 1615248000,
+			recycle_time = 1615823999,
+		},
+		[17]=
+		{
+			id = 17,
+			prop_type = "prop_cyj_cjq",
+			start_time = 1603152000,
+			recycle_time = 1603727999,
+		},
+		[18]=
+		{
+			id = 18,
+			prop_type = "prop_cjlb_1",
+			start_time = 1609200000,
+			recycle_time = 1609775999,
+		},
+		[19]=
+		{
+			id = 19,
+			prop_type = "prop_cjlb_2",
+			start_time = 1609200000,
+			recycle_time = 1609775999,
+		},
+		[20]=
+		{
+			id = 20,
+			prop_type = "prop_cjlb_3",
+			start_time = 1609200000,
+			recycle_time = 1609775999,
+		},
+		[21]=
+		{
+			id = 21,
+			prop_type = "prop_cjlb_4",
+			start_time = 1609200000,
+			recycle_time = 1609775999,
+		},
+		[22]=
+		{
+			id = 22,
+			prop_type = "prop_cjlb_5",
+			start_time = 1609200000,
+			recycle_time = 1609775999,
+		},
+		[23]=
+		{
+			id = 23,
+			prop_type = "prop_cjlb_6",
+			start_time = 1609200000,
+			recycle_time = 1609775999,
+		},
+		[24]=
+		{
+			id = 24,
+			prop_type = "prop_11.11_cjq",
+			start_time = 1604966400,
+			recycle_time = 1605542399,
+		},
+		[25]=
+		{
+			id = 25,
+			prop_type = "prop_gej_hj",
+			start_time = 1606176000,
+			recycle_time = 1606751999,
+		},
+		[26]=
+		{
+			id = 26,
+			prop_type = "prop_gej_xy",
+			start_time = 1606176000,
+			recycle_time = 1606751999,
+		},
+		[27]=
+		{
+			id = 27,
+			prop_type = "prop_gej_ss",
+			start_time = 1606176000,
+			recycle_time = 1606751999,
+		},
+		[28]=
+		{
+			id = 28,
+			prop_type = "prop_gej_cs",
+			start_time = 1606176000,
+			recycle_time = 1606751999,
+		},
+		[29]=
+		{
+			id = 29,
+			prop_type = "prop_12_12_lh",
+			start_time = 1607385600,
+			recycle_time = 1607961599,
+		},
+		[30]=
+		{
+			id = 30,
+			prop_type = "prop_12_12_cjq",
+			start_time = 1607385600,
+			recycle_time = 1607961599,
+		},
+		[31]=
+		{
+			id = 31,
+			prop_type = "prop_dz_jz",
+			start_time = 1607990400,
+			recycle_time = 1608566399,
+		},
+		[32]=
+		{
+			id = 32,
+			prop_type = "prop_sd_lh",
+			start_time = 1608595200,
+			recycle_time = 1609171199,
+		},
+		[33]=
+		{
+			id = 33,
+			prop_type = "prop_yd_jyb",
+			start_time = 1609200000,
+			recycle_time = 1609775999,
+		},
+		[34]=
+		{
+			id = 34,
+			prop_type = "prop_hlqd_xx",
+			start_time = 1609804800,
+			recycle_time = 1610380799,
+		},
+		[35]=
+		{
+			id = 35,
+			prop_type = "prop_qdlb_cjq",
+			start_time = 1615852800,
+			recycle_time = 1616428799,
+		},
+		[36]=
+		{
+			id = 36,
+			prop_type = "prop_hqjnh_csbox",
+			start_time = 1610409600,
+			recycle_time = 1610985599,
+		},
+		[37]=
+		{
+			id = 37,
+			prop_type = "prop_hqjnh_ssbox",
+			start_time = 1610409600,
+			recycle_time = 1610985599,
+		},
+		[38]=
+		{
+			id = 38,
+			prop_type = "prop_hqjnh_xybox",
+			start_time = 1610409600,
+			recycle_time = 1610985599,
+		},
+		[39]=
+		{
+			id = 39,
+			prop_type = "prop_fclb_gong",
+			start_time = 1611014400,
+			recycle_time = 1611590399,
+		},
+		[40]=
+		{
+			id = 40,
+			prop_type = "prop_fclb_xi",
+			start_time = 1611014400,
+			recycle_time = 1611590399,
+		},
+		[41]=
+		{
+			id = 41,
+			prop_type = "prop_fclb_fa",
+			start_time = 1611014400,
+			recycle_time = 1611590399,
+		},
+		[42]=
+		{
+			id = 42,
+			prop_type = "prop_fclb_cai",
+			start_time = 1611014400,
+			recycle_time = 1611590399,
+		},
+		[43]=
+		{
+			id = 43,
+			prop_type = "prop_gdn_x",
+			start_time = 1612828800,
+			recycle_time = 1613404799,
+		},
+		[44]=
+		{
+			id = 44,
+			prop_type = "prop_gdn_n",
+			start_time = 1612828800,
+			recycle_time = 1613404799,
+		},
+		[45]=
+		{
+			id = 45,
+			prop_type = "prop_gdn_k",
+			start_time = 1612828800,
+			recycle_time = 1613404799,
+		},
+		[46]=
+		{
+			id = 46,
+			prop_type = "prop_gdn_l",
+			start_time = 1612828800,
+			recycle_time = 1613404799,
+		},
+		[47]=
+		{
+			id = 47,
+			prop_type = "prop_gdn_wn",
+			start_time = 1612828800,
+			recycle_time = 1613404799,
+		},
+		[48]=
+		{
+			id = 48,
+			prop_type = "prop_xnsmt_cjq",
+			start_time = 1612828800,
+			recycle_time = 1613404799,
+		},
+		[49]=
+		{
+			id = 49,
+			prop_type = "prop_xnsmt_mtsp",
+			start_time = 1612828800,
+			recycle_time = 1613404799,
+		},
+		[50]=
+		{
+			id = 50,
+			prop_type = "prop_fish_drop_act_1",
+			start_time = 1612828800,
+			recycle_time = 1613404799,
+		},
+		[51]=
+		{
+			id = 51,
+			prop_type = "prop_fish_drop_act_2",
+			start_time = 1614038400,
+			recycle_time = 1614614399,
+		},
+		[52]=
+		{
+			id = 52,
+			prop_type = "prop_ty_csbox",
+			start_time = 1614643200,
+			recycle_time = 1615219199,
+		},
+		[53]=
+		{
+			id = 53,
+			prop_type = "prop_ty_ssbox",
+			start_time = 1614643200,
+			recycle_time = 1615219199,
+		},
+		[54]=
+		{
+			id = 54,
+			prop_type = "prop_ty_xybox",
+			start_time = 1614643200,
+			recycle_time = 1615219199,
+		},
+	},
+	player_ext_status=
+	{
+	},
+}
