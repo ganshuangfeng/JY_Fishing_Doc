e7a1ec6de5a5cf717f31fe07c19723e9551adf33
duchao <duchao@qq.com>
2021-04-13 10:59:45 +0800

    提交

---
+++ config_4.13/export_config/lwgp_config_server.lua
@@ -0,0 +1,170 @@
+return {
+	bet=
+	{
+		[1]=
+		{
+			id = 1,
+			bet_type = 1,
+			level0 = 0,
+			level1 = 20000,
+			level2 = 50000,
+			level3 = 200000,
+		},
+		[2]=
+		{
+			id = 2,
+			bet_type = 2,
+			level0 = 0,
+			level1 = 20000,
+			level2 = 100000,
+			level3 = 400000,
+		},
+		[3]=
+		{
+			id = 3,
+			bet_type = 3,
+			level0 = 0,
+			level1 = 20000,
+			level2 = 100000,
+			level3 = 400000,
+		},
+		[4]=
+		{
+			id = 4,
+			bet_type = 4,
+			level0 = 0,
+			level1 = 20000,
+			level2 = 100000,
+			level3 = 400000,
+		},
+		[5]=
+		{
+			id = 5,
+			bet_type = 5,
+			level0 = 0,
+			level1 = 20000,
+			level2 = 200000,
+			level3 = 800000,
+		},
+		[6]=
+		{
+			id = 6,
+			bet_type = 6,
+			level0 = 0,
+			level1 = 20000,
+			level2 = 200000,
+			level3 = 800000,
+		},
+		[7]=
+		{
+			id = 7,
+			bet_type = 7,
+			level0 = 0,
+			level1 = 20000,
+			level2 = 200000,
+			level3 = 800000,
+		},
+	},
+	kaijiang=
+	{
+		[1]=
+		{
+			id = 1,
+			kaijiang_type = 1,
+			weight = 70,
+			level = 1,
+			num = 1,
+			bet_weight = {20,20,20,20,10,5,5},
+		},
+		[2]=
+		{
+			id = 2,
+			kaijiang_type = 2,
+			weight = 5,
+			level = 2,
+			num = 1,
+			bet_weight = {20,20,20,20,10,5,5},
+		},
+		[3]=
+		{
+			id = 3,
+			kaijiang_type = 3,
+			weight = 2,
+			level = 1,
+			num = 2,
+			bet_weight = {20,20,20,20,10,5,5},
+		},
+		[4]=
+		{
+			id = 4,
+			kaijiang_type = 4,
+			weight = 1,
+			level = 1,
+			num = 3,
+			bet_weight = {20,20,20,20,10,5,5},
+		},
+		[5]=
+		{
+			id = 5,
+			kaijiang_type = 5,
+			weight = 1,
+			level = 1,
+			num = 4,
+			bet_weight = {20,20,20,20,10,5,5},
+		},
+		[6]=
+		{
+			id = 6,
+			kaijiang_type = 6,
+			weight = 5,
+			level = 2,
+			num = 3,
+			bet_weight = {0,100,100,100,0,0,0},
+		},
+		[7]=
+		{
+			id = 7,
+			kaijiang_type = 7,
+			weight = 5,
+			level = 2,
+			num = 3,
+			bet_weight = {0,0,0,0,100,10,10},
+		},
+		[8]=
+		{
+			id = 8,
+			kaijiang_type = 8,
+			weight = 1,
+			level = 1,
+			num = 7,
+			bet_weight = {100,100,100,100,100,100,100,},
+		},
+		[9]=
+		{
+			id = 9,
+			kaijiang_type = 9,
+			weight = 1,
+			level = 2,
+			num = 7,
+			bet_weight = {100,100,100,100,100,100,100,},
+		},
+		[10]=
+		{
+			id = 10,
+			kaijiang_type = 10,
+			weight = 3,
+			level = 3,
+			num = 1,
+			bet_weight = {20,20,20,20,10,5,5},
+		},
+		[11]=
+		{
+			id = 11,
+			kaijiang_type = 11,
+			weight = 5,
+			level = 1,
+			num = 0,
+			bet_weight = {0,0,0,0,0,0,0,},
+		},
+	},
+}
