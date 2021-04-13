26de5e61e13c02cae2f0c18a7f2c0d60846654cc
denglu <denglu@qq.com>
2021-04-13 11:18:47 +0800

    欢乐豪礼

---
+++ config_4.20/export_config/act_027_jqshl_config.lua
@@ -0,0 +1,240 @@
+return {
+	task=
+	{
+		[1]=
+		{
+			index = 1,
+			task_level = 1,
+			type = 0,
+			task_name = "累计消耗10万",
+			image = "ty_icon_jb_6y",
+			text = "500金币",
+		},
+		[2]=
+		{
+			index = 2,
+			task_level = 2,
+			type = 0,
+			task_name = "累计消耗30万",
+			image = "ty_icon_jb_15y",
+			text = "1000金币",
+		},
+		[3]=
+		{
+			index = 3,
+			task_level = 1,
+			type = 1,
+			task_name = "累计充值6元",
+			image = "ty_icon_flq2",
+			text = "30福利券",
+		},
+		[4]=
+		{
+			index = 4,
+			task_level = 3,
+			type = 0,
+			task_name = "累计消耗60万",
+			image = "ty_icon_jb_15y",
+			text = "1500金币",
+		},
+		[5]=
+		{
+			index = 5,
+			task_level = 4,
+			type = 0,
+			task_name = "累计消耗100万",
+			image = "ty_icon_flq1",
+			text = "2福利券",
+		},
+		[6]=
+		{
+			index = 6,
+			task_level = 2,
+			type = 1,
+			task_name = "累计充值48元",
+			image = "ty_icon_flq3",
+			text = "180福利券",
+		},
+		[7]=
+		{
+			index = 7,
+			task_level = 5,
+			type = 0,
+			task_name = "累计消耗150万",
+			image = "ty_icon_flq2",
+			text = "3福利券",
+		},
+		[8]=
+		{
+			index = 8,
+			task_level = 6,
+			type = 0,
+			task_name = "累计消耗300万",
+			image = "ty_icon_flq2",
+			text = "7福利券",
+		},
+		[9]=
+		{
+			index = 9,
+			task_level = 7,
+			type = 0,
+			task_name = "累计消耗600万",
+			image = "ty_icon_flq3",
+			text = "15福利券",
+		},
+		[10]=
+		{
+			index = 10,
+			task_level = 3,
+			type = 1,
+			task_name = "累计充值198元",
+			image = "ty_icon_flq3",
+			text = "700福利券",
+		},
+		[11]=
+		{
+			index = 11,
+			task_level = 8,
+			type = 0,
+			task_name = "累计消耗1200万",
+			image = "ty_icon_flq3",
+			text = "30福利券",
+		},
+		[12]=
+		{
+			index = 12,
+			task_level = 9,
+			type = 0,
+			task_name = "累计消耗2000万",
+			image = "ty_icon_flq3",
+			text = "40福利券",
+		},
+		[13]=
+		{
+			index = 13,
+			task_level = 4,
+			type = 1,
+			task_name = "累计充值500元",
+			image = "ty_icon_flq3",
+			text = "1500福利券",
+		},
+		[14]=
+		{
+			index = 14,
+			task_level = 10,
+			type = 0,
+			task_name = "累计消耗4000万",
+			image = "ty_icon_flq3",
+			text = "100福利券",
+		},
+		[15]=
+		{
+			index = 15,
+			task_level = 11,
+			type = 0,
+			task_name = "累计消耗8000万",
+			image = "ty_icon_flq4",
+			text = "200福利券",
+		},
+		[16]=
+		{
+			index = 16,
+			task_level = 12,
+			type = 0,
+			task_name = "累计消耗1.5亿",
+			image = "ty_icon_flq4",
+			text = "350福利券",
+		},
+		[17]=
+		{
+			index = 17,
+			task_level = 5,
+			type = 1,
+			task_name = "累计充值2498元",
+			image = "ty_icon_flq4",
+			text = "10000福利券",
+		},
+		[18]=
+		{
+			index = 18,
+			task_level = 13,
+			type = 0,
+			task_name = "累计消耗5亿",
+			image = "ty_icon_flq4",
+			text = "1750福利券",
+		},
+		[19]=
+		{
+			index = 19,
+			task_level = 14,
+			type = 0,
+			task_name = "累计消耗10亿",
+			image = "ty_icon_flq5",
+			text = "2500福利券",
+		},
+		[20]=
+		{
+			index = 20,
+			task_level = 15,
+			type = 0,
+			task_name = "累计消耗30亿",
+			image = "ty_icon_flq5",
+			text = "10000福利券",
+		},
+	},
+	other_data=
+	{
+		[1]=
+		{
+			line = 1,
+			cz_task_id = 1000539,
+			taskid = 1000540,
+			end_time = 1619452799,
+			sta_time = 1618876800,
+			act_time = """",
+			cur_path = "act_027_style_002",
+			condiy_key = "platform_limit_notcjj_show",
+			btn_gotoui = "game_Fishing3DHall",
+		},
+		[2]=
+		{
+			line = 2,
+			cz_task_id = 1000539,
+			taskid = 1000540,
+			end_time = 1619452799,
+			sta_time = 1618876800,
+			act_time = """",
+			cur_path = "act_027_style_002",
+			condiy_key = "platform_limit_cjj_show",
+			btn_gotoui = "game_MiniGame",
+		},
+	},
+	help_Info=
+	{
+		[1]=
+		{
+			line = 1,
+			content = "1.活动时间：4月20日8:00-4月26日23:59:59。",
+		},
+		[2]=
+		{
+			line = 2,
+			content = "2.所有任务奖励必须先领取前一个任务的奖励后才能领取。",
+		},
+		[3]=
+		{
+			line = 3,
+			content = "4.累计充值任务只记录游戏商城中的充值，不包含带有“推荐必买”和“活动”标签的商品。",
+		},
+		[4]=
+		{
+			line = 4,
+			content = "4.苹果大战的数据不计入活动统计。",
+		},
+		[5]=
+		{
+			line = 5,
+			content = "5.每日0点重置所有任务，未领取的奖励视为自动放弃",
+		},
+	},
+}
