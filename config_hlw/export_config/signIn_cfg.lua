6e8b9c1ada0cc5330e2bb616a4b6e035c3404b7b
duchao <duchao@qq.com>
2021-04-25 15:14:16 +0800

    提交

---
+++ config_hlw/export_config/signIn_cfg.lua
@@ -0,0 +1,102 @@
+return {
+	week=
+	{
+		[1]=
+		{
+			id = 1,
+			img = "pay_icon_gold2",
+			info = "金币x1000",
+			vip = 1,
+			vipinfo = "金币x2000",
+			vip_desc = "V1双倍",
+		},
+		[2]=
+		{
+			id = 2,
+			img = "gy_61_25",
+			info = "锁定x3",
+		},
+		[3]=
+		{
+			id = 3,
+			img = "pay_icon_gold3",
+			info = "金币x1500",
+			vip = 2,
+			vipinfo = "金币x3000",
+			vip_desc = "V2三倍",
+		},
+		[4]=
+		{
+			id = 4,
+			img = "com_award_icon_yb1",
+			info = "鱼币x2000",
+		},
+		[5]=
+		{
+			id = 5,
+			img = "3dby_btn_zh",
+			info = "召唤*3",
+		},
+		[6]=
+		{
+			id = 6,
+			img = "pay_icon_gold4",
+			info = "金币x2000",
+			vip = 3,
+			vipinfo = "金币x6000",
+			vip_desc = "V3三倍",
+		},
+		[7]=
+		{
+			id = 7,
+			img = "pay_icon_gold8",
+			info = "金币x3000",
+			vip = 5,
+			vipinfo = "金币x9000",
+			vip_desc = "V5三倍",
+		},
+	},
+	month=
+	{
+		[1]=
+		{
+			line = 1,
+			id = 1,
+			img = "ty_icon_flq2",
+			info = "0.1福利券",
+			day = 3,
+		},
+		[2]=
+		{
+			line = 2,
+			id = 2,
+			img = "ty_icon_jb_50y",
+			info = "金币x2000",
+			day = 7,
+		},
+		[3]=
+		{
+			line = 3,
+			id = 3,
+			img = "ty_icon_flq3",
+			info = "0.3福利券",
+			day = 14,
+		},
+		[4]=
+		{
+			line = 4,
+			id = 4,
+			img = "ty_icon_jb_198y",
+			info = "金币x5000",
+			day = 21,
+		},
+		[5]=
+		{
+			line = 5,
+			id = 5,
+			img = "ty_icon_flq3",
+			info = "1福利券",
+			day = "M",
+		},
+	},
+}
