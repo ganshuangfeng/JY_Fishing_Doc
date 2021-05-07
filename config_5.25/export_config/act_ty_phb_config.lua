e3c8548189dadb2c7d3fbf5d34136b0acbae68f1
yanghao <yanghao@qq.com>
2021-05-07 17:52:35 +0800

    同步lua

---
+++ config_5.25/export_config/act_ty_phb_config.lua
@@ -0,0 +1,122 @@
+return {
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			is_on_off = 1,
+			ID = 1,
+			item_key = "prop_grade",
+			reward_item_key = "prop_web_chip_huafei",
+			rank_type = "hlwyt_016_fqdr_rank",
+			s_time = 1621296000,
+			e_time = 1621871999,
+			award = {1,2,3,4,5,6,7},
+			extra_award = {8,9,10,11,12,13,14},
+			path = "act_015_dlphb",
+			condiy_key = "actp_rank_hlwyt_016_fqdr_rank",
+			type_info = 1,
+		},
+	},
+	award_config=
+	{
+		[1]=
+		{
+			ID = 1,
+			award = 40000,
+			limit = {1,1,},
+		},
+		[2]=
+		{
+			ID = 2,
+			award = 15000,
+			limit = {2,2},
+		},
+		[3]=
+		{
+			ID = 3,
+			award = 5000,
+			limit = {3,3,},
+		},
+		[4]=
+		{
+			ID = 4,
+			award = 2200,
+			limit = {4,6,},
+		},
+		[5]=
+		{
+			ID = 5,
+			award = 1200,
+			limit = {7,10,},
+		},
+		[6]=
+		{
+			ID = 6,
+			award = 600,
+			limit = {11,15,},
+		},
+		[7]=
+		{
+			ID = 7,
+			award = 300,
+			limit = {16,20,},
+		},
+		[8]=
+		{
+			ID = 8,
+			award = 60000,
+			limit = {1,1,},
+			need_num = 100000,
+			condition = "福气达到10万",
+		},
+		[9]=
+		{
+			ID = 9,
+			award = 15000,
+			limit = {2,2},
+			need_num = 60000,
+			condition = "福气达到6万",
+		},
+		[10]=
+		{
+			ID = 10,
+			award = 5000,
+			limit = {3,3,},
+			need_num = 30000,
+			condition = "福气达到3万",
+		},
+		[11]=
+		{
+			ID = 11,
+			award = 800,
+			limit = {4,6,},
+			need_num = 15000,
+			condition = "福气达到15000",
+		},
+		[12]=
+		{
+			ID = 12,
+			award = 800,
+			limit = {7,10,},
+			need_num = 10000,
+			condition = "福气达到1万",
+		},
+		[13]=
+		{
+			ID = 13,
+			award = 400,
+			limit = {11,15,},
+			need_num = 6000,
+			condition = "福气达到6000",
+		},
+		[14]=
+		{
+			ID = 14,
+			award = 200,
+			limit = {16,20,},
+			need_num = 3000,
+			condition = "福气达到3000",
+		},
+	},
+}
