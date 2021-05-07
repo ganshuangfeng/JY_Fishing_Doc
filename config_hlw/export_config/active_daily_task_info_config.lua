c1e1f43d7fef2af108174b15ca75787c3d6d4163
duchao <duchao@qq.com>
2021-05-07 16:44:05 +0800

    调整奖励

--- config_hlw/export_config/active_daily_task_info_config.lua
+++ config_hlw/export_config/active_daily_task_info_config.lua
@@ -48,8 +48,8 @@ return {
 			task_icon = "rw_icon_bh",
 			task_instruction = "3D捕鱼中捕获任意100条鱼",
 			task_award_icon = {"ty_icon_pms_1","ty_icon_hytb",},
-			task_award_instruction = {"250福利券赛门票","活跃度"},
-			task_award_count = {"x1","x20"},
+			task_award_instruction = {"金币","活跃度"},
+			task_award_count = {"x1000","x20"},
 			gotoui = {"game_Fishing3DHall",},
 		},
 		[4]=
