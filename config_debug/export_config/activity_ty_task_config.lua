58c888ccc28a2bbad531e1eccdd9e29ab885db54
denglu <denglu@qq.com>
2021-05-11 11:05:41 +0800

    修改显示

--- config_debug/export_config/activity_ty_task_config.lua
+++ config_debug/export_config/activity_ty_task_config.lua
@@ -157,8 +157,8 @@ return {
 		{
 			id = 5,
 			task = 1000567,
-			total = 1,
-			task_name = "在西游消消乐中消除1次孙悟空",
+			total = 3,
+			task_name = "在西游消消乐中消除3个孙悟空",
 			level = 1,
 			item = {"jing_bi","prop_fish_drop_act_0"},
 			count = {10000,10},
@@ -234,8 +234,8 @@ return {
 		{
 			id = 12,
 			task = 1000574,
-			total = 2,
-			task_name = "在西游消消乐中消除2次孙悟空",
+			total = 6,
+			task_name = "在西游消消乐中消除6个孙悟空",
 			level = 1,
 			item = {"jing_bi",},
 			count = {20000,},
