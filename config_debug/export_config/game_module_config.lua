8bdfe6057a98d6b61ac8410567194dc46c93e449
yanghao <yanghao@qq.com>
2021-05-12 10:32:28 +0800

    关闭五星

--- config_debug/export_config/game_module_config.lua
+++ config_debug/export_config/game_module_config.lua
@@ -3101,7 +3101,7 @@ return {
 		{
 			id = 316,
 			key = "act_ty_sjb_style/act_014_dlphb",
-			desc = "掉落排行榜皮肤（铁锤）",
+			desc = "掉落排行榜皮肤（财力）",
 			is_on_off = 1,
 			enable = 1,
 			state = 1,
@@ -3110,19 +3110,19 @@ return {
 		{
 			id = 317,
 			key = "act_ty_by_drop_style/act_015_gongju",
-			desc = "活动boss皮肤（铁锤）",
-			is_on_off = 1,
-			enable = 1,
-			state = 1,
+			desc = "活动boss皮肤（五星）",
+			is_on_off = 0,
+			enable = 0,
+			state = 0,
 		},
 		[318]=
 		{
 			id = 318,
 			key = "act_ty_by_hhl_style/act_010_hhl",
 			desc = "换好礼皮肤",
-			is_on_off = 1,
-			enable = 1,
-			state = 1,
+			is_on_off = 0,
+			enable = 0,
+			state = 0,
 		},
 		[319]=
 		{
--- config_debug/export_config/game_module_config_cjj.lua
+++ config_debug/export_config/game_module_config_cjj.lua
@@ -3051,7 +3051,7 @@ return {
 		{
 			id = 311,
 			key = "act_ty_sjb_style/act_014_dlphb",
-			desc = "掉落排行榜皮肤（铁锤）",
+			desc = "掉落排行榜皮肤（财力）",
 			is_on_off = 1,
 			enable = 1,
 			state = 1,
@@ -3060,19 +3060,19 @@ return {
 		{
 			id = 312,
 			key = "act_ty_by_drop_style/act_015_gongju",
-			desc = "活动boss皮肤（铁锤）",
-			is_on_off = 1,
-			enable = 1,
-			state = 1,
+			desc = "活动boss皮肤（五星）",
+			is_on_off = 0,
+			enable = 0,
+			state = 0,
 		},
 		[313]=
 		{
 			id = 313,
 			key = "act_ty_by_hhl_style/act_010_hhl",
 			desc = "换好礼皮肤",
-			is_on_off = 1,
-			enable = 1,
-			state = 1,
+			is_on_off = 0,
+			enable = 0,
+			state = 0,
 		},
 		[314]=
 		{
