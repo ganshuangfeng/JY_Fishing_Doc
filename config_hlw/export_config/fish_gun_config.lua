6c55a6155499708e8ff1f84cb9253b1b479b861e
duchao <duchao@qq.com>
2021-05-13 10:56:41 +0800

    tijaio

---
+++ config_hlw/export_config/fish_gun_config.lua
@@ -0,0 +1,503 @@
+return {
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			index = 1,
+			gun_rate = 10,
+			gun_icon = "bygame_icon_pao1",
+			laser_max_rate = 900,
+		},
+		[2]=
+		{
+			line = 2,
+			index = 2,
+			gun_rate = 20,
+			gun_icon = "bygame_icon_pao2",
+			laser_max_rate = 1800,
+		},
+		[3]=
+		{
+			line = 3,
+			index = 3,
+			gun_rate = 30,
+			gun_icon = "bygame_icon_pao3",
+			laser_max_rate = 2700,
+		},
+		[4]=
+		{
+			line = 4,
+			index = 4,
+			gun_rate = 40,
+			gun_icon = "bygame_icon_pao4",
+			laser_max_rate = 3600,
+		},
+		[5]=
+		{
+			line = 5,
+			index = 5,
+			gun_rate = 50,
+			gun_icon = "bygame_icon_pao5",
+			laser_max_rate = 4500,
+		},
+		[6]=
+		{
+			line = 6,
+			index = 6,
+			gun_rate = 60,
+			gun_icon = "bygame_icon_pao6",
+			laser_max_rate = 5400,
+		},
+		[7]=
+		{
+			line = 7,
+			index = 7,
+			gun_rate = 70,
+			gun_icon = "bygame_icon_pao7",
+			laser_max_rate = 6300,
+		},
+		[8]=
+		{
+			line = 8,
+			index = 8,
+			gun_rate = 80,
+			gun_icon = "bygame_icon_pao8",
+			laser_max_rate = 7200,
+		},
+		[9]=
+		{
+			line = 9,
+			index = 9,
+			gun_rate = 90,
+			gun_icon = "bygame_icon_pao9",
+			laser_max_rate = 8100,
+		},
+		[10]=
+		{
+			line = 10,
+			index = 10,
+			gun_rate = 100,
+			gun_icon = "bygame_icon_pao10",
+			laser_max_rate = 9000,
+		},
+		[11]=
+		{
+			line = 11,
+			index = 11,
+			gun_rate = 100,
+			gun_icon = "bygame_icon_pao1",
+			laser_max_rate = 9000,
+		},
+		[12]=
+		{
+			line = 12,
+			index = 12,
+			gun_rate = 200,
+			gun_icon = "bygame_icon_pao2",
+			laser_max_rate = 18000,
+		},
+		[13]=
+		{
+			line = 13,
+			index = 13,
+			gun_rate = 300,
+			gun_icon = "bygame_icon_pao3",
+			laser_max_rate = 27000,
+		},
+		[14]=
+		{
+			line = 14,
+			index = 14,
+			gun_rate = 400,
+			gun_icon = "bygame_icon_pao4",
+			laser_max_rate = 36000,
+		},
+		[15]=
+		{
+			line = 15,
+			index = 15,
+			gun_rate = 500,
+			gun_icon = "bygame_icon_pao5",
+			laser_max_rate = 45000,
+		},
+		[16]=
+		{
+			line = 16,
+			index = 16,
+			gun_rate = 600,
+			gun_icon = "bygame_icon_pao6",
+			laser_max_rate = 54000,
+		},
+		[17]=
+		{
+			line = 17,
+			index = 17,
+			gun_rate = 700,
+			gun_icon = "bygame_icon_pao7",
+			laser_max_rate = 63000,
+		},
+		[18]=
+		{
+			line = 18,
+			index = 18,
+			gun_rate = 800,
+			gun_icon = "bygame_icon_pao8",
+			laser_max_rate = 72000,
+		},
+		[19]=
+		{
+			line = 19,
+			index = 19,
+			gun_rate = 900,
+			gun_icon = "bygame_icon_pao9",
+			laser_max_rate = 81000,
+		},
+		[20]=
+		{
+			line = 20,
+			index = 20,
+			gun_rate = 1000,
+			gun_icon = "bygame_icon_pao10",
+			laser_max_rate = 90000,
+		},
+		[21]=
+		{
+			line = 21,
+			index = 21,
+			gun_rate = 1000,
+			gun_icon = "bygame_icon_pao1",
+			laser_max_rate = 90000,
+		},
+		[22]=
+		{
+			line = 22,
+			index = 22,
+			gun_rate = 2000,
+			gun_icon = "bygame_icon_pao2",
+			laser_max_rate = 180000,
+		},
+		[23]=
+		{
+			line = 23,
+			index = 23,
+			gun_rate = 3000,
+			gun_icon = "bygame_icon_pao3",
+			laser_max_rate = 270000,
+		},
+		[24]=
+		{
+			line = 24,
+			index = 24,
+			gun_rate = 4000,
+			gun_icon = "bygame_icon_pao4",
+			laser_max_rate = 360000,
+		},
+		[25]=
+		{
+			line = 25,
+			index = 25,
+			gun_rate = 5000,
+			gun_icon = "bygame_icon_pao5",
+			laser_max_rate = 450000,
+		},
+		[26]=
+		{
+			line = 26,
+			index = 26,
+			gun_rate = 6000,
+			gun_icon = "bygame_icon_pao6",
+			laser_max_rate = 540000,
+		},
+		[27]=
+		{
+			line = 27,
+			index = 27,
+			gun_rate = 7000,
+			gun_icon = "bygame_icon_pao7",
+			laser_max_rate = 630000,
+		},
+		[28]=
+		{
+			line = 28,
+			index = 28,
+			gun_rate = 8000,
+			gun_icon = "bygame_icon_pao8",
+			laser_max_rate = 720000,
+		},
+		[29]=
+		{
+			line = 29,
+			index = 29,
+			gun_rate = 9000,
+			gun_icon = "bygame_icon_pao9",
+			laser_max_rate = 810000,
+		},
+		[30]=
+		{
+			line = 30,
+			index = 30,
+			gun_rate = 10000,
+			gun_icon = "bygame_icon_pao10",
+			laser_max_rate = 900000,
+		},
+		[31]=
+		{
+			line = 31,
+			index = 31,
+			gun_rate = 10000,
+			gun_icon = "bygame_icon_pao1",
+			laser_max_rate = 900000,
+		},
+		[32]=
+		{
+			line = 32,
+			index = 32,
+			gun_rate = 20000,
+			gun_icon = "bygame_icon_pao2",
+			laser_max_rate = 1800000,
+		},
+		[33]=
+		{
+			line = 33,
+			index = 33,
+			gun_rate = 30000,
+			gun_icon = "bygame_icon_pao3",
+			laser_max_rate = 2700000,
+		},
+		[34]=
+		{
+			line = 34,
+			index = 34,
+			gun_rate = 40000,
+			gun_icon = "bygame_icon_pao4",
+			laser_max_rate = 3600000,
+		},
+		[35]=
+		{
+			line = 35,
+			index = 35,
+			gun_rate = 50000,
+			gun_icon = "bygame_icon_pao5",
+			laser_max_rate = 4500000,
+		},
+		[36]=
+		{
+			line = 36,
+			index = 36,
+			gun_rate = 60000,
+			gun_icon = "bygame_icon_pao6",
+			laser_max_rate = 5400000,
+		},
+		[37]=
+		{
+			line = 37,
+			index = 37,
+			gun_rate = 70000,
+			gun_icon = "bygame_icon_pao7",
+			laser_max_rate = 6300000,
+		},
+		[38]=
+		{
+			line = 38,
+			index = 38,
+			gun_rate = 80000,
+			gun_icon = "bygame_icon_pao8",
+			laser_max_rate = 7200000,
+		},
+		[39]=
+		{
+			line = 39,
+			index = 39,
+			gun_rate = 90000,
+			gun_icon = "bygame_icon_pao9",
+			laser_max_rate = 8100000,
+		},
+		[40]=
+		{
+			line = 40,
+			index = 40,
+			gun_rate = 100000,
+			gun_icon = "bygame_icon_pao10",
+			laser_max_rate = 9000000,
+		},
+		[41]=
+		{
+			line = 41,
+			index = 41,
+			gun_rate = 10,
+			gun_icon = "bygame_icon_pao12",
+			bullet_prefab = "BulletPrefab_z_1",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 6.250000,
+			bullet_num_limit = 10,
+			gun_level = 1,
+		},
+		[42]=
+		{
+			line = 42,
+			index = 42,
+			gun_rate = 50,
+			gun_icon = "bygame_icon_pao13",
+			bullet_prefab = "BulletPrefab_z_2",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 6.250000,
+			bullet_num_limit = 10,
+			gun_level = 2,
+		},
+		[43]=
+		{
+			line = 43,
+			index = 43,
+			gun_rate = 100,
+			gun_icon = "bygame_icon_pao14",
+			bullet_prefab = "BulletPrefab_z_3",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 6.250000,
+			bullet_num_limit = 10,
+			gun_level = 3,
+		},
+		[44]=
+		{
+			line = 44,
+			index = 44,
+			gun_rate = 200,
+			gun_icon = "bygame_icon_pao15",
+			bullet_prefab = "BulletPrefab_z_4",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 6.250000,
+			bullet_num_limit = 10,
+			gun_level = 4,
+		},
+		[45]=
+		{
+			line = 45,
+			index = 45,
+			gun_rate = 10,
+			gun_icon = "bygame_icon_pao1",
+			net_prefab = "FishNetPrefab_f_2",
+			bullet_prefab = "BulletPrefab_f_1",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 3.125000,
+			bullet_num_limit = 10,
+			gun_level = 1,
+		},
+		[46]=
+		{
+			line = 46,
+			index = 46,
+			gun_rate = 20,
+			gun_icon = "bygame_icon_pao2",
+			net_prefab = "FishNetPrefab_f_2",
+			bullet_prefab = "BulletPrefab_f_2",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 3.125000,
+			bullet_num_limit = 10,
+			gun_level = 2,
+		},
+		[47]=
+		{
+			line = 47,
+			index = 47,
+			gun_rate = 30,
+			gun_icon = "bygame_icon_pao3",
+			net_prefab = "FishNetPrefab_f_2",
+			bullet_prefab = "BulletPrefab_f_3",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 3.125000,
+			bullet_num_limit = 10,
+			gun_level = 3,
+		},
+		[48]=
+		{
+			line = 48,
+			index = 48,
+			gun_rate = 60,
+			gun_icon = "bygame_icon_pao4",
+			net_prefab = "FishNetPrefab_f_2",
+			bullet_prefab = "BulletPrefab_f_1",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 3.125000,
+			bullet_num_limit = 10,
+			gun_level = 1,
+		},
+		[49]=
+		{
+			line = 49,
+			index = 49,
+			gun_rate = 50,
+			gun_icon = "bygame_icon_pao5",
+			net_prefab = "FishNetPrefab_f_2",
+			bullet_prefab = "BulletPrefab_f_2",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 3.125000,
+			bullet_num_limit = 10,
+			gun_level = 2,
+		},
+		[50]=
+		{
+			line = 50,
+			index = 50,
+			gun_rate = 150,
+			gun_icon = "bygame_icon_pao6",
+			net_prefab = "FishNetPrefab_f_2",
+			bullet_prefab = "BulletPrefab_f_3",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 3.125000,
+			bullet_num_limit = 10,
+			gun_level = 3,
+		},
+		[51]=
+		{
+			line = 51,
+			index = 51,
+			gun_rate = 200,
+			gun_icon = "bygame_icon_pao7",
+			net_prefab = "FishNetPrefab_f_2",
+			bullet_prefab = "BulletPrefab_f_1",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 3.125000,
+			bullet_num_limit = 10,
+			gun_level = 1,
+		},
+		[52]=
+		{
+			line = 52,
+			index = 52,
+			gun_rate = 150,
+			gun_icon = "bygame_icon_pao7",
+			net_prefab = "FishNetPrefab_f_2",
+			bullet_prefab = "BulletPrefab_f_2",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 3.125000,
+			bullet_num_limit = 10,
+			gun_level = 2,
+		},
+		[53]=
+		{
+			line = 53,
+			index = 53,
+			gun_rate = 350,
+			gun_icon = "bygame_icon_pao8",
+			net_prefab = "FishNetPrefab_f_2",
+			bullet_prefab = "BulletPrefab_f_3",
+			laser_max_rate = 900,
+			bullet_move_speed = 20,
+			fire_speed = 3.125000,
+			bullet_num_limit = 10,
+			gun_level = 3,
+		},
+	},
+}
