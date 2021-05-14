5a9faab854acffe632d6deb5d2113d11e812b4aa
yanghao <yanghao@qq.com>
2021-05-14 15:57:52 +0800

    祈福好礼

---
+++ config_5.18/export_config/activity_052_qfhl_config.lua
@@ -0,0 +1,125 @@
+return {
+	Award1=
+	{
+		[1]=
+		{
+			id = 1,
+			img = "activity_icon_gift270_sqm",
+			real = 1,
+			text = "华为手机",
+			server_award_id = 12032,
+		},
+		[2]=
+		{
+			id = 2,
+			img = "activity_icon_gift52",
+			real = 1,
+			text = "运动手环",
+			server_award_id = 12033,
+		},
+		[3]=
+		{
+			id = 3,
+			img = "activity_icon_gift272_sqm",
+			real = 1,
+			text = "可乐12瓶",
+			server_award_id = 12034,
+		},
+		[4]=
+		{
+			id = 4,
+			img = "activity_icon_gift271_sqm",
+			real = 1,
+			text = "猴菇饼干",
+			server_award_id = 12035,
+		},
+		[5]=
+		{
+			id = 5,
+			img = "activity_icon_gift273_sqm",
+			real = 1,
+			text = "卷纸16卷",
+			server_award_id = 12036,
+		},
+		[6]=
+		{
+			id = 6,
+			img = "ty_icon_flq2",
+			asset_type = "shop_gold_sum",
+			text = "800福利券",
+			server_award_id = 12037,
+		},
+		[7]=
+		{
+			id = 7,
+			img = "ty_icon_jb_15y",
+			asset_type = "jing_bi",
+			text = "100万金币",
+			server_award_id = 12038,
+		},
+		[8]=
+		{
+			id = 8,
+			img = "ty_icon_jb_15y",
+			asset_type = "jing_bi",
+			text = "50万金币",
+			server_award_id = 12039,
+		},
+		[9]=
+		{
+			id = 9,
+			img = "ty_icon_flq2",
+			asset_type = "shop_gold_sum",
+			text = "100福利券",
+			server_award_id = 12040,
+		},
+		[10]=
+		{
+			id = 10,
+			img = "ty_icon_jb_15y",
+			asset_type = "jing_bi",
+			text = "10万金币",
+			server_award_id = 12041,
+		},
+		[11]=
+		{
+			id = 11,
+			img = "com_award_icon_hfsp",
+			asset_type = "prop_web_chip_huafei",
+			text = "100话费碎片",
+			server_award_id = 12042,
+		},
+		[12]=
+		{
+			id = 12,
+			img = "ty_icon_yxb_2",
+			asset_type = "prop_tiny_game_coin",
+			text = "8万小游戏币",
+			server_award_id = 12043,
+		},
+		[13]=
+		{
+			id = 13,
+			img = "ty_icon_flq2",
+			asset_type = "shop_gold_sum",
+			text = "30福利券",
+			server_award_id = 12044,
+		},
+		[14]=
+		{
+			id = 14,
+			img = "com_award_icon_hfsp",
+			asset_type = "prop_web_chip_huafei",
+			text = "15话费碎片",
+			server_award_id = 12045,
+		},
+		[15]=
+		{
+			id = 15,
+			img = "ty_icon_yxb_2",
+			asset_type = "prop_tiny_game_coin",
+			text = "30000小游戏币",
+			server_award_id = 12046,
+		},
+	},
+}
