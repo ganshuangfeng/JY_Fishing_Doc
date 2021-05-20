768d1071d98dad16510e850bc2990d11ee8ed2aa
duchao <duchao@qq.com>
2021-05-20 20:58:19 +0800

    提交

---
+++ config_hlw/export_config/sys_binding_phone.lua
@@ -0,0 +1,26 @@
+return {
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			id = 1,
+			icon = "ty_icon_jb_30y",
+			desc = "金币3000",
+		},
+		[2]=
+		{
+			line = 2,
+			id = 2,
+			icon = "ty_icon_yb_3",
+			desc = "鱼币1000",
+		},
+		[3]=
+		{
+			line = 3,
+			id = 3,
+			icon = "3dby_btn_sd",
+			desc = "锁定x1",
+		},
+	},
+}
