0c84f0381bf0d8463f88f1ac90856c988c74879d
duchao <duchao@qq.com>
2021-05-21 09:55:36 +0800

    提交

--- config_hlw/export_config/systaskmanagement_config.lua
+++ config_hlw/export_config/systaskmanagement_config.lua
@@ -20,7 +20,7 @@ return {
 		{
 			id = 1,
 			title = "金币场每日",
-			top_img = "rw_bg_img02_1",
+			top_img = "rw_bg_img02_5",
 			gotoUI = {"sys_task_management_type_1","panel1"},
 			is_on_off = 1,
 			beginTime = -1,
@@ -40,7 +40,7 @@ return {
 		{
 			id = 3,
 			title = "福卡场每日",
-			top_img = "rw_bg_img02_3",
+			top_img = "rw_bg_img02_4",
 			gotoUI = {"sys_task_management_type_1","panel3"},
 			is_on_off = 1,
 			beginTime = -1,
@@ -50,7 +50,7 @@ return {
 		{
 			id = 4,
 			title = "福卡场每月",
-			top_img = "rw_bg_img02_4",
+			top_img = "rw_bg_img02_3",
 			gotoUI = {"sys_task_management_type_1","panel4"},
 			is_on_off = 1,
 			beginTime = -1,
