a3cf92c49dd2ad3868555be56827eebfe8068329
yanghao <yanghao@qq.com>
2021-05-24 16:57:18 +0800

    祈福有礼

---
+++ config_6.8/export_config/activity_052_qfhl_config.lua
@@ -0,0 +1,185 @@
+return {
+	Award1=
+	{
+		[1]=
+		{
+			id = 1,
+			img = "activity_icon_mdkt",
+			real = 1,
+			text = "美的空调",
+			server_award_id = 12032,
+		},
+		[2]=
+		{
+			id = 2,
+			img = "activity_icon_pbdn",
+			real = 1,
+			text = "平板电脑",
+			server_award_id = 12033,
+		},
+		[3]=
+		{
+			id = 3,
+			img = "activity_icon_kl",
+			real = 1,
+			text = "可乐12瓶",
+			server_award_id = 12034,
+		},
+		[4]=
+		{
+			id = 4,
+			img = "activity_icon_qzs1",
+			real = 1,
+			text = "亲嘴烧30片",
+			server_award_id = 12035,
+		},
+		[5]=
+		{
+			id = 5,
+			img = "activity_icon_cj",
+			real = 1,
+			text = "抽纸五包",
+			server_award_id = 12036,
+		},
+		[6]=
+		{
+			id = 6,
+			img = "ty_icon_flq2",
+			asset_type = "shop_gold_sum",
+			text = "800福利券",
+			server_award_id = 12037,
+		},
+		[7]=
+		{
+			id = 7,
+			img = "ty_icon_jb_15y",
+			asset_type = "jing_bi",
+			text = "100万金币",
+			server_award_id = 12038,
+		},
+		[8]=
+		{
+			id = 8,
+			img = "ty_icon_jb_15y",
+			asset_type = "jing_bi",
+			text = "50万金币",
+			server_award_id = 12039,
+		},
+		[9]=
+		{
+			id = 9,
+			img = "ty_icon_flq2",
+			asset_type = "shop_gold_sum",
+			text = "100福利券",
+			server_award_id = 12040,
+		},
+		[10]=
+		{
+			id = 10,
+			img = "ty_icon_jb_15y",
+			asset_type = "jing_bi",
+			text = "10万金币",
+			server_award_id = 12041,
+		},
+		[11]=
+		{
+			id = 11,
+			img = "com_award_icon_hfsp",
+			asset_type = "prop_web_chip_huafei",
+			text = "100话费碎片",
+			server_award_id = 12042,
+		},
+		[12]=
+		{
+			id = 12,
+			img = "ty_icon_yxb_2",
+			asset_type = "prop_tiny_game_coin",
+			text = "8万小游戏币",
+			server_award_id = 12043,
+		},
+		[13]=
+		{
+			id = 13,
+			img = "ty_icon_flq2",
+			asset_type = "shop_gold_sum",
+			text = "30福利券",
+			server_award_id = 12044,
+		},
+		[14]=
+		{
+			id = 14,
+			img = "com_award_icon_hfsp",
+			asset_type = "prop_web_chip_huafei",
+			text = "15话费碎片",
+			server_award_id = 12045,
+		},
+		[15]=
+		{
+			id = 15,
+			img = "ty_icon_yxb_2",
+			asset_type = "prop_tiny_game_coin",
+			text = "30000小游戏币",
+			server_award_id = 12046,
+		},
+	},
+	Task=
+	{
+		[1]=
+		{
+			id = 1,
+			task_id = 1000655,
+		},
+		[2]=
+		{
+			id = 2,
+			task_id = 1000656,
+		},
+		[3]=
+		{
+			id = 3,
+			task_id = 1000657,
+		},
+	},
+	Box_exchange=
+	{
+		[1]=
+		{
+			id = 1,
+			box_exchange_id = 96,
+		},
+		[2]=
+		{
+			id = 2,
+			box_exchange_id = 97,
+		},
+		[3]=
+		{
+			id = 3,
+			box_exchange_id = 98,
+		},
+	},
+	Award_sw=
+	{
+		[1]=
+		{
+			id = 1,
+			image = "activity_icon_gift269_sqm",
+			tex = "金龙鱼油900ml",
+			offset = {0,20,50,100,200,400},
+		},
+		[2]=
+		{
+			id = 2,
+			image = "activity_icon_gift268_sqm",
+			tex = "椰汁24盒",
+			offset = {0,100,500,1000,1500,2200},
+		},
+		[3]=
+		{
+			id = 3,
+			image = "activity_icon_gift103_xlx",
+			tex = "美的电火锅",
+			offset = {0,100,500,1000,1500,3500},
+		},
+	},
+}
