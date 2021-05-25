2bb2fbc524bd8308d2443f8297d5ab7b98c6ec4b
yanghao <yanghao@qq.com>
2021-05-25 15:33:56 +0800

    超级夺宝前端

---
+++ config_6.8/export_config/act_cjdb_xybx config.lua
@@ -0,0 +1,188 @@
+return {
+	award_1=
+	{
+		[1]=
+		{
+			award_index = 1,
+			real_award_id = 12063,
+			award_name = "华为手机",
+			num = 1,
+			dec = "华为手机",
+			icon = "cjdb_cjsw_1",
+		},
+		[2]=
+		{
+			award_index = 2,
+			real_award_id = 12064,
+			asset_type = "jing_bi",
+			num = 100000000,
+			dec = "金币",
+			icon = "ty_icon_jb_98y",
+		},
+		[3]=
+		{
+			award_index = 3,
+			real_award_id = 12065,
+			asset_type = "shop_gold_sum",
+			num = 43000,
+			dec = "福利券",
+			icon = "ty_icon_flq3",
+		},
+		[4]=
+		{
+			award_index = 4,
+			real_award_id = 12066,
+			asset_type = "jing_bi",
+			num = 43000000,
+			dec = "金币",
+			icon = "ty_icon_jb_50y",
+		},
+		[5]=
+		{
+			award_index = 5,
+			real_award_id = 12067,
+			asset_type = "shop_gold_sum",
+			num = 1000000,
+			dec = "福利券",
+			icon = "ty_icon_flq4",
+		},
+		[6]=
+		{
+			award_index = 6,
+			real_award_id = 12068,
+			asset_type = "jing_bi",
+			num = 1000000000,
+			dec = "金币",
+			icon = "ty_icon_jb_198y",
+		},
+		[7]=
+		{
+			award_index = 7,
+			real_award_id = 12069,
+			asset_type = "shop_gold_sum",
+			num = 38000,
+			dec = "福利券",
+			icon = "ty_icon_flq3",
+		},
+		[8]=
+		{
+			award_index = 8,
+			real_award_id = 12070,
+			asset_type = "jing_bi",
+			num = 50000000,
+			dec = "金币",
+			icon = "ty_icon_jb_50y",
+		},
+		[9]=
+		{
+			award_index = 9,
+			real_award_id = 12071,
+			asset_type = "shop_gold_sum",
+			num = 4000000,
+			dec = "福利券",
+			icon = "ty_icon_flq5",
+		},
+		[10]=
+		{
+			award_index = 10,
+			real_award_id = 12072,
+			asset_type = "jing_bi",
+			num = 100000000,
+			dec = "金币",
+			icon = "ty_icon_jb_98y",
+		},
+	},
+	award_2=
+	{
+		[1]=
+		{
+			award_index = 1,
+			real_award_id = 12053,
+			award_name = "vivo手机",
+			num = 1,
+			dec = "vivo手机",
+			icon = "cjdb_cjsw_2",
+		},
+		[2]=
+		{
+			award_index = 2,
+			real_award_id = 12054,
+			asset_type = "jing_bi",
+			num = 10000000,
+			dec = "金币",
+			icon = "ty_icon_jb_50y",
+		},
+		[3]=
+		{
+			award_index = 3,
+			real_award_id = 12055,
+			asset_type = "shop_gold_sum",
+			num = 4300,
+			dec = "福利券",
+			icon = "ty_icon_flq3",
+		},
+		[4]=
+		{
+			award_index = 4,
+			real_award_id = 12056,
+			asset_type = "jing_bi",
+			num = 4300000,
+			dec = "金币",
+			icon = "ty_icon_jb_48y",
+		},
+		[5]=
+		{
+			award_index = 5,
+			real_award_id = 12057,
+			asset_type = "shop_gold_sum",
+			num = 100000,
+			dec = "福利券",
+			icon = "ty_icon_flq4",
+		},
+		[6]=
+		{
+			award_index = 6,
+			real_award_id = 12058,
+			asset_type = "jing_bi",
+			num = 100000000,
+			dec = "金币",
+			icon = "ty_icon_jb_98y",
+		},
+		[7]=
+		{
+			award_index = 7,
+			real_award_id = 12059,
+			asset_type = "shop_gold_sum",
+			num = 3800,
+			dec = "福利券",
+			icon = "ty_icon_flq3",
+		},
+		[8]=
+		{
+			award_index = 8,
+			real_award_id = 12060,
+			asset_type = "jing_bi",
+			num = 5000000,
+			dec = "金币",
+			icon = "ty_icon_jb_48y",
+		},
+		[9]=
+		{
+			award_index = 9,
+			real_award_id = 12061,
+			asset_type = "shop_gold_sum",
+			num = 400000,
+			dec = "福利券",
+			icon = "ty_icon_flq4",
+		},
+		[10]=
+		{
+			award_index = 10,
+			real_award_id = 12062,
+			asset_type = "jing_bi",
+			num = 10000000,
+			dec = "金币",
+			icon = "ty_icon_jb_50y",
+		},
+	},
+}
