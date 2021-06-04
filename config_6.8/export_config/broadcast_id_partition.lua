e22272a0cd030d308126b14b5aa4a13312c4fef6
yanghao <yanghao@qq.com>
2021-06-04 10:22:08 +0800

    6.8跑马灯

---
+++ config_6.8/export_config/broadcast_id_partition.lua
@@ -0,0 +1,111 @@
+return {
+	cpl_cjj=
+	{
+		award_pool_award=
+		{
+			id = "award_pool_award",
+			listen = 0,
+		},
+		award_pool_weekend_award=
+		{
+			id = "award_pool_weekend_award",
+			listen = 0,
+		},
+		fish_game_kill_boss=
+		{
+			id = "fish_game_kill_boss",
+			listen = 0,
+		},
+		fish_game_3d_kill_boss=
+		{
+			id = "fish_game_3d_kill_boss",
+			listen = 0,
+		},
+		fish_game_challenge_award=
+		{
+			id = "fish_game_challenge_award",
+			listen = 0,
+		},
+		fish_3d_game_challenge_award=
+		{
+			id = "fish_3d_game_challenge_award",
+			listen = 0,
+		},
+		luck_lottery_1=
+		{
+			id = "luck_lottery_1",
+			listen = 0,
+		},
+		luck_lottery_2=
+		{
+			id = "luck_lottery_2",
+			listen = 0,
+		},
+		buyu_task_get_award =
+		{
+			id = "buyu_task_get_award ",
+			listen = 0,
+		},
+		fish_game_3d_kill_boss_1=
+		{
+			id = "fish_game_3d_kill_boss_1",
+			listen = 0,
+		},
+		fish_game_3d_kill_boss_2=
+		{
+			id = "fish_game_3d_kill_boss_2",
+			listen = 0,
+		},
+		qiaoqiaole_award_big_award=
+		{
+			id = "qiaoqiaole_award_big_award",
+			listen = 0,
+		},
+		qiaoqiaole_award_caishen=
+		{
+			id = "qiaoqiaole_award_caishen",
+			listen = 0,
+		},
+		bullet_rank_match_award=
+		{
+			id = "bullet_rank_match_award",
+			listen = 0,
+		},
+	},
+	tthlby_all_pmd=
+	{
+		qiaoqiaole_cjj_award_big_award=
+		{
+			id = "qiaoqiaole_cjj_award_big_award",
+			listen = 0,
+		},
+		xiaoxiaole_sanguo_award_big_award=
+		{
+			id = "xiaoxiaole_sanguo_award_big_award",
+			listen = 0,
+		},
+		lwzb_award_big_award=
+		{
+			id = "lwzb_award_big_award",
+			listen = 0,
+		},
+	},
+	byam_all_pmd=
+	{
+		qiaoqiaole_cjj_award_big_award=
+		{
+			id = "qiaoqiaole_cjj_award_big_award",
+			listen = 0,
+		},
+		xiaoxiaole_sanguo_award_big_award=
+		{
+			id = "xiaoxiaole_sanguo_award_big_award",
+			listen = 0,
+		},
+		lwzb_award_big_award=
+		{
+			id = "lwzb_award_big_award",
+			listen = 0,
+		},
+	},
+}
---
+++ config_6.8/export_config/fake_broadcast_server.lua
@@ -0,0 +1,82 @@
+return {
+	main=
+	{
+		[1]=
+		{
+			id = 1,
+			period = {0,7200},
+			interval = {20,60},
+		},
+		[2]=
+		{
+			id = 2,
+			period = {7200,25200},
+			interval = {120,300},
+		},
+		[3]=
+		{
+			id = 3,
+			period = {25200,86400},
+			interval = {20,60},
+		},
+	},
+	config=
+	{
+		[1]=
+		{
+			id = 1,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "VIVO手机",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[2]=
+		{
+			id = 2,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "VIVO手机",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[3]=
+		{
+			id = 3,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "VIVO手机",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[4]=
+		{
+			id = 4,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "金龙鱼玉米油4L",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[5]=
+		{
+			id = 5,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "金龙鱼玉米油4L",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[6]=
+		{
+			id = 6,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "金龙鱼玉米油4L",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[7]=
+		{
+			id = 7,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "50元话费",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+	},
+}
