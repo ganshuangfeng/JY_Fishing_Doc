36318f85547fd51038dc6af4dce329548f1d40fc
yanghao <yanghao@qq.com>
2021-06-08 11:15:21 +0800

    关过期活动

--- config_aomi_6.8/export_config/game_module_config.lua
+++ config_aomi_6.8/export_config/game_module_config.lua
@@ -3305,9 +3305,9 @@ return {
 			id = 337,
 			key = "act_ty_zp1_style/act_ty_zp1_003",
 			desc = "通用转盘皮肤",
-			is_on_off = 1,
-			enable = 1,
-			state = 1,
+			is_on_off = 0,
+			enable = 0,
+			state = 0,
 		},
 		[338]=
 		{
