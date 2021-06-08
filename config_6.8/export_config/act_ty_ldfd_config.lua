84aa231dce784a99ae590f053109dd0d7fbed30c
yanghao <yanghao@qq.com>
2021-06-08 11:01:14 +0800

    6.8活动

---
+++ config_6.8/export_config/act_ty_ldfd_config.lua
@@ -0,0 +1,38 @@
+return {
+	info=
+	{
+		[1]=
+		{
+			line = 1,
+			key = "beginTime",
+			value = 1623110400,
+		},
+		[2]=
+		{
+			line = 2,
+			key = "endTime",
+			value = 1623686399,
+		},
+		[3]=
+		{
+			line = 3,
+			key = "show_condiy_key",
+		},
+		[4]=
+		{
+			line = 4,
+			key = "group_id",
+			value = 1,
+		},
+	},
+	gifts=
+	{
+		[1]=
+		{
+			line = 1,
+			group_id = 1,
+			task_id = 1000653,
+			gift_ids = {10571,10572,10573,10574,10575,10576,10577},
+		},
+	},
+}
