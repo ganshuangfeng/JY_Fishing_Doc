debdf2d7ec1c24e11cc9a144749d5cf0513fef31
yanghao <yanghao@qq.com>
2021-06-13 08:57:21 +0800

    同步

--- config_debug/export_config/act_042_xshb_config.lua
+++ config_debug/export_config/act_042_xshb_config.lua
@@ -4,10 +4,11 @@ return {
 		[1]=
 		{
 			index = 1,
-			award = "10元奖励金",
-			cost = 1000,
-			task_id = 1000659,
-			task_txt = "任意游戏累计消耗120万",
+			award = "0.5元奖励金",
+			cost = 50,
+			task_id = 1000664,
+			task_txt = "携带金币20万",
+			show_permission = "actp_own_task_p_show_not",
 			limit = "终身1次",
 			unlock_id = 6,
 			goto_ui = {"shop_bay","jing_bi",},
@@ -16,10 +17,10 @@ return {
 		[2]=
 		{
 			index = 2,
-			award = "5元奖励金",
-			cost = 500,
-			task_id = 1000660,
-			task_txt = "任意游戏累计消耗80万",
+			award = "3元奖励金",
+			cost = 300,
+			task_id = 1000663,
+			task_txt = "单笔充值≥6元",
 			limit = "终身3次",
 			unlock_id = 5,
 			unlock_permission = "xshb_042_deblocking_v0",
@@ -30,10 +31,10 @@ return {
 		[3]=
 		{
 			index = 3,
-			award = "3元奖励金",
-			cost = 300,
-			task_id = 1000661,
-			task_txt = "任意游戏累计消耗30万",
+			award = "1元奖励金",
+			cost = 100,
+			task_id = 1000662,
+			task_txt = "任意游戏累计消耗20万",
 			limit = "每天1次",
 			unlock_id = 4,
 			unlock_permission = "xshb_042_deblocking_v1",
@@ -44,10 +45,10 @@ return {
 		[4]=
 		{
 			index = 4,
-			award = "1元奖励金",
-			cost = 100,
-			task_id = 1000662,
-			task_txt = "任意游戏累计消耗20万",
+			award = "3元奖励金",
+			cost = 300,
+			task_id = 1000661,
+			task_txt = "任意游戏累计消耗30万",
 			limit = "每天1次",
 			unlock_id = 3,
 			unlock_permission = "xshb_042_deblocking_v2",
@@ -58,10 +59,10 @@ return {
 		[5]=
 		{
 			index = 5,
-			award = "3元奖励金",
-			cost = 300,
-			task_id = 1000663,
-			task_txt = "单笔充值≥6元",
+			award = "5元奖励金",
+			cost = 500,
+			task_id = 1000660,
+			task_txt = "任意游戏累计消耗80万",
 			limit = "每天1次",
 			unlock_id = 2,
 			unlock_permission = "xshb_042_deblocking_v3",
@@ -72,11 +73,10 @@ return {
 		[6]=
 		{
 			index = 6,
-			award = "0.5元奖励金",
-			cost = 50,
-			task_id = 1000664,
-			task_txt = "携带金币20万",
-			show_permission = "actp_own_task_p_show_not",
+			award = "10元奖励金",
+			cost = 1000,
+			task_id = 1000659,
+			task_txt = "任意游戏累计消耗120万",
 			limit = "每天1次",
 			unlock_id = 1,
 			unlock_permission = "xshb_042_deblocking_v4",
