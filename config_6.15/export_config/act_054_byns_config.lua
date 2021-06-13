2a3948fbd6a9c79dc2bece57e5233352551d79e6
yanghao <yanghao@qq.com>
2021-06-13 09:35:38 +0800

    捕鱼能手

---
+++ config_6.15/export_config/act_054_byns_config.lua
@@ -0,0 +1,104 @@
+return {
+	otherInfo=
+	{
+		[21783]=
+		{
+			task_id = 21783,
+			startTime = 1620689400,
+			endTime = 1621267199,
+			gotoui = "game_Fishing3DHall",
+			downDes = "每日0点重置积分和奖励\n1000及以上炮倍捕鱼可获得积分，获得积分数量=鱼的倍数",
+		},
+	},
+	awardInfo=
+	{
+		[1]=
+		{
+			index = 1,
+			need_num = 100,
+			award_name = "话费碎片",
+			award_amount = 5,
+			award_icon = "com_award_icon_hfsp",
+		},
+		[2]=
+		{
+			index = 2,
+			need_num = 500,
+			award_name = "鲸币",
+			award_amount = 1800,
+			award_icon = "pay_icon_gold3",
+		},
+		[3]=
+		{
+			index = 3,
+			need_num = 1000,
+			award_name = "福卡",
+			award_amount = 0.250000,
+			award_icon = "ad_2znjnk_icon_fk1",
+		},
+		[4]=
+		{
+			index = 4,
+			need_num = 2000,
+			award_name = "鱼币",
+			award_amount = 5000,
+			award_icon = "com_icon_yb",
+		},
+		[5]=
+		{
+			index = 5,
+			need_num = 5000,
+			award_name = "鲸币",
+			award_amount = "1万~3万",
+			award_icon = "pay_icon_gold4",
+		},
+		[6]=
+		{
+			index = 6,
+			need_num = 10000,
+			award_name = "鲸币",
+			award_amount = "2万~5万",
+			award_icon = "pay_icon_gold5",
+		},
+		[7]=
+		{
+			index = 7,
+			need_num = 30000,
+			award_name = "福卡",
+			award_amount = "8~15",
+			award_icon = "ad_2znjnk_icon_fk2",
+		},
+		[8]=
+		{
+			index = 8,
+			need_num = 80000,
+			award_name = "鱼币",
+			award_amount = "20万~50万",
+			award_icon = "com_award_icon_yb1",
+		},
+		[9]=
+		{
+			index = 9,
+			need_num = 150000,
+			award_name = "鲸币",
+			award_amount = "30万~80万",
+			award_icon = "pay_icon_gold6",
+		},
+		[10]=
+		{
+			index = 10,
+			need_num = 300000,
+			award_name = "福卡",
+			award_amount = "60~120",
+			award_icon = "ad_2znjnk_icon_fk3",
+		},
+		[11]=
+		{
+			index = 11,
+			need_num = 500000,
+			award_name = "福卡",
+			award_amount = "80~200",
+			award_icon = "ad_2znjnk_icon_fk4",
+		},
+	},
+}
