1d2928370c0192166e985a733393a7dac7961807
yanghao <yanghao@qq.com>
2021-06-13 13:58:32 +0800

    跑马灯

--- config_6.8/export_config/fake_broadcast_server.lua
+++ config_6.8/export_config/fake_broadcast_server.lua
@@ -78,5 +78,637 @@ return {
 			power = 116,
 			permission = "tthlby_all_pmd",
 		},
+		[8]=
+		{
+			id = 8,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "50元话费",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[9]=
+		{
+			id = 9,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "50元话费",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[10]=
+		{
+			id = 10,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "100元话费",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[11]=
+		{
+			id = 11,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "100元话费",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[12]=
+		{
+			id = 12,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "100元话费",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[13]=
+		{
+			id = 13,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "100元京东E卡",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[14]=
+		{
+			id = 14,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "100元京东E卡",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[15]=
+		{
+			id = 15,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "100元京东E卡",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[16]=
+		{
+			id = 16,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "200元京东E卡",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[17]=
+		{
+			id = 17,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "200元京东E卡",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[18]=
+		{
+			id = 18,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "200元京东E卡",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[19]=
+		{
+			id = 19,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "300元京东E卡",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[20]=
+		{
+			id = 20,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "300元京东E卡",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[21]=
+		{
+			id = 21,
+			content_type = "user_shoping_gold_pay",
+			fixed_award = "300元京东E卡",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[22]=
+		{
+			id = 22,
+			content_type = "luck_lottery_1",
+			fixed_award = "4750福利券",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[23]=
+		{
+			id = 23,
+			content_type = "luck_lottery_1",
+			fixed_award = "4750福利券",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[24]=
+		{
+			id = 24,
+			content_type = "luck_lottery_1",
+			fixed_award = "40万福利券",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[25]=
+		{
+			id = 25,
+			content_type = "luck_lottery_1",
+			fixed_award = "40万福利券",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[26]=
+		{
+			id = 26,
+			content_type = "luck_lottery_1",
+			fixed_award = "1亿金币",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[27]=
+		{
+			id = 27,
+			content_type = "luck_lottery_1",
+			fixed_award = "1亿金币",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[28]=
+		{
+			id = 28,
+			content_type = "luck_lottery_2",
+			fixed_award = "4750福利券",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[29]=
+		{
+			id = 29,
+			content_type = "luck_lottery_2",
+			fixed_award = "4750福利券",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[30]=
+		{
+			id = 30,
+			content_type = "luck_lottery_2",
+			fixed_award = "40万福利券",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[31]=
+		{
+			id = 31,
+			content_type = "luck_lottery_2",
+			fixed_award = "40万福利券",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[32]=
+		{
+			id = 32,
+			content_type = "luck_lottery_2",
+			fixed_award = "1亿金币",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[33]=
+		{
+			id = 33,
+			content_type = "luck_lottery_2",
+			fixed_award = "1亿金币",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[34]=
+		{
+			id = 34,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "1000万~5亿金币",
+			power = 2081,
+			permission = "tthlby_all_pmd",
+		},
+		[35]=
+		{
+			id = 35,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "1000万~5亿金币",
+			power = 2081,
+			permission = "byam_all_pmd",
+		},
+		[36]=
+		{
+			id = 36,
+			content_type = "guess_award_big_award",
+			random_award = "1000万~5亿金币",
+			power = 2081,
+			permission = "tthlby_all_pmd",
+		},
+		[37]=
+		{
+			id = 37,
+			content_type = "guess_award_big_award",
+			random_award = "1000万~5亿金币",
+			power = 2081,
+			permission = "byam_all_pmd",
+		},
+		[38]=
+		{
+			id = 38,
+			content_type = "xiaoxiaole_shuihu_award_big_award",
+			random_award = "1000万～3亿金币",
+			power = 520,
+			permission = "tthlby_all_pmd",
+		},
+		[39]=
+		{
+			id = 39,
+			content_type = "xiaoxiaole_shuihu_award_big_award",
+			random_award = "1000万～3亿金币",
+			power = 520,
+			permission = "byam_all_pmd",
+		},
+		[40]=
+		{
+			id = 40,
+			content_type = "xiaoxiaole_xiyou_award_big_award",
+			random_award = "1000万～3亿金币",
+			power = 520,
+			permission = "tthlby_all_pmd",
+		},
+		[41]=
+		{
+			id = 41,
+			content_type = "xiaoxiaole_xiyou_award_big_award",
+			random_award = "1000万～3亿金币",
+			power = 520,
+			permission = "byam_all_pmd",
+		},
+		[42]=
+		{
+			id = 42,
+			content_type = "tantanle_award_big_award",
+			random_award = "1000万~2亿金币",
+			power = 216,
+			permission = "tthlby_all_pmd",
+		},
+		[43]=
+		{
+			id = 43,
+			content_type = "tantanle_award_big_award",
+			random_award = "1000万~2亿金币",
+			power = 216,
+			permission = "byam_all_pmd",
+		},
+		[44]=
+		{
+			id = 44,
+			content_type = "qiaoqiaole_award_big_award",
+			fixed_award = "1200万金币",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[45]=
+		{
+			id = 45,
+			content_type = "qiaoqiaole_award_big_award",
+			fixed_award = "1200万金币",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[46]=
+		{
+			id = 46,
+			content_type = "qiaoqiaole_award_big_award",
+			fixed_award = "1800万金币",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[47]=
+		{
+			id = 47,
+			content_type = "qiaoqiaole_award_big_award",
+			fixed_award = "1800万金币",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[48]=
+		{
+			id = 48,
+			content_type = "qiaoqiaole_award_big_award",
+			fixed_award = "2400万金币",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[49]=
+		{
+			id = 49,
+			content_type = "qiaoqiaole_award_big_award",
+			fixed_award = "2400万金币",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[50]=
+		{
+			id = 50,
+			content_type = "qiaoqiaole_award_big_award",
+			fixed_award = "3800万金币",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[51]=
+		{
+			id = 51,
+			content_type = "qiaoqiaole_award_big_award",
+			fixed_award = "3800万金币",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[52]=
+		{
+			id = 52,
+			content_type = "qiaoqiaole_award_big_award",
+			fixed_award = "4200万金币",
+			power = 116,
+			permission = "tthlby_all_pmd",
+		},
+		[53]=
+		{
+			id = 53,
+			content_type = "qiaoqiaole_award_big_award",
+			fixed_award = "4200万金币",
+			power = 116,
+			permission = "byam_all_pmd",
+		},
+		[54]=
+		{
+			id = 54,
+			content_type = "qiaoqiaole_award_caishen",
+			fixed_award = "1000万金币",
+			power = 578,
+			permission = "tthlby_all_pmd",
+		},
+		[55]=
+		{
+			id = 55,
+			content_type = "qiaoqiaole_award_caishen",
+			fixed_award = "1000万金币",
+			power = 578,
+			permission = "byam_all_pmd",
+		},
+		[56]=
+		{
+			id = 56,
+			content_type = "jykp_big_award",
+			random_award = "6000万～8亿金币",
+			power = 421,
+			permission = "tthlby_all_pmd",
+		},
+		[57]=
+		{
+			id = 57,
+			content_type = "jykp_big_award",
+			random_award = "6000万～8亿金币",
+			power = 421,
+			permission = "byam_all_pmd",
+		},
+		[58]=
+		{
+			id = 58,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "1000万～2000万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[59]=
+		{
+			id = 59,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "2000万～2500万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[60]=
+		{
+			id = 60,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "2500万～3000万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[61]=
+		{
+			id = 61,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "3000万～3500万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[62]=
+		{
+			id = 62,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "3500万～4000万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[63]=
+		{
+			id = 63,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "4000万～4500万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[64]=
+		{
+			id = 64,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "4500万金币～5000万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[65]=
+		{
+			id = 65,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "5000万金币～1亿金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[66]=
+		{
+			id = 66,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "1亿金币～1.5亿金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[67]=
+		{
+			id = 67,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "1.5亿金币～2亿金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[68]=
+		{
+			id = 68,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "2亿金币～2.5亿金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[69]=
+		{
+			id = 69,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "2.5亿金币～3亿金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[70]=
+		{
+			id = 70,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "3亿金币～3.5亿金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[71]=
+		{
+			id = 71,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "3.5亿金币～4亿金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[72]=
+		{
+			id = 72,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "4亿金币～4.5亿金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[73]=
+		{
+			id = 73,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "4.5亿金币～5亿金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[74]=
+		{
+			id = 74,
+			content_type = "xiaoxiaole_award_big_award",
+			random_award = "5亿金币～5.5亿金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[75]=
+		{
+			id = 75,
+			content_type = "xiaoxiaole_shuihu_award_big_award",
+			random_award = "1000万~5亿金币",
+			power = 980,
+			permission = "cpl_cjj",
+		},
+		[76]=
+		{
+			id = 76,
+			content_type = "guess_award_big_award",
+			random_award = "1000万~5亿金币",
+			power = 1280,
+			permission = "cpl_cjj",
+		},
+		[77]=
+		{
+			id = 77,
+			content_type = "xiaoxiaole_sanguo_award_big_award",
+			random_award = "1000万～3亿金币",
+			power = 950,
+			permission = "cpl_cjj",
+		},
+		[78]=
+		{
+			id = 78,
+			content_type = "xiaoxiaole_xiyou_award_big_award",
+			random_award = "1000万～3亿金币",
+			power = 1081,
+			permission = "cpl_cjj",
+		},
+		[79]=
+		{
+			id = 79,
+			content_type = "lwzb_award_big_award",
+			random_award = "1000万~5亿金币",
+			power = 1016,
+			permission = "cpl_cjj",
+		},
+		[80]=
+		{
+			id = 80,
+			content_type = "qiaoqiaole_cjj_award_big_award",
+			fixed_award = "1200万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[81]=
+		{
+			id = 81,
+			content_type = "qiaoqiaole_cjj_award_big_award",
+			fixed_award = "1800万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[82]=
+		{
+			id = 82,
+			content_type = "qiaoqiaole_cjj_award_big_award",
+			fixed_award = "2400万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[83]=
+		{
+			id = 83,
+			content_type = "qiaoqiaole_cjj_award_big_award",
+			fixed_award = "3800万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[84]=
+		{
+			id = 84,
+			content_type = "qiaoqiaole_cjj_award_big_award",
+			fixed_award = "4200万金币",
+			power = 116,
+			permission = "cpl_cjj",
+		},
+		[85]=
+		{
+			id = 85,
+			content_type = "qiaoqiaole_award_caishen",
+			fixed_award = "1000万金币",
+			power = 573,
+			permission = "cpl_cjj",
+		},
+		[86]=
+		{
+			id = 86,
+			content_type = "jykp_big_award",
+			random_award = "6000万～8亿金币",
+			power = 421,
+			permission = "cpl_cjj",
+		},
 	},
 }
