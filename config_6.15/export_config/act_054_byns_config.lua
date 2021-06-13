5b1864d19f10a005837584b5e5189819b78912eb
yanghao <yanghao@qq.com>
2021-06-13 11:14:27 +0800

    捕鱼能手

--- config_6.15/export_config/act_054_byns_config.lua
+++ config_6.15/export_config/act_054_byns_config.lua
@@ -1,13 +1,14 @@
 return {
 	otherInfo=
 	{
-		[21783]=
+		[1]=
 		{
-			task_id = 21783,
-			startTime = 1620689400,
-			endTime = 1621267199,
+			line = 1,
+			task_id = 1000665,
+			startTime = 1623715200,
+			endTime = 1624291199,
 			gotoui = "game_Fishing3DHall",
-			downDes = "每日0点重置积分和奖励\n1000及以上炮倍捕鱼可获得积分，获得积分数量=鱼的倍数",
+			downDes = "每日0点重置积分和奖励\n10000及以上炮倍捕鱼可获得积分，获得积分数量=鱼的倍数",
 		},
 	},
 	awardInfo=
@@ -24,81 +25,81 @@ return {
 		{
 			index = 2,
 			need_num = 500,
-			award_name = "鲸币",
-			award_amount = 1800,
+			award_name = "金币",
+			award_amount = 18000,
 			award_icon = "pay_icon_gold3",
 		},
 		[3]=
 		{
 			index = 3,
 			need_num = 1000,
-			award_name = "福卡",
-			award_amount = 0.250000,
-			award_icon = "ad_2znjnk_icon_fk1",
+			award_name = "福利券",
+			award_amount = 25,
+			award_icon = "ty_icon_flq2",
 		},
 		[4]=
 		{
 			index = 4,
 			need_num = 2000,
 			award_name = "鱼币",
-			award_amount = 5000,
+			award_amount = 50000,
 			award_icon = "com_icon_yb",
 		},
 		[5]=
 		{
 			index = 5,
 			need_num = 5000,
-			award_name = "鲸币",
-			award_amount = "1万~3万",
+			award_name = "金币",
+			award_amount = "10万~30万",
 			award_icon = "pay_icon_gold4",
 		},
 		[6]=
 		{
 			index = 6,
 			need_num = 10000,
-			award_name = "鲸币",
-			award_amount = "2万~5万",
+			award_name = "金币",
+			award_amount = "20万~50万",
 			award_icon = "pay_icon_gold5",
 		},
 		[7]=
 		{
 			index = 7,
 			need_num = 30000,
-			award_name = "福卡",
-			award_amount = "8~15",
-			award_icon = "ad_2znjnk_icon_fk2",
+			award_name = "福利券",
+			award_amount = "800~1500",
+			award_icon = "ty_icon_flq3",
 		},
 		[8]=
 		{
 			index = 8,
 			need_num = 80000,
 			award_name = "鱼币",
-			award_amount = "20万~50万",
+			award_amount = "200万~500万",
 			award_icon = "com_award_icon_yb1",
 		},
 		[9]=
 		{
 			index = 9,
 			need_num = 150000,
-			award_name = "鲸币",
-			award_amount = "30万~80万",
+			award_name = "金币",
+			award_amount = "300万~800万",
 			award_icon = "pay_icon_gold6",
 		},
 		[10]=
 		{
 			index = 10,
 			need_num = 300000,
-			award_name = "福卡",
-			award_amount = "60~120",
-			award_icon = "ad_2znjnk_icon_fk3",
+			award_name = "福利券",
+			award_amount = "6000~12000",
+			award_icon = "ty_icon_flq4",
 		},
 		[11]=
 		{
 			index = 11,
 			need_num = 500000,
-			award_name = "福卡",
-			award_amount = "80~200",
-			award_icon = "ad_2znjnk_icon_fk4",
+			award_name = "福利券",
+			award_amount = "8000~20000",
+			award_icon = "ty_icon_flq5",
 		},
 	},
 }
