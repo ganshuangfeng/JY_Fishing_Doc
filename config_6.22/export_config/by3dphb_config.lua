e20c55ecd7914872b2a09bd56f248b8bb5a2a46f
yanghao <yanghao@qq.com>
2021-06-13 09:21:44 +0800

    6.22活动

---
+++ config_6.22/export_config/by3dphb_config.lua
@@ -0,0 +1,109 @@
+return {
+	info=
+	{
+		[1]=
+		{
+			id = 1,
+			page_name = "赢金榜",
+			panelName = "BY3DPHBRightPanel_yjb",
+			itemName = "BY3DPHBRightItemBase_yjb",
+			s_t = -1,
+			e_t = -1,
+			order = 1,
+			is_on = 0,
+			condition = "cpl_notcjj",
+		},
+		[2]=
+		{
+			id = 2,
+			page_name = "海怪榜",
+			panelName = "BY3DPHBRightPanel_hgb",
+			itemName = "BY3DPHBRightItemBase_hgb",
+			s_t = -1,
+			e_t = -1,
+			order = 2,
+			is_on = 0,
+		},
+		[3]=
+		{
+			id = 3,
+			page_name = "探险榜",
+			panelName = "BY3DPHBRightPanel_shtxb",
+			itemName = "BY3DPHBRightItemBase_shtxb",
+			s_t = -1,
+			e_t = -1,
+			order = 3,
+			is_on = 1,
+			condition = "cpl_notcjj",
+		},
+		[4]=
+		{
+			id = 4,
+			page_name = "排名赛",
+			panelName = "BY3DPHBRightPanel_pms",
+			itemName = "BY3DPHBRightItemBase_pms",
+			s_t = -1,
+			e_t = -1,
+			order = 4,
+			is_on = 0,
+		},
+		[5]=
+		{
+			id = 5,
+			page_name = "千元赛",
+			panelName = "BY3DPHBRightPanel_qys",
+			itemName = "BY3DPHBRightItemBase_qys",
+			s_t = -1,
+			e_t = -1,
+			order = 5,
+			is_on = 0,
+		},
+		[6]=
+		{
+			id = 6,
+			page_name = "大奖赛",
+			panelName = "BY3DPHBRightPanel_djs",
+			itemName = "BY3DPHBRightItemBase_djs",
+			s_t = -1,
+			e_t = -1,
+			order = 6,
+			is_on = 0,
+		},
+		[7]=
+		{
+			id = 7,
+			page_name = "闯关榜",
+			panelName = "BY3DPHBRightPanel_sgxxlb",
+			itemName = "BY3DPHBRightItemBase_sgxxlb",
+			s_t = -1,
+			e_t = -1,
+			order = 7,
+			is_on = 1,
+			condition = "cpl_cjj",
+		},
+		[8]=
+		{
+			id = 8,
+			page_name = "达人榜",
+			panelName = "BY3DPHBRightPanel_drb",
+			itemName = "BY3DPHBRightItemBase_drb",
+			s_t = -1,
+			e_t = -1,
+			order = 1,
+			is_on = 1,
+			condition = "cpl_notcjj",
+		},
+		[9]=
+		{
+			id = 9,
+			page_name = "达人榜",
+			panelName = "Act_045_XXLBDPanel",
+			itemName = "BY3DPHBRightItemBase_sgxxlb",
+			s_t = -1,
+			e_t = -1,
+			order = 1,
+			is_on = 1,
+			condition = "cpl_cjj",
+		},
+	},
+}
