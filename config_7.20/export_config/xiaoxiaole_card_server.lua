59395d85116dacb3a7ee61087b9e085b1e10a355
yanghao <yanghao@qq.com>
2021-07-23 09:31:24 +0800

    yxk

---
+++ config_7.20/export_config/xiaoxiaole_card_server.lua
@@ -0,0 +1,164 @@
+return {
+	main=
+	{
+		[1]=
+		{
+			id = 1,
+			card_type = "prop_xxl_shuiguo_card_1",
+			chip_type = "prop_xxl_card_chip_1",
+			game_name = "xiaoxiaole_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 5000,
+			power = 25,
+		},
+		[2]=
+		{
+			id = 2,
+			card_type = "prop_xxl_shuihu_card_1",
+			chip_type = "prop_xxl_card_chip_1",
+			game_name = "xiaoxiaole_shuihu_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 5000,
+			power = 25,
+		},
+		[3]=
+		{
+			id = 3,
+			card_type = "prop_xxl_caishen_card_1",
+			chip_type = "prop_xxl_card_chip_1",
+			game_name = "xiaoxiaole_caishen_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 5000,
+			power = 25,
+		},
+		[4]=
+		{
+			id = 4,
+			card_type = "prop_xxl_xiyou_card_1",
+			chip_type = "prop_xxl_card_chip_1",
+			game_name = "xiaoxiaole_xiyou_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 5000,
+			power = 25,
+		},
+		[7]=
+		{
+			id = 7,
+			card_type = "prop_xxl_shuiguo_card_2",
+			chip_type = "prop_xxl_card_chip_2",
+			game_name = "xiaoxiaole_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 10000,
+			power = 25,
+		},
+		[8]=
+		{
+			id = 8,
+			card_type = "prop_xxl_shuihu_card_2",
+			chip_type = "prop_xxl_card_chip_2",
+			game_name = "xiaoxiaole_shuihu_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 10000,
+			power = 25,
+		},
+		[9]=
+		{
+			id = 9,
+			card_type = "prop_xxl_caishen_card_2",
+			chip_type = "prop_xxl_card_chip_2",
+			game_name = "xiaoxiaole_caishen_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 10000,
+			power = 25,
+		},
+		[10]=
+		{
+			id = 10,
+			card_type = "prop_xxl_xiyou_card_2",
+			chip_type = "prop_xxl_card_chip_2",
+			game_name = "xiaoxiaole_xiyou_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 10000,
+			power = 25,
+		},
+		[13]=
+		{
+			id = 13,
+			card_type = "prop_xxl_shuiguo_card_3",
+			chip_type = "prop_xxl_card_chip_3",
+			game_name = "xiaoxiaole_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 30000,
+			power = 25,
+		},
+		[14]=
+		{
+			id = 14,
+			card_type = "prop_xxl_shuihu_card_3",
+			chip_type = "prop_xxl_card_chip_3",
+			game_name = "xiaoxiaole_shuihu_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 30000,
+			power = 25,
+		},
+		[15]=
+		{
+			id = 15,
+			card_type = "prop_xxl_caishen_card_3",
+			chip_type = "prop_xxl_card_chip_3",
+			game_name = "xiaoxiaole_caishen_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 30000,
+			power = 25,
+		},
+		[16]=
+		{
+			id = 16,
+			card_type = "prop_xxl_xiyou_card_3",
+			chip_type = "prop_xxl_card_chip_3",
+			game_name = "xiaoxiaole_xiyou_game",
+			min_rate = 125,
+			max_rate = 150,
+			need_money = 30000,
+			power = 25,
+		},
+	},
+	chip_merge=
+	{
+		[1]=
+		{
+			id = 1,
+			chip_type = "prop_xxl_card_chip_1",
+			chip_num = 20,
+			select_spend_type = "jing_bi",
+			select_spend_num = 5000,
+		},
+		[2]=
+		{
+			id = 2,
+			chip_type = "prop_xxl_card_chip_2",
+			chip_num = 20,
+			select_spend_type = "jing_bi",
+			select_spend_num = 10000,
+		},
+		[3]=
+		{
+			id = 3,
+			chip_type = "prop_xxl_card_chip_3",
+			chip_num = 20,
+			select_spend_type = "jing_bi",
+			select_spend_num = 30000,
+		},
+	},
+}
