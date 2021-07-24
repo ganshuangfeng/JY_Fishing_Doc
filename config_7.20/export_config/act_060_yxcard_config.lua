08006059e6330d812173a42993e67436749b3459
yanghao <yanghao@qq.com>
2021-07-24 10:54:53 +0800

    游戏卡

---
+++ config_7.20/export_config/act_060_yxcard_config.lua
@@ -0,0 +1,41 @@
+return {
+	yxcard=
+	{
+		[1]=
+		{
+			index = 1,
+			level = "初级",
+			need_chip_key = "prop_xxl_card_chip_1",
+			need_chip_num = 20,
+			consume_type = "jing_bi",
+			consume_num = 5000,
+			qp_image = "yxk_btn_djyxk",
+			game_level = 5000,
+			card_list = {"prop_xxl_shuiguo_card_1","prop_xxl_shuihu_card_1","prop_xxl_caishen_card_1","prop_xxl_xiyou_card_1","prop_xxl_chaoji_card_1","prop_xxl_sanguo_card_1"},
+		},
+		[2]=
+		{
+			index = 2,
+			level = "中级",
+			need_chip_key = "prop_xxl_card_chip_2",
+			need_chip_num = 20,
+			consume_type = "jing_bi",
+			consume_num = 10000,
+			qp_image = "yxk_btn_zjyxk",
+			game_level = 10000,
+			card_list = {"prop_xxl_shuiguo_card_2","prop_xxl_shuihu_card_2","prop_xxl_caishen_card_2","prop_xxl_xiyou_card_2","prop_xxl_chaoji_card_2","prop_xxl_sanguo_card_2"},
+		},
+		[3]=
+		{
+			index = 3,
+			level = "高级",
+			need_chip_key = "prop_xxl_card_chip_3",
+			need_chip_num = 20,
+			consume_type = "jing_bi",
+			consume_num = 30000,
+			qp_image = "yxk_btn_gjyxk",
+			game_level = 30000,
+			card_list = {"prop_xxl_shuiguo_card_3","prop_xxl_shuihu_card_3","prop_xxl_caishen_card_3","prop_xxl_xiyou_card_3","prop_xxl_chaoji_card_3","prop_xxl_sanguo_card_3"},
+		},
+	},
+}
