55483f4bbbd2c240cf5516c4520a1f80895bb063
denglu <denglu@qq.com>
2021-07-28 10:49:22 +0800

    提交

---
+++ config_8.10/export_config/act_ty_ldfd_config.lua
@@ -0,0 +1,40 @@
+return {
+	info=
+	{
+		[1]=
+		{
+			line = 1,
+			key = "beginTime",
+			value = 1627948800,
+		},
+		[2]=
+		{
+			line = 2,
+			key = "endTime",
+			value = 1630339199,
+		},
+		[3]=
+		{
+			line = 3,
+			key = "show_condiy_key",
+			value = "actp_buy_gift_class_hqjnh_046_cjj_hllb",
+		},
+		[4]=
+		{
+			line = 4,
+			key = "group_id",
+			value = 1,
+		},
+	},
+	gifts=
+	{
+		[1]=
+		{
+			line = 1,
+			group_id = 1,
+			condiy_key = "actp_buy_gift_class_hqjnh_046_cjj_hllb",
+			task_id = 1000653,
+			gift_ids = {10577,10576,10575,10574,10573,10572,10571},
+		},
+	},
+}
