6e1175e98c220343efda9c7c62fed4506ed13a27
denglu <denglu@qq.com>
2021-08-05 14:40:26 +0800

    提交

---
+++ config_8.17/export_config/signIn_cfg.lua
@@ -0,0 +1,252 @@
+return {
+	week=
+	{
+		[1]=
+		{
+			id = 1,
+			img = "ty_icon_jb_18y",
+			info = "3000金币",
+			vip = 1,
+			vipinfo = "金币x6000",
+			vip_desc = "V1双倍",
+			key = "signin_cpl",
+		},
+		[2]=
+		{
+			id = 2,
+			img = "3dby_btn_sd",
+			info = "锁定*3",
+			key = "signin_cpl",
+		},
+		[3]=
+		{
+			id = 3,
+			img = "ty_icon_jb_30y",
+			info = "5000金币",
+			vip = 2,
+			vipinfo = "金币x15000",
+			vip_desc = "V2三倍",
+			key = "signin_cpl",
+		},
+		[4]=
+		{
+			id = 4,
+			img = "ty_icon_jb_30y",
+			info = "8000金币",
+			key = "signin_cpl",
+		},
+		[5]=
+		{
+			id = 5,
+			img = "3dby_btn_zh",
+			info = "召唤*3",
+			key = "signin_cpl",
+		},
+		[6]=
+		{
+			id = 6,
+			img = "ty_icon_jb_50y",
+			info = "10000金币",
+			vip = 3,
+			vipinfo = "金币x30000",
+			vip_desc = "V3三倍",
+			key = "signin_cpl",
+		},
+		[7]=
+		{
+			id = 7,
+			img = "ty_icon_jb_198y",
+			info = "20000金币",
+			vip = 5,
+			vipinfo = "金币x60000",
+			vip_desc = "V5三倍",
+			key = "signin_cpl",
+		},
+		[8]=
+		{
+			id = 8,
+			img = "ty_icon_jb_18y",
+			info = "金币x10000",
+			vip = 1,
+			vipinfo = "金币x20000",
+			vip_desc = "V1双倍",
+			key = "signin_notcpl",
+		},
+		[9]=
+		{
+			id = 9,
+			img = "ty_icon_jb_18y",
+			info = "金币x15000",
+			key = "signin_notcpl",
+		},
+		[10]=
+		{
+			id = 10,
+			img = "ty_icon_jb_30y",
+			info = "金币x20000",
+			vip = 2,
+			vipinfo = "金币x40000",
+			vip_desc = "V2双倍",
+			key = "signin_notcpl",
+		},
+		[11]=
+		{
+			id = 11,
+			img = "ty_icon_jb_30y",
+			info = "金币x25000",
+			key = "signin_notcpl",
+		},
+		[12]=
+		{
+			id = 12,
+			img = "ty_icon_jb_50y",
+			info = "金币x30000",
+			vip = 3,
+			vipinfo = "金币x60000",
+			vip_desc = "V3双倍",
+			key = "signin_notcpl",
+		},
+		[13]=
+		{
+			id = 13,
+			img = "ty_icon_jb_50y",
+			info = "金币x35000",
+			key = "signin_notcpl",
+		},
+		[14]=
+		{
+			id = 14,
+			img = "ty_icon_jb_198y",
+			info = "金币x50000",
+			key = "signin_notcpl",
+		},
+	},
+	month=
+	{
+		[1]=
+		{
+			line = 1,
+			id = 1,
+			img = "ty_icon_flq2",
+			info = "10福利券",
+			day = 3,
+		},
+		[2]=
+		{
+			line = 2,
+			id = 2,
+			img = "ty_icon_jb_50y",
+			info = "金币x20000",
+			day = 7,
+		},
+		[3]=
+		{
+			line = 3,
+			id = 3,
+			img = "ty_icon_flq3",
+			info = "30福利券",
+			day = 14,
+		},
+		[4]=
+		{
+			line = 4,
+			id = 4,
+			img = "ty_icon_jb_198y",
+			info = "金币x50000",
+			day = 21,
+		},
+		[5]=
+		{
+			line = 5,
+			id = 5,
+			img = "3dby_icon_p1",
+			info = "七日雷霆幻影",
+			day = "M",
+			key = "cpl_notcjj",
+		},
+		[6]=
+		{
+			line = 6,
+			id = 5,
+			img = "ty_icon_flq3",
+			info = "100福利券",
+			day = "M",
+			key = "cpl_cjj",
+		},
+	},
+	vip=
+	{
+		[1]=
+		{
+			index = 1,
+			vip_level = 0,
+			dec_txt = {"*解锁自动开炮特权","*解锁1元支付宝奖励金","*解锁兑换商城实物商品"},
+		},
+		[2]=
+		{
+			index = 2,
+			vip_level = 1,
+			dec_txt = {"*解锁VIP超级转盘","*解锁3元支付宝奖励金","*解锁10元京东卡"},
+		},
+		[3]=
+		{
+			index = 3,
+			vip_level = 2,
+			dec_txt = {"*解锁闪耀风暴炮台","*解锁5元支付宝奖励金","*解锁20元京东卡，50元话费"},
+		},
+		[4]=
+		{
+			index = 4,
+			vip_level = 3,
+			dec_txt = {"*解锁10元支付宝奖励金","*解锁100元京东卡，100元话费","*福利券容量提升到15万"},
+		},
+		[5]=
+		{
+			index = 5,
+			vip_level = 4,
+			dec_txt = {"*解锁金币使者炮台","*解锁200元、300元京东卡","*福利券容量提升到30万"},
+		},
+		[6]=
+		{
+			index = 6,
+			vip_level = 5,
+			dec_txt = {"*福利券容量提升到50万",},
+		},
+		[7]=
+		{
+			index = 7,
+			vip_level = 6,
+			dec_txt = {"*福利券容量提升到100万",},
+		},
+		[8]=
+		{
+			index = 8,
+			vip_level = 7,
+			dec_txt = {"*福利券容量提升到200万",},
+		},
+		[9]=
+		{
+			index = 9,
+			vip_level = 8,
+			dec_txt = {"*福利券容量提升到300万",},
+		},
+		[10]=
+		{
+			index = 10,
+			vip_level = 9,
+			dec_txt = {"*福利券容量提升到500万",},
+		},
+		[11]=
+		{
+			index = 11,
+			vip_level = 10,
+			dec_txt = {"*每日免费领取至尊礼包","*领30次礼包额外奖励1000万鱼币","*福利券容量提升到800万"},
+		},
+		[12]=
+		{
+			index = 12,
+			vip_level = 11,
+			dec_txt = {"*每日免费领取至尊礼包","*领30次礼包额外奖励200元话费","*福利券容量提升到1200万"},
+		},
+	},
+}
