703403b6d5e4ea08c990f4aacc844293bf489f27
denglu <denglu@qq.com>
2021-08-10 17:30:20 +0800

    提交

--- config_debug/export_config/act_ty_phb_config.lua
+++ config_debug/export_config/act_ty_phb_config.lua
@@ -12,7 +12,7 @@ return {
 			rank_type = "lxjkh_024_jfphb_rank",
 			s_time = 1628553600,
 			e_time = 1629129599,
-			gotoUI = {"game_MiniGame",},
+			gotoUI = {"game_Fishing3DHall",},
 			award = {1,2,3,4,5,6,7},
 			extra_award = {8,9,10,11,12,13,14},
 			path = "act_025_dlphb",
