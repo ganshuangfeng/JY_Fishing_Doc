d3e80f4d2f1b4de5e17539ebf0399e9c278a8225
denglu <denglu@qq.com>
2021-08-16 18:04:20 +0800

    提交

---
+++ config_debug/export_config/fish3d_use_config.lua
@@ -0,0 +1,999 @@
+return {
+	use_fish=
+	{
+		[1]=
+		{
+			id = 1,
+			fish_id = 1,
+		},
+		[2]=
+		{
+			id = 2,
+			fish_id = 2,
+		},
+		[3]=
+		{
+			id = 3,
+			fish_id = 3,
+		},
+		[4]=
+		{
+			id = 4,
+			fish_id = 4,
+		},
+		[5]=
+		{
+			id = 5,
+			fish_id = 5,
+		},
+		[6]=
+		{
+			id = 6,
+			fish_id = 6,
+		},
+		[7]=
+		{
+			id = 7,
+			fish_id = 7,
+		},
+		[8]=
+		{
+			id = 8,
+			fish_id = 8,
+		},
+		[9]=
+		{
+			id = 9,
+			fish_id = 9,
+		},
+		[10]=
+		{
+			id = 10,
+			fish_id = 10,
+		},
+		[11]=
+		{
+			id = 11,
+			fish_id = 11,
+		},
+		[12]=
+		{
+			id = 12,
+			fish_id = 12,
+		},
+		[13]=
+		{
+			id = 13,
+			fish_id = 13,
+		},
+		[14]=
+		{
+			id = 14,
+			fish_id = 14,
+		},
+		[15]=
+		{
+			id = 15,
+			fish_id = 15,
+		},
+		[16]=
+		{
+			id = 16,
+			fish_id = 16,
+		},
+		[17]=
+		{
+			id = 17,
+			fish_id = 17,
+		},
+		[18]=
+		{
+			id = 18,
+			fish_id = 18,
+		},
+		[19]=
+		{
+			id = 19,
+			fish_id = 19,
+		},
+		[20]=
+		{
+			id = 20,
+			fish_id = 20,
+		},
+		[21]=
+		{
+			id = 21,
+			fish_id = 21,
+		},
+		[22]=
+		{
+			id = 22,
+			fish_id = 22,
+		},
+		[23]=
+		{
+			id = 23,
+			fish_id = 23,
+		},
+		[24]=
+		{
+			id = 24,
+			fish_id = 24,
+		},
+		[25]=
+		{
+			id = 25,
+			fish_id = 25,
+		},
+		[26]=
+		{
+			id = 26,
+			fish_id = 26,
+		},
+		[27]=
+		{
+			id = 27,
+			fish_id = 27,
+		},
+		[28]=
+		{
+			id = 28,
+			fish_id = 28,
+		},
+		[29]=
+		{
+			id = 29,
+			fish_id = 29,
+		},
+		[30]=
+		{
+			id = 30,
+			fish_id = 30,
+		},
+		[31]=
+		{
+			id = 31,
+			fish_id = 31,
+		},
+		[32]=
+		{
+			id = 32,
+			fish_id = 32,
+		},
+		[33]=
+		{
+			id = 33,
+			fish_id = 33,
+		},
+		[34]=
+		{
+			id = 34,
+			fish_id = 34,
+		},
+		[35]=
+		{
+			id = 35,
+			fish_id = 35,
+		},
+		[36]=
+		{
+			id = 36,
+			fish_id = 36,
+		},
+		[37]=
+		{
+			id = 37,
+			fish_id = 37,
+		},
+		[38]=
+		{
+			id = 38,
+			fish_id = 38,
+		},
+		[39]=
+		{
+			id = 39,
+			fish_id = 39,
+		},
+		[40]=
+		{
+			id = 40,
+			fish_id = 40,
+		},
+		[41]=
+		{
+			id = 41,
+			fish_id = 41,
+		},
+		[42]=
+		{
+			id = 42,
+			fish_id = 42,
+		},
+		[43]=
+		{
+			id = 43,
+			fish_id = 43,
+		},
+		[44]=
+		{
+			id = 44,
+			fish_id = 44,
+		},
+		[45]=
+		{
+			id = 45,
+			fish_id = 45,
+		},
+		[46]=
+		{
+			id = 46,
+			fish_id = 46,
+		},
+		[47]=
+		{
+			id = 47,
+			fish_id = 47,
+		},
+		[48]=
+		{
+			id = 48,
+			fish_id = 48,
+		},
+		[49]=
+		{
+			id = 49,
+			fish_id = 49,
+		},
+		[50]=
+		{
+			id = 50,
+			fish_id = 50,
+		},
+		[51]=
+		{
+			id = 51,
+			fish_id = 11,
+		},
+		[52]=
+		{
+			id = 52,
+			fish_id = 12,
+		},
+		[53]=
+		{
+			id = 53,
+			fish_id = 13,
+		},
+		[54]=
+		{
+			id = 54,
+			fish_id = 14,
+		},
+		[55]=
+		{
+			id = 55,
+			fish_id = 15,
+		},
+		[56]=
+		{
+			id = 56,
+			fish_id = 16,
+		},
+		[57]=
+		{
+			id = 57,
+			fish_id = 17,
+		},
+		[58]=
+		{
+			id = 58,
+			fish_id = 18,
+		},
+		[59]=
+		{
+			id = 59,
+			fish_id = 20,
+		},
+		[60]=
+		{
+			id = 60,
+			fish_id = 22,
+		},
+		[61]=
+		{
+			id = 61,
+			fish_id = 23,
+		},
+		[62]=
+		{
+			id = 62,
+			fish_id = 25,
+		},
+		[63]=
+		{
+			id = 63,
+			fish_id = 26,
+		},
+		[64]=
+		{
+			id = 64,
+			fish_id = 27,
+		},
+		[65]=
+		{
+			id = 65,
+			fish_id = 11,
+		},
+		[66]=
+		{
+			id = 66,
+			fish_id = 12,
+		},
+		[67]=
+		{
+			id = 67,
+			fish_id = 13,
+		},
+		[68]=
+		{
+			id = 68,
+			fish_id = 14,
+		},
+		[69]=
+		{
+			id = 69,
+			fish_id = 15,
+		},
+		[70]=
+		{
+			id = 70,
+			fish_id = 16,
+		},
+		[71]=
+		{
+			id = 71,
+			fish_id = 17,
+		},
+		[72]=
+		{
+			id = 72,
+			fish_id = 18,
+		},
+		[73]=
+		{
+			id = 73,
+			fish_id = 20,
+		},
+		[74]=
+		{
+			id = 74,
+			fish_id = 22,
+		},
+		[75]=
+		{
+			id = 75,
+			fish_id = 23,
+		},
+		[76]=
+		{
+			id = 76,
+			fish_id = 25,
+		},
+		[77]=
+		{
+			id = 77,
+			fish_id = 26,
+		},
+		[78]=
+		{
+			id = 78,
+			fish_id = 27,
+		},
+		[79]=
+		{
+			id = 79,
+			fish_id = 51,
+		},
+		[80]=
+		{
+			id = 80,
+			fish_id = 18,
+			attr_id = 9,
+		},
+		[81]=
+		{
+			id = 81,
+			fish_id = 17,
+			attr_id = 9,
+		},
+		[82]=
+		{
+			id = 82,
+			fish_id = 15,
+			attr_id = 9,
+		},
+		[83]=
+		{
+			id = 83,
+			fish_id = 13,
+			attr_id = 9,
+		},
+		[84]=
+		{
+			id = 84,
+			fish_id = 52,
+		},
+		[85]=
+		{
+			id = 85,
+			fish_id = 52,
+		},
+		[86]=
+		{
+			id = 86,
+			fish_id = 52,
+		},
+		[87]=
+		{
+			id = 87,
+			fish_id = 53,
+			attr_id = 9,
+		},
+		[88]=
+		{
+			id = 88,
+			fish_id = 54,
+			attr_id = 9,
+		},
+		[89]=
+		{
+			id = 89,
+			fish_id = 55,
+			attr_id = 9,
+		},
+		[90]=
+		{
+			id = 90,
+			fish_id = 56,
+			attr_id = 9,
+		},
+		[91]=
+		{
+			id = 91,
+			fish_id = 57,
+			cond_key = "actp_031_xxy_show",
+		},
+		[92]=
+		{
+			id = 92,
+			fish_id = 57,
+			cond_key = "actp_031_xxy_show",
+		},
+		[93]=
+		{
+			id = 93,
+			fish_id = 57,
+			cond_key = "actp_031_xxy_show",
+		},
+		[94]=
+		{
+			id = 94,
+			fish_id = 11,
+			attr_id = 9,
+			cond_key = "actp_031_xxy_show",
+		},
+		[95]=
+		{
+			id = 95,
+			fish_id = 12,
+			attr_id = 9,
+			cond_key = "actp_031_xxy_show",
+		},
+		[96]=
+		{
+			id = 96,
+			fish_id = 13,
+			attr_id = 9,
+			cond_key = "actp_031_xxy_show",
+		},
+		[97]=
+		{
+			id = 97,
+			fish_id = 14,
+			attr_id = 9,
+			cond_key = "actp_031_xxy_show",
+		},
+		[98]=
+		{
+			id = 98,
+			fish_id = 15,
+			attr_id = 9,
+			cond_key = "actp_031_xxy_show",
+		},
+		[99]=
+		{
+			id = 99,
+			fish_id = 16,
+			attr_id = 9,
+			cond_key = "actp_031_xxy_show",
+		},
+		[100]=
+		{
+			id = 100,
+			fish_id = 17,
+			attr_id = 9,
+			cond_key = "actp_031_xxy_show",
+		},
+		[101]=
+		{
+			id = 101,
+			fish_id = 13,
+			attr_id = 9,
+		},
+		[102]=
+		{
+			id = 102,
+			fish_id = 14,
+			attr_id = 9,
+		},
+		[103]=
+		{
+			id = 103,
+			fish_id = 15,
+			attr_id = 9,
+		},
+		[104]=
+		{
+			id = 104,
+			fish_id = 16,
+			attr_id = 9,
+		},
+		[105]=
+		{
+			id = 105,
+			fish_id = 17,
+			attr_id = 9,
+		},
+		[106]=
+		{
+			id = 106,
+			fish_id = 18,
+			attr_id = 9,
+		},
+		[107]=
+		{
+			id = 107,
+			fish_id = 58,
+		},
+		[108]=
+		{
+			id = 108,
+			fish_id = 58,
+		},
+		[109]=
+		{
+			id = 109,
+			fish_id = 58,
+		},
+		[110]=
+		{
+			id = 110,
+			fish_id = 59,
+		},
+		[111]=
+		{
+			id = 111,
+			fish_id = 59,
+		},
+		[112]=
+		{
+			id = 112,
+			fish_id = 13,
+			attr_id = 9,
+		},
+		[113]=
+		{
+			id = 113,
+			fish_id = 14,
+			attr_id = 9,
+		},
+		[114]=
+		{
+			id = 114,
+			fish_id = 15,
+			attr_id = 9,
+		},
+		[115]=
+		{
+			id = 115,
+			fish_id = 16,
+			attr_id = 9,
+		},
+		[116]=
+		{
+			id = 116,
+			fish_id = 17,
+			attr_id = 9,
+		},
+		[117]=
+		{
+			id = 117,
+			fish_id = 18,
+			attr_id = 9,
+		},
+		[118]=
+		{
+			id = 118,
+			fish_id = 60,
+		},
+		[119]=
+		{
+			id = 119,
+			fish_id = 60,
+		},
+		[120]=
+		{
+			id = 120,
+			fish_id = 60,
+		},
+		[121]=
+		{
+			id = 121,
+			fish_id = 20,
+		},
+		[122]=
+		{
+			id = 122,
+			fish_id = 22,
+		},
+		[123]=
+		{
+			id = 123,
+			fish_id = 23,
+		},
+		[124]=
+		{
+			id = 124,
+			fish_id = 25,
+		},
+		[125]=
+		{
+			id = 125,
+			fish_id = 26,
+		},
+		[126]=
+		{
+			id = 126,
+			fish_id = 27,
+		},
+		[127]=
+		{
+			id = 127,
+			fish_id = 20,
+		},
+		[128]=
+		{
+			id = 128,
+			fish_id = 22,
+		},
+		[129]=
+		{
+			id = 129,
+			fish_id = 23,
+		},
+		[130]=
+		{
+			id = 130,
+			fish_id = 25,
+		},
+		[131]=
+		{
+			id = 131,
+			fish_id = 26,
+		},
+		[132]=
+		{
+			id = 132,
+			fish_id = 27,
+		},
+		[133]=
+		{
+			id = 133,
+			fish_id = 20,
+		},
+		[134]=
+		{
+			id = 134,
+			fish_id = 22,
+		},
+		[135]=
+		{
+			id = 135,
+			fish_id = 23,
+		},
+		[136]=
+		{
+			id = 136,
+			fish_id = 25,
+		},
+		[137]=
+		{
+			id = 137,
+			fish_id = 26,
+		},
+		[138]=
+		{
+			id = 138,
+			fish_id = 27,
+		},
+		[139]=
+		{
+			id = 139,
+			fish_id = 20,
+		},
+		[140]=
+		{
+			id = 140,
+			fish_id = 22,
+		},
+		[141]=
+		{
+			id = 141,
+			fish_id = 23,
+		},
+		[142]=
+		{
+			id = 142,
+			fish_id = 25,
+		},
+		[143]=
+		{
+			id = 143,
+			fish_id = 26,
+		},
+		[144]=
+		{
+			id = 144,
+			fish_id = 27,
+		},
+		[145]=
+		{
+			id = 145,
+			fish_id = 13,
+			attr_id = 9,
+			cond_key = "actp_own_task_p_drop_act_nor",
+		},
+		[146]=
+		{
+			id = 146,
+			fish_id = 14,
+			attr_id = 9,
+			cond_key = "actp_own_task_p_drop_act_nor",
+		},
+		[147]=
+		{
+			id = 147,
+			fish_id = 15,
+			attr_id = 9,
+			cond_key = "actp_own_task_p_drop_act_nor",
+		},
+		[148]=
+		{
+			id = 148,
+			fish_id = 16,
+			attr_id = 9,
+			cond_key = "actp_own_task_p_drop_act_nor",
+		},
+		[149]=
+		{
+			id = 149,
+			fish_id = 17,
+			attr_id = 9,
+			cond_key = "actp_own_task_p_drop_act_nor",
+		},
+		[150]=
+		{
+			id = 150,
+			fish_id = 18,
+			attr_id = 9,
+			cond_key = "actp_own_task_p_drop_act_nor",
+		},
+		[151]=
+		{
+			id = 151,
+			fish_id = 61,
+			cond_key = "actp_own_task_p_drop_act_nor",
+		},
+		[152]=
+		{
+			id = 152,
+			fish_id = 61,
+			cond_key = "actp_own_task_p_drop_act_nor",
+		},
+		[153]=
+		{
+			id = 153,
+			fish_id = 61,
+			cond_key = "actp_own_task_p_drop_act_nor",
+		},
+		[154]=
+		{
+			id = 154,
+			fish_id = 13,
+			attr_id = 9,
+		},
+		[155]=
+		{
+			id = 155,
+			fish_id = 14,
+			attr_id = 9,
+		},
+		[156]=
+		{
+			id = 156,
+			fish_id = 15,
+			attr_id = 9,
+		},
+		[157]=
+		{
+			id = 157,
+			fish_id = 16,
+			attr_id = 9,
+		},
+		[158]=
+		{
+			id = 158,
+			fish_id = 17,
+			attr_id = 9,
+		},
+		[159]=
+		{
+			id = 159,
+			fish_id = 18,
+			attr_id = 9,
+		},
+		[160]=
+		{
+			id = 160,
+			fish_id = 61,
+		},
+		[161]=
+		{
+			id = 161,
+			fish_id = 61,
+		},
+		[162]=
+		{
+			id = 162,
+			fish_id = 61,
+		},
+		[163]=
+		{
+			id = 163,
+			fish_id = 13,
+			attr_id = 9,
+		},
+		[164]=
+		{
+			id = 164,
+			fish_id = 14,
+			attr_id = 9,
+		},
+		[165]=
+		{
+			id = 165,
+			fish_id = 15,
+			attr_id = 9,
+		},
+		[166]=
+		{
+			id = 166,
+			fish_id = 16,
+			attr_id = 9,
+		},
+		[167]=
+		{
+			id = 167,
+			fish_id = 17,
+			attr_id = 9,
+		},
+		[168]=
+		{
+			id = 168,
+			fish_id = 18,
+			attr_id = 9,
+		},
+		[169]=
+		{
+			id = 169,
+			fish_id = 61,
+		},
+		[170]=
+		{
+			id = 170,
+			fish_id = 61,
+		},
+		[171]=
+		{
+			id = 171,
+			fish_id = 61,
+		},
+		[172]=
+		{
+			id = 172,
+			fish_id = 62,
+		},
+		[173]=
+		{
+			id = 173,
+			fish_id = 62,
+		},
+		[174]=
+		{
+			id = 174,
+			fish_id = 63,
+		},
+		[175]=
+		{
+			id = 175,
+			fish_id = 64,
+		},
+		[176]=
+		{
+			id = 176,
+			fish_id = 65,
+		},
+		[177]=
+		{
+			id = 177,
+			fish_id = 66,
+		},
+		[178]=
+		{
+			id = 178,
+			fish_id = 67,
+		},
+		[179]=
+		{
+			id = 179,
+			fish_id = 68,
+		},
+		[180]=
+		{
+			id = 180,
+			fish_id = 20,
+		},
+		[181]=
+		{
+			id = 181,
+			fish_id = 22,
+		},
+		[182]=
+		{
+			id = 182,
+			fish_id = 23,
+		},
+		[183]=
+		{
+			id = 183,
+			fish_id = 25,
+		},
+		[184]=
+		{
+			id = 184,
+			fish_id = 26,
+		},
+		[185]=
+		{
+			id = 185,
+			fish_id = 27,
+		},
+		[186]=
+		{
+			id = 186,
+			fish_id = 61,
+		},
+	},
+}
