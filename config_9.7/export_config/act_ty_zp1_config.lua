54726636c190667435004dcfd518180ea2c1cf63
denglu <denglu@qq.com>
2021-08-24 17:36:12 +0800

    9.7

---
+++ config_9.7/export_config/act_ty_zp1_config.lua
@@ -0,0 +1,304 @@
+return {
+	platform_channel=
+	{
+		[1]=
+		{
+			line = 1,
+			permission = "actp_own_task_p_hljnh_046_hlfl_cjj",
+			item_key = "prop_qdlb_cjq_cjj",
+			box_exchange_id = 99,
+			config = "config1",
+			gift = "gift1",
+		},
+		[2]=
+		{
+			line = 2,
+			permission = "actp_own_task_p_hljnh_046_hlfl_nor",
+			item_key = "prop_qdlb_cjq",
+			box_exchange_id = 87,
+			config = "config2",
+			gift = "gift2",
+		},
+	},
+	other_data=
+	{
+		[1]=
+		{
+			line = 1,
+			sta_t = 1630972800,
+			end_t = 1631548799,
+			cur_path = "act_ty_zp1_007",
+			xg_desc = "礼包不限购",
+			sort = 0,
+		},
+		[2]=
+		{
+			line = 2,
+			sta_t = 1630972800,
+			end_t = 1631548799,
+			cur_path = "act_ty_zp1_007",
+			xg_desc = "礼包不限购",
+			sort = 0,
+		},
+	},
+	config1=
+	{
+		[1]=
+		{
+			line = 1,
+			icon = "cjlb_Iphone12_2",
+			name = "美的空调",
+			award_id = 12093,
+		},
+		[2]=
+		{
+			line = 2,
+			icon = "ty_icon_flq1",
+			name = "5福利券",
+			asset_type = "shop_gold_sum",
+			value = 5,
+			award_id = 12088,
+		},
+		[3]=
+		{
+			line = 3,
+			icon = "ty_icon_jb_15y",
+			name = "10000金币",
+			asset_type = "jing_bi",
+			value = 10000,
+			award_id = 12089,
+		},
+		[4]=
+		{
+			line = 4,
+			icon = "ty_icon_flq2",
+			name = "30福利券",
+			asset_type = "shop_gold_sum",
+			value = 30,
+			award_id = 12090,
+		},
+		[5]=
+		{
+			line = 5,
+			icon = "ty_icon_jb_18y",
+			name = "50000金币",
+			asset_type = "jing_bi",
+			value = 50000,
+			award_id = 12091,
+		},
+		[6]=
+		{
+			line = 6,
+			icon = "activity_icon_gift185_syhf",
+			name = "10元话费",
+			asset_type = "prop_web_chip_huafei",
+			value = 1000,
+			award_id = 12092,
+		},
+	},
+	gift1=
+	{
+		[1]=
+		{
+			line = 1,
+			gift_id = 10599,
+			name = "498元礼包",
+			order = 6,
+			pay_icon = {"ty_icon_jb_498y","ty_icon_yxb_5","act_ty_zp1_cjq_cjj",},
+			pay_name = {"5000万金币","100万小游戏币","抽奖券*50",},
+			Isindefinitely = 0,
+			Isrecommended = 1,
+			tips = {"","","可用于抽奖"},
+		},
+		[2]=
+		{
+			line = 2,
+			gift_id = 10600,
+			name = "198元礼包",
+			order = 5,
+			pay_icon = {"ty_icon_jb_198y","ty_icon_yxb_4","act_ty_zp1_cjq_cjj",},
+			pay_name = {"1980万金币","40万小游戏币","抽奖券*30",},
+			Isindefinitely = 0,
+			Isrecommended = 0,
+			tips = {"","","可用于抽奖"},
+		},
+		[3]=
+		{
+			line = 3,
+			gift_id = 10601,
+			name = "98元礼包",
+			order = 4,
+			pay_icon = {"ty_icon_jb_98y","ty_icon_yxb_2","act_ty_zp1_cjq_cjj",},
+			pay_name = {"980万金币","18万小游戏币","抽奖券*15",},
+			Isindefinitely = 0,
+			Isrecommended = 0,
+			tips = {"","","可用于抽奖"},
+		},
+		[4]=
+		{
+			line = 4,
+			gift_id = 10602,
+			name = "48元礼包",
+			order = 3,
+			pay_icon = {"ty_icon_jb_50y","ty_icon_yxb_2","act_ty_zp1_cjq_cjj",},
+			pay_name = {"480万金币","8万小游戏币","抽奖券*8",},
+			Isindefinitely = 0,
+			Isrecommended = 0,
+			tips = {"","","可用于抽奖"},
+		},
+		[5]=
+		{
+			line = 5,
+			gift_id = 10603,
+			name = "20元礼包",
+			order = 2,
+			pay_icon = {"ty_icon_jb_30y","ty_icon_yxb_3","act_ty_zp1_cjq_cjj",},
+			pay_name = {"200万金币","2万小游戏币","抽奖券*4",},
+			Isindefinitely = 0,
+			Isrecommended = 0,
+			tips = {"","","可用于抽奖"},
+		},
+		[6]=
+		{
+			line = 6,
+			gift_id = 10604,
+			name = "6元礼包",
+			order = 1,
+			pay_icon = {"ty_icon_jb_6y","ty_icon_yxb_3","act_ty_zp1_cjq_cjj",},
+			pay_name = {"60万金币","5000小游戏币","抽奖券*1",},
+			Isindefinitely = 0,
+			Isrecommended = 0,
+			tips = {"","","可用于抽奖"},
+		},
+	},
+	config2=
+	{
+		[1]=
+		{
+			line = 1,
+			icon = "cjlb_Iphone12_2",
+			name = "美的空调",
+			award_id = 12100,
+		},
+		[2]=
+		{
+			line = 2,
+			icon = "ty_icon_flq1",
+			name = "5福利券",
+			asset_type = "shop_gold_sum",
+			value = 5,
+			award_id = 12005,
+		},
+		[3]=
+		{
+			line = 3,
+			icon = "ty_icon_jb_15y",
+			name = "10000金币",
+			asset_type = "jing_bi",
+			value = 10000,
+			award_id = 12006,
+		},
+		[4]=
+		{
+			line = 4,
+			icon = "ty_icon_flq2",
+			name = "30福利券",
+			asset_type = "shop_gold_sum",
+			value = 30,
+			award_id = 12007,
+		},
+		[5]=
+		{
+			line = 5,
+			icon = "ty_icon_jb_18y",
+			name = "50000金币",
+			asset_type = "jing_bi",
+			value = 50000,
+			award_id = 12008,
+		},
+		[6]=
+		{
+			line = 6,
+			icon = "activity_icon_gift185_syhf",
+			name = "10元话费",
+			asset_type = "prop_web_chip_huafei",
+			value = 1000,
+			award_id = 12009,
+		},
+	},
+	gift2=
+	{
+		[1]=
+		{
+			line = 1,
+			gift_id = 10605,
+			name = "498元礼包",
+			order = 6,
+			pay_icon = {"ty_icon_jb_498y","ty_icon_yxb_5","act_ty_zp1_cjq",},
+			pay_name = {"5000万金币","100万小游戏币","抽奖券*50",},
+			Isindefinitely = 0,
+			Isrecommended = 1,
+			tips = {"","","可用于抽奖"},
+		},
+		[2]=
+		{
+			line = 2,
+			gift_id = 10606,
+			name = "198元礼包",
+			order = 5,
+			pay_icon = {"ty_icon_jb_198y","ty_icon_yxb_4","act_ty_zp1_cjq",},
+			pay_name = {"1980万金币","40万小游戏币","抽奖券*30",},
+			Isindefinitely = 0,
+			Isrecommended = 0,
+			tips = {"","","可用于抽奖"},
+		},
+		[3]=
+		{
+			line = 3,
+			gift_id = 10607,
+			name = "98元礼包",
+			order = 4,
+			pay_icon = {"ty_icon_jb_98y","ty_icon_yxb_2","act_ty_zp1_cjq",},
+			pay_name = {"980万金币","18万小游戏币","抽奖券*15",},
+			Isindefinitely = 0,
+			Isrecommended = 0,
+			tips = {"","","可用于抽奖"},
+		},
+		[4]=
+		{
+			line = 4,
+			gift_id = 10608,
+			name = "48元礼包",
+			order = 3,
+			pay_icon = {"ty_icon_jb_50y","ty_icon_yxb_2","act_ty_zp1_cjq",},
+			pay_name = {"480万金币","8万小游戏币","抽奖券*8",},
+			Isindefinitely = 0,
+			Isrecommended = 0,
+			tips = {"","","可用于抽奖"},
+		},
+		[5]=
+		{
+			line = 5,
+			gift_id = 10609,
+			name = "20元礼包",
+			order = 2,
+			pay_icon = {"ty_icon_jb_30y","ty_icon_yxb_3","act_ty_zp1_cjq",},
+			pay_name = {"200万金币","2万小游戏币","抽奖券*4",},
+			Isindefinitely = 0,
+			Isrecommended = 0,
+			tips = {"","","可用于抽奖"},
+		},
+		[6]=
+		{
+			line = 6,
+			gift_id = 10610,
+			name = "6元礼包",
+			order = 1,
+			pay_icon = {"ty_icon_jb_6y","ty_icon_yxb_3","act_ty_zp1_cjq",},
+			pay_name = {"60万金币","5000小游戏币","抽奖券*1",},
+			Isindefinitely = 0,
+			Isrecommended = 0,
+			tips = {"","","可用于抽奖"},
+		},
+	},
+}
