38d742ac2dddc20137a29eccd26c45d3559423fe
denglu <denglu@qq.com>
2021-08-24 17:42:17 +0800

    小优化

---
+++ config_9.7/export_config/fish_hall_config.lua
@@ -0,0 +1,58 @@
+return {
+	game=
+	{
+		[1]=
+		{
+			index = 1,
+			game_id = 1,
+			name = "浅水湾",
+			sceneID = 27,
+			game_type = "game_Fishing",
+			order = 1,
+			enter_min = 500000,
+			enter_max = 5000000,
+			is_on = 1,
+			recommend_min = 0,
+			recommend_max = 999999,
+		},
+		[2]=
+		{
+			index = 2,
+			game_id = 2,
+			name = "深海寻宝",
+			sceneID = 27,
+			game_type = "game_Fishing",
+			order = 2,
+			enter_min = 2000000,
+			enter_max = 200000000,
+			is_on = 1,
+			recommend_min = 1000000,
+			recommend_max = 5999999,
+		},
+		[3]=
+		{
+			index = 3,
+			game_id = 3,
+			name = "地底遗迹",
+			sceneID = 27,
+			game_type = "game_Fishing",
+			order = 3,
+			enter_min = 10000000,
+			is_on = 1,
+			recommend_min = 6000000,
+		},
+		[4]=
+		{
+			index = 4,
+			game_id = 4,
+			name = "体验场",
+			sceneID = 27,
+			game_type = "game_Fishing",
+			order = 4,
+			enter_max = 100000,
+			is_on = 1,
+			recommend_min = 0,
+			recommend_max = 0,
+		},
+	},
+}
