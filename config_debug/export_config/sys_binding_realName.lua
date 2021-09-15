d4e445e4e1b249da90b9c7ce10842824b5cb2ae5
denglu <denglu@qq.com>
2021-09-15 15:10:53 +0800

    提交

---
+++ config_debug/export_config/sys_binding_realName.lua
@@ -0,0 +1,44 @@
+return {
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			id = 1,
+			icon = "ty_icon_jb_30y",
+			desc = "金币×5000",
+		},
+		[2]=
+		{
+			line = 2,
+			id = 2,
+			icon = "ty_icon_yb_3",
+			desc = "鱼币×5000",
+			condi_key = "cpl_notcjj",
+		},
+		[3]=
+		{
+			line = 3,
+			id = 2,
+			icon = "ty_icon_yxb_4",
+			desc = "小游戏币×5000",
+			condi_key = "cpl_cjj",
+		},
+		[4]=
+		{
+			line = 4,
+			id = 3,
+			icon = "3dby_btn_sd",
+			desc = "锁定x3",
+			condi_key = "cpl_notcjj",
+		},
+		[5]=
+		{
+			line = 5,
+			id = 3,
+			icon = "zjd_icon_wjc",
+			desc = "玩具锤x1",
+			condi_key = "cpl_cjj",
+		},
+	},
+}
