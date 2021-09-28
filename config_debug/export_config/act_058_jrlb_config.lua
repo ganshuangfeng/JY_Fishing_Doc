0955a345790bc3bd4367f022a19a4d114e755c72
denglu <denglu@qq.com>
2021-09-28 13:56:50 +0800

    提交

---
+++ config_debug/export_config/act_058_jrlb_config.lua
@@ -0,0 +1,57 @@
+return {
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			gift_ids = {10625,10626,10627,10628},
+			permission = "actp_buy_gift_bag_class_integral_gift_buy_nor",
+			gift_1_txt = {"600000","稀有宝箱*1","狂暴*1","子弹加速*1"},
+			gift_2_txt = {"1000000","稀有宝箱*2","狂暴*2","子弹加速*2"},
+			gift_3_txt = {"1800000","稀有宝箱*3","狂暴*3","子弹加速*3"},
+			gift_1_icon = {"pay_icon_gold7","cylb_btn_bx1","3dby_btn_kb","3dby_btn_js"},
+			gift_2_icon = {"pay_icon_gold7","cylb_btn_bx1","3dby_btn_kb","3dby_btn_js"},
+			gift_3_icon = {"pay_icon_gold7","cylb_btn_bx1","3dby_btn_kb","3dby_btn_js"},
+			buy_all_give = "20万鱼币",
+		},
+		[2]=
+		{
+			line = 2,
+			gift_ids = {10629,10630,10631,10632},
+			permission = "actp_buy_gift_bag_class_integral_gift_buy_v1",
+			gift_1_txt = {"1800000","稀有宝箱*1","狂暴*3","子弹加速*3"},
+			gift_2_txt = {"3000000","稀有宝箱*1","狂暴*4","子弹加速*4"},
+			gift_3_txt = {"4800000","稀有宝箱*2","狂暴*5","子弹加速*5"},
+			gift_1_icon = {"pay_icon_gold7","cylb_btn_bx1","3dby_btn_kb","3dby_btn_js"},
+			gift_2_icon = {"pay_icon_gold7","cylb_btn_bx1","3dby_btn_kb","3dby_btn_js"},
+			gift_3_icon = {"pay_icon_gold7","cylb_btn_bx1","3dby_btn_kb","3dby_btn_js"},
+			buy_all_give = "30万鱼币",
+		},
+		[3]=
+		{
+			line = 3,
+			gift_ids = {10633,10634,10635,10636},
+			permission = "actp_buy_gift_bag_class_integral_gift_buy_v4",
+			gift_1_txt = {"4800000","史诗宝箱*1","狂暴*5","子弹加速*5"},
+			gift_2_txt = {"9800000","史诗宝箱*2","狂暴*6","子弹加速*6"},
+			gift_3_txt = {"19800000","史诗宝箱*3","狂暴*7","子弹加速*7"},
+			gift_1_icon = {"pay_icon_gold7","cylb_btn_bx2","3dby_btn_kb","3dby_btn_js"},
+			gift_2_icon = {"pay_icon_gold7","cylb_btn_bx2","3dby_btn_kb","3dby_btn_js"},
+			gift_3_icon = {"pay_icon_gold7","cylb_btn_bx2","3dby_btn_kb","3dby_btn_js"},
+			buy_all_give = "100万鱼币",
+		},
+		[4]=
+		{
+			line = 4,
+			gift_ids = {10637,10638,10639,10640},
+			permission = "actp_buy_gift_bag_class_integral_gift_buy_v8",
+			gift_1_txt = {"9800000","传说宝箱*1","狂暴*8","子弹加速*8"},
+			gift_2_txt = {"19800000","传说宝箱*2","狂暴*9","子弹加速*9"},
+			gift_3_txt = {"49800000","传说宝箱*5","狂暴*10","子弹加速*10"},
+			gift_1_icon = {"pay_icon_gold7","cylb_btn_bx3","3dby_btn_kb","3dby_btn_js"},
+			gift_2_icon = {"pay_icon_gold7","cylb_btn_bx3","3dby_btn_kb","3dby_btn_js"},
+			gift_3_icon = {"pay_icon_gold7","cylb_btn_bx3","3dby_btn_kb","3dby_btn_js"},
+			buy_all_give = "200万鱼币",
+		},
+	},
+}
--- config_debug/export_config/act_ty_phb_config.lua
+++ config_debug/export_config/act_ty_phb_config.lua
@@ -7,6 +7,7 @@ return {
 			is_on_off = 1,
 			ID = 1,
 			item_key = "prop_grade",
+			act_name = "祈福排行榜",
 			reward_item_key = "shop_gold_sum",
 			ext_reward_item_key = "shop_gold_sum",
 			rank_type = "lxjkh_024_jfphb_rank",
@@ -68,7 +69,9 @@ return {
 		[8]=
 		{
 			ID = 8,
-			award = 60000,
+			award = {60000,},
+			award_img = "ty_icon_flq2",
+			award_txt = {"60000福利券",},
 			limit = {1,1,},
 			need_num = 60000,
 			condition = "福气值达到6万",
@@ -76,7 +79,9 @@ return {
 		[9]=
 		{
 			ID = 9,
-			award = 15000,
+			award = {15000,},
+			award_img = "ty_icon_flq2",
+			award_txt = {"15000福利券",},
 			limit = {2,2},
 			need_num = 30000,
 			condition = "福气值达到3万",
@@ -84,7 +89,9 @@ return {
 		[10]=
 		{
 			ID = 10,
-			award = 5000,
+			award = {5000,},
+			award_img = "ty_icon_flq2",
+			award_txt = {"5000福利券",},
 			limit = {3,3,},
 			need_num = 15000,
 			condition = "福气值达到1.5万",
@@ -92,7 +99,9 @@ return {
 		[11]=
 		{
 			ID = 11,
-			award = 800,
+			award = {800,},
+			award_img = "ty_icon_flq2",
+			award_txt = {"800福利券",},
 			limit = {4,6,},
 			need_num = 8000,
 			condition = "福气值达到8000",
@@ -100,7 +109,9 @@ return {
 		[12]=
 		{
 			ID = 12,
-			award = 800,
+			award = {800,},
+			award_img = "ty_icon_flq2",
+			award_txt = {"800福利券",},
 			limit = {7,10,},
 			need_num = 8000,
 			condition = "福气值达到8000",
@@ -108,7 +119,9 @@ return {
 		[13]=
 		{
 			ID = 13,
-			award = 400,
+			award = {400,},
+			award_img = "ty_icon_flq2",
+			award_txt = {"400福利券",},
 			limit = {11,15,},
 			need_num = 3000,
 			condition = "福气值达到3000",
@@ -116,7 +129,9 @@ return {
 		[14]=
 		{
 			ID = 14,
-			award = 200,
+			award = {200,},
+			award_img = "ty_icon_flq2",
+			award_txt = {"200福利券",},
 			limit = {16,20,},
 			need_num = 1500,
 			condition = "福气值达到1500",
