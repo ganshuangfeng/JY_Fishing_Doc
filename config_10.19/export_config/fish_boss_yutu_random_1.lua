8362b7cf3b820fd1a64da74e6f43391ae3685027
denglu <denglu@qq.com>
2021-10-09 10:12:13 +0800

    欢乐券掉落

---
+++ config_10.19/export_config/fish_boss_yutu_random_1.lua
@@ -0,0 +1,537 @@
+return {
+	limit=
+	{
+		[1]=
+		{
+			no = 1,
+			max_num = 30,
+			max_act = 2,
+			max_hide = 1,
+			ywdj_max = 1,
+			boss_max = 1,
+			gsd_max = 1,
+		},
+	},
+	act_fish_limit=
+	{
+		[1]=
+		{
+			no = 1,
+			act_type = "activity",
+			act_id = 15,
+			judge_type = 2,
+			max_num = 5,
+			start_time = 1569886200,
+			end_time = 1570456800,
+		},
+		[2]=
+		{
+			no = 2,
+			act_type = "activity",
+			act_id = 16,
+			judge_type = 2,
+			max_num = 1,
+			start_time = 1569886200,
+			end_time = 1570456800,
+		},
+		[3]=
+		{
+			no = 3,
+			act_type = "activity",
+			act_id = 17,
+			judge_type = 2,
+			max_num = 1,
+			create_delay = 1200,
+			start_time = 1559186972,
+			end_time = 2555555555,
+		},
+		[4]=
+		{
+			no = 4,
+			act_type = "activity",
+			act_id = 21,
+			judge_type = 2,
+			max_num = 2,
+			start_time = 1569886200,
+			end_time = 2555555555,
+		},
+		[5]=
+		{
+			no = 5,
+			act_type = "activity",
+			act_id = 22,
+			judge_type = 2,
+			max_num = 2,
+			start_time = 1569886200,
+			end_time = 2555555555,
+		},
+		[6]=
+		{
+			no = 6,
+			act_type = "activity",
+			act_id = 23,
+			judge_type = 2,
+			max_num = 1,
+			start_time = 1569886200,
+			end_time = 2555555555,
+		},
+		[7]=
+		{
+			no = 7,
+			act_type = "activity",
+			act_id = 24,
+			judge_type = 2,
+			max_num = 1,
+			start_time = 1569886200,
+			end_time = 2555555555,
+		},
+	},
+	change_style=
+	{
+		[1]=
+		{
+			no = 1,
+			change_time_min = 400,
+			change_time_max = 600,
+		},
+	},
+	intensity_style=
+	{
+		[1]=
+		{
+			no = 1,
+			low = 1,
+			medium = 1,
+			high = 2,
+		},
+		[2]=
+		{
+			no = 2,
+			low = 0,
+			medium = 1,
+			high = 3,
+		},
+		[3]=
+		{
+			no = 3,
+			low = 0,
+			medium = 2,
+			high = 2,
+		},
+		[4]=
+		{
+			no = 4,
+			low = 1,
+			medium = 3,
+			high = 0,
+		},
+		[5]=
+		{
+			no = 5,
+			low = 0,
+			medium = 4,
+			high = 0,
+		},
+		[6]=
+		{
+			no = 6,
+			low = 1,
+			medium = 2,
+			high = 1,
+		},
+		[7]=
+		{
+			no = 7,
+			low = 2,
+			medium = 1,
+			high = 1,
+		},
+		[8]=
+		{
+			no = 8,
+			low = 2,
+			medium = 0,
+			high = 2,
+		},
+	},
+	create=
+	{
+		[1]=
+		{
+			no = 1,
+			choose_group = {1,},
+			create_pro = {5,5,5,},
+			create_config = "create_weight6",
+		},
+		[2]=
+		{
+			no = 2,
+			choose_group = {1,},
+			create_pro = {30,40,30},
+			create_config = "create_weight1",
+		},
+		[3]=
+		{
+			no = 3,
+			choose_group = {1,},
+			create_pro = {30,40,30},
+			create_config = "create_weight2",
+		},
+		[4]=
+		{
+			no = 4,
+			choose_group = {1,},
+			create_pro = {20,60,5},
+			create_config = "create_weight3",
+		},
+		[5]=
+		{
+			no = 5,
+			choose_group = {1,},
+			create_pro = {10,10,5},
+			create_config = "create_weight4",
+		},
+	},
+	create_style=
+	{
+		[1]=
+		{
+			no = 1,
+			low = 1,
+			medium = 2,
+			high = 2,
+		},
+		[2]=
+		{
+			no = 2,
+			low = 3,
+			medium = 1,
+			high = 1,
+		},
+		[3]=
+		{
+			no = 3,
+			low = 2,
+			medium = 2,
+			high = 1,
+		},
+		[4]=
+		{
+			no = 4,
+			low = 2,
+			medium = 2,
+			high = 1,
+		},
+		[5]=
+		{
+			no = 5,
+			low = 1,
+			medium = 3,
+			high = 1,
+		},
+		[6]=
+		{
+			no = 6,
+			low = 0,
+			medium = 4,
+			high = 1,
+		},
+		[7]=
+		{
+			no = 7,
+			low = 0,
+			medium = 3,
+			high = 2,
+		},
+		[8]=
+		{
+			no = 8,
+			low = 1,
+			medium = 2,
+			high = 2,
+		},
+		[9]=
+		{
+			no = 9,
+			low = 2,
+			medium = 1,
+			high = 2,
+		},
+	},
+	create_speed_cfg=
+	{
+		[1]=
+		{
+			no = 1,
+			min_time = 99999,
+			max_time = 99999,
+			type = 1,
+		},
+		[2]=
+		{
+			no = 2,
+			min_time = 99999,
+			max_time = 99999,
+			type = 2,
+		},
+		[3]=
+		{
+			no = 3,
+			min_time = 99999,
+			max_time = 99999,
+			type = 3,
+		},
+	},
+	create_weight1=
+	{
+		[1]=
+		{
+			no = 1,
+			weight = 1,
+		},
+		[2]=
+		{
+			no = 2,
+			weight = 1,
+		},
+		[3]=
+		{
+			no = 3,
+			weight = 1,
+		},
+		[4]=
+		{
+			no = 4,
+			weight = 1,
+		},
+		[5]=
+		{
+			no = 5,
+			weight = 1,
+		},
+	},
+	create_weight2=
+	{
+		[1]=
+		{
+			no = 1,
+			weight = 1,
+		},
+		[2]=
+		{
+			no = 2,
+			weight = 1,
+		},
+		[3]=
+		{
+			no = 3,
+			weight = 1,
+		},
+	},
+	create_weight3=
+	{
+		[1]=
+		{
+			no = 1,
+			weight = 1,
+		},
+		[2]=
+		{
+			no = 2,
+			weight = 1,
+		},
+		[3]=
+		{
+			no = 3,
+			weight = 1,
+		},
+	},
+	create_weight4=
+	{
+		[1]=
+		{
+			no = 1,
+			weight = 1,
+		},
+		[2]=
+		{
+			no = 2,
+			weight = 1,
+		},
+		[3]=
+		{
+			no = 3,
+			weight = 1,
+		},
+		[4]=
+		{
+			no = 4,
+			weight = 1,
+		},
+	},
+	create_weight5=
+	{
+		[1]=
+		{
+			no = 1,
+			weight = 1,
+		},
+		[2]=
+		{
+			no = 2,
+			weight = 1,
+		},
+		[3]=
+		{
+			no = 3,
+			weight = 1,
+		},
+	},
+	create_weight6=
+	{
+		[1]=
+		{
+			no = 1,
+			weight = 1,
+		},
+		[2]=
+		{
+			no = 2,
+			weight = 1,
+		},
+		[3]=
+		{
+			no = 3,
+			weight = 1,
+		},
+	},
+	interval=
+	{
+		[1]=
+		{
+			no = 1,
+			group_id = {34,36,},
+			min_time = 130,
+			max_time = 160,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+		},
+		[2]=
+		{
+			no = 2,
+			group_id = {37,38,},
+			min_time = 130,
+			max_time = 160,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+		},
+		[3]=
+		{
+			no = 3,
+			group_id = {39,40,41,},
+			min_time = 130,
+			max_time = 160,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+		},
+		[4]=
+		{
+			no = 4,
+			group_id = {18,19,20,21,22,23,24,25,},
+			min_time = 400,
+			max_time = 500,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+		},
+		[5]=
+		{
+			no = 5,
+			group_id = {35,42,43,44,45,46,47,48,49,49,50,50,},
+			min_time = 200,
+			max_time = 400,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+		},
+		[6]=
+		{
+			no = 6,
+			group_id = {1,2,3,4,5,6,7,8,9,10,11,64,65,66,1,2,3,4,5,6,7,8,9,10,11,26,27,},
+			min_time = 10,
+			max_time = 30,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+		},
+		[7]=
+		{
+			no = 7,
+			group_id = {28,29,30,31,32,33,},
+			min_time = 30,
+			max_time = 50,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+		},
+		[8]=
+		{
+			no = 8,
+			group_id = {131,132,133,134,135,136,},
+			min_time = 200,
+			max_time = 300,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+			start_time = 1634601600,
+			end_time = 1635177599,
+		},
+		[9]=
+		{
+			no = 9,
+			group_id = {131,132,133,134,135,136,},
+			min_time = 200,
+			max_time = 300,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+			start_time = 1634601600,
+			end_time = 1635177599,
+		},
+		[10]=
+		{
+			no = 10,
+			group_id = {131,132,133,134,135,136,},
+			min_time = 200,
+			max_time = 300,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+			start_time = 1634601600,
+			end_time = 1635177599,
+		},
+		[11]=
+		{
+			no = 11,
+			group_id = {131,132,133,134,135,136,},
+			min_time = 200,
+			max_time = 300,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+			start_time = 1634601600,
+			end_time = 1635177599,
+		},
+		[12]=
+		{
+			no = 12,
+			group_id = {128,129,130,},
+			min_time = 2400,
+			max_time = 2400,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+			start_time = 1634601600,
+			end_time = 1635177599,
+		},
+		[13]=
+		{
+			no = 13,
+			group_id = {137,},
+			min_time = 2400,
+			max_time = 2400,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+			start_time = 1623167999,
+			end_time = 1623254399,
+		},
+		[14]=
+		{
+			no = 14,
+			group_id = {138,},
+			min_time = 3000,
+			max_time = 3000,
+			path = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,},
+			start_time = 1941617584,
+			end_time = 2572819200,
+		},
+	},
+}
