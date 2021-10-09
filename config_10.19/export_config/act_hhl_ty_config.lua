24892b21bade93c8647f749862aca8257ec85875
denglu <denglu@qq.com>
2021-10-09 11:09:20 +0800

    欢乐兑奖

---
+++ config_10.19/export_config/act_hhl_ty_config.lua
@@ -0,0 +1,116 @@
+return {
+	config_infor=
+	{
+		[1]=
+		{
+			line = 1,
+			change_type = 57,
+			cur_path = "act_017_hhl_",
+			item_key = "prop_fish_drop_act_0",
+			beginTime = 1634601600,
+			endTime = 1635177599,
+			config = {1,2,3,4,5,6,7,8,9,10},
+			GotoUI = {"game_Fishing3DHall",},
+			help = "help_info",
+		},
+		[2]=
+		{
+			line = 2,
+			change_type = 57,
+			cur_path = "act_017_hhl_",
+			item_key = "prop_fish_drop_act_0",
+			beginTime = 1634601600,
+			endTime = 1635177599,
+			config = {1,2,3,4,5,6,7,8,9,10},
+			GotoUI = {"game_MiniGame",},
+			help = "help_info",
+		},
+	},
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			award_name = "2元话费",
+			award_image = "ty_icon_jb_15y",
+			item_cost_text = 1200,
+			type = 0,
+		},
+		[2]=
+		{
+			line = 2,
+			award_name = "50万小游戏币",
+			award_image = "ty_icon_flq3",
+			item_cost_text = 3000,
+			type = 0,
+		},
+		[3]=
+		{
+			line = 3,
+			award_name = "10元话费",
+			award_image = "ty_icon_flq4",
+			item_cost_text = 6000,
+			type = 0,
+		},
+		[4]=
+		{
+			line = 4,
+			award_name = "150万小游戏币",
+			award_image = "ty_icon_jb_30y",
+			item_cost_text = 9000,
+			type = 0,
+		},
+		[5]=
+		{
+			line = 5,
+			award_name = "20元话费",
+			award_image = "com_award_icon_hf100",
+			item_cost_text = 12000,
+			type = 0,
+		},
+		[6]=
+		{
+			line = 6,
+			award_name = "400万金币",
+			award_image = "com_award_icon_hf200",
+			item_cost_text = 24000,
+			type = 0,
+		},
+		[7]=
+		{
+			line = 7,
+			award_name = "50元话费",
+			award_image = "ty_icon_flq5",
+			item_cost_text = 30000,
+			type = 0,
+		},
+		[8]=
+		{
+			line = 8,
+			award_name = "700万金币",
+			award_image = "swjl_1",
+			item_cost_text = 42000,
+			type = 0,
+			condiy_key = "actp_prop_exchange_nor_v3",
+		},
+		[9]=
+		{
+			line = 9,
+			award_name = "100元话费",
+			item_cost_text = 60000,
+			type = 0,
+			condiy_key = "actp_prop_exchange_nor_v4",
+		},
+		[10]=
+		{
+			line = 10,
+			award_name = "500元话费",
+			item_cost_text = 300000,
+			type = 0,
+			condiy_key = "actp_prop_exchange_nor_v5",
+		},
+	},
+	shop_config=
+	{
+	},
+}
