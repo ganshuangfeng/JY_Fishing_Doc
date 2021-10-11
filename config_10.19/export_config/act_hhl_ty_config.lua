2c7e4efc6f0c7aa808a364e5c53007fcea5e8847
denglu <denglu@qq.com>
2021-10-11 11:14:55 +0800

    提交

--- config_10.19/export_config/act_hhl_ty_config.lua
+++ config_10.19/export_config/act_hhl_ty_config.lua
@@ -31,85 +31,85 @@ return {
 		[1]=
 		{
 			line = 1,
-			award_name = "2元话费",
-			award_image = "com_award_icon_hf2",
-			item_cost_text = 1200,
+			award_name = "500元话费",
+			award_image = "com_award_icon_hf500",
+			item_cost_text = 300000,
 			type = 0,
+			condiy_key = "actp_prop_exchange_v3",
 		},
 		[2]=
 		{
 			line = 2,
-			award_name = "50万小游戏币",
-			award_image = "ty_icon_yxb_4",
-			item_cost_text = 3000,
-			type = 0,
-		},
-		[3]=
-		{
-			line = 3,
-			award_name = "10元话费",
-			award_image = "com_award_icon_hf10",
-			item_cost_text = 6000,
-			type = 0,
-		},
-		[4]=
-		{
-			line = 4,
-			award_name = "150万小游戏币",
-			award_image = "ty_icon_yxb_5",
-			item_cost_text = 9000,
-			type = 0,
-		},
-		[5]=
-		{
-			line = 5,
-			award_name = "20元话费",
-			award_image = "com_award_icon_hf20",
-			item_cost_text = 12000,
-			type = 0,
-		},
-		[6]=
-		{
-			line = 6,
-			award_name = "400万金币",
-			award_image = "pay_icon_gold7",
-			item_cost_text = 24000,
-			type = 0,
-		},
-		[7]=
-		{
-			line = 7,
-			award_name = "50元话费",
-			award_image = "com_award_icon_hf50",
-			item_cost_text = 30000,
-			type = 0,
-		},
-		[8]=
-		{
-			line = 8,
-			award_name = "700万金币",
-			award_image = "pay_icon_gold8",
-			item_cost_text = 42000,
-			type = 0,
-			condiy_key = "actp_prop_exchange_v5",
-		},
-		[9]=
-		{
-			line = 9,
 			award_name = "100元话费",
 			award_image = "com_award_icon_hf100",
 			item_cost_text = 60000,
 			type = 0,
 			condiy_key = "actp_prop_exchange_v4",
 		},
+		[3]=
+		{
+			line = 3,
+			award_name = "700万金币",
+			award_image = "pay_icon_gold8",
+			item_cost_text = 42000,
+			type = 0,
+			condiy_key = "actp_prop_exchange_v5",
+		},
+		[4]=
+		{
+			line = 4,
+			award_name = "50元话费",
+			award_image = "com_award_icon_hf50",
+			item_cost_text = 30000,
+			type = 0,
+		},
+		[5]=
+		{
+			line = 5,
+			award_name = "400万金币",
+			award_image = "pay_icon_gold7",
+			item_cost_text = 24000,
+			type = 0,
+		},
+		[6]=
+		{
+			line = 6,
+			award_name = "20元话费",
+			award_image = "com_award_icon_hf20",
+			item_cost_text = 12000,
+			type = 0,
+		},
+		[7]=
+		{
+			line = 7,
+			award_name = "150万小游戏币",
+			award_image = "ty_icon_yxb_5",
+			item_cost_text = 9000,
+			type = 0,
+		},
+		[8]=
+		{
+			line = 8,
+			award_name = "10元话费",
+			award_image = "com_award_icon_hf10",
+			item_cost_text = 6000,
+			type = 0,
+		},
+		[9]=
+		{
+			line = 9,
+			award_name = "50万小游戏币",
+			award_image = "ty_icon_yxb_4",
+			item_cost_text = 3000,
+			type = 0,
+		},
 		[10]=
 		{
 			line = 10,
-			award_name = "500元话费",
-			award_image = "com_award_icon_hf500",
-			item_cost_text = 300000,
+			award_name = "2元话费",
+			award_image = "com_award_icon_hf2",
+			item_cost_text = 1200,
 			type = 0,
-			condiy_key = "actp_prop_exchange_v3",
 		},
 	},
 	shop_config=
