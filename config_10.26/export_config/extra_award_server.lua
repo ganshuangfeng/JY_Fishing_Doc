fe166bd3bb586a802779ce1414956a736ea5a690
denglu <denglu@qq.com>
2021-10-13 14:42:16 +0800

    提交

---
+++ config_10.26/export_config/extra_award_server.lua
@@ -0,0 +1,121 @@
+return {
+	task=
+	{
+		[1]=
+		{
+			id = 1,
+			task_id = 1000702,
+			asset_type = "_common_rank_ymshf_017_hldr_rank",
+			award_task_id = 1000703,
+			award_rate = {100,110,130,150},
+			extra_rank_type = "ymshf_017_hldr_rank",
+		},
+	},
+	activity_exchange=
+	{
+		[1]=
+		{
+			id = 1,
+			activity_id = 59,
+			award_id = 1,
+			task_id = 1000703,
+			award_rate = {1,1.1,1.3,1.5},
+		},
+		[2]=
+		{
+			id = 2,
+			activity_id = 59,
+			award_id = 2,
+			task_id = 1000703,
+			award_rate = {1,1.1,1.3,1.5},
+		},
+		[3]=
+		{
+			id = 3,
+			activity_id = 59,
+			award_id = 3,
+			task_id = 1000703,
+			award_rate = {1,1.1,1.3,1.5},
+		},
+		[4]=
+		{
+			id = 4,
+			activity_id = 59,
+			award_id = 4,
+			task_id = 1000703,
+			award_rate = {1,1.1,1.3,1.5},
+		},
+		[5]=
+		{
+			id = 5,
+			activity_id = 59,
+			award_id = 5,
+			task_id = 1000703,
+			award_rate = {1,1.1,1.3,1.5},
+		},
+		[6]=
+		{
+			id = 6,
+			activity_id = 59,
+			award_id = 6,
+			task_id = 1000703,
+			award_rate = {1,1.1,1.3,1.5},
+		},
+		[7]=
+		{
+			id = 7,
+			activity_id = 59,
+			award_id = 7,
+			task_id = 1000703,
+			award_rate = {1,1.1,1.3,1.5},
+		},
+		[8]=
+		{
+			id = 8,
+			activity_id = 59,
+			award_id = 8,
+			task_id = 1000703,
+			award_rate = {1,1.1,1.3,1.5},
+		},
+		[9]=
+		{
+			id = 9,
+			activity_id = 59,
+			award_id = 9,
+			task_id = 1000703,
+			award_rate = {1,1.1,1.3,1.5},
+		},
+		[10]=
+		{
+			id = 10,
+			activity_id = 59,
+			award_id = 10,
+			task_id = 1000703,
+			award_rate = {1,1.1,1.3,1.5},
+		},
+	},
+	box_exchange=
+	{
+	},
+	rank=
+	{
+		[1]=
+		{
+			id = 1,
+			rank_type = "ymshf_017_hldr_rank",
+			source_type = "buyu_3d_get_use_item",
+			task_id = 1000703,
+			award_rate = {100,110,130,150},
+			extra_rank_type = "ymshf_017_hldr_rank",
+		},
+		[2]=
+		{
+			id = 2,
+			rank_type = "ymshf_017_hldr_rank",
+			source_type = "buyu_get_use_item",
+			task_id = 1000703,
+			award_rate = {100,110,130,150},
+			extra_rank_type = "ymshf_017_hldr_rank",
+		},
+	},
+}
