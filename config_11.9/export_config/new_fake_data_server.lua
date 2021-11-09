2dbf4962cd979320de6eca598ea7e55037a778c6
denglu <denglu@qq.com>
2021-11-09 16:00:39 +0800

    提交

---
+++ config_11.9/export_config/new_fake_data_server.lua
@@ -0,0 +1,76 @@
+return {
+	main=
+	{
+		[1]=
+		{
+			id = 1,
+			data_type = "zhizhun_duanwei",
+			text = "玩家<color=#fffc01>{player}</color>段位晋升到{duanwei.1}，成功提高{duanwei.2}的奖池上限！",
+			weight = 100,
+		},
+	},
+	random_var=
+	{
+		[1]=
+		{
+			id = 1,
+			name = "duanwei",
+			v1 = "青铜Ⅰ",
+			v2 = "3万",
+			weight = 8,
+		},
+		[2]=
+		{
+			id = 2,
+			name = "duanwei",
+			v1 = "青铜Ⅱ",
+			v2 = "4万",
+			weight = 8,
+		},
+		[3]=
+		{
+			id = 3,
+			name = "duanwei",
+			v1 = "青铜Ⅲ",
+			v2 = "5万",
+			weight = 8,
+		},
+		[4]=
+		{
+			id = 4,
+			name = "duanwei",
+			v1 = "白银Ⅰ",
+			v2 = "6万",
+			weight = 8,
+		},
+		[5]=
+		{
+			id = 5,
+			name = "duanwei",
+			v1 = "白银Ⅱ",
+			v2 = "7万",
+			weight = 8,
+		},
+		[6]=
+		{
+			id = 6,
+			name = "duanwei",
+			v1 = "白银Ⅲ",
+			v2 = "8万",
+			weight = 8,
+		},
+		[7]=
+		{
+			id = 7,
+			name = "sample_num",
+			v1 = {3442,233234523},
+		},
+		[8]=
+		{
+			id = 8,
+			name = "sample_num2",
+			v1 = {3442,2333534},
+			v2 = {3442,44455},
+		},
+	},
+}
