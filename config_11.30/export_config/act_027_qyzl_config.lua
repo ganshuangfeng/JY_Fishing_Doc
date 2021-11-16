e526e8f2a6f3470fc2b64e24172f68a843a0ac97
denglu <denglu@qq.com>
2021-11-16 09:40:26 +0800

    十二月赠礼

---
+++ config_11.30/export_config/act_027_qyzl_config.lua
@@ -0,0 +1,260 @@
+return {
+	task=
+	{
+		[1]=
+		{
+			index = 1,
+			task_level = 1,
+			type = 0,
+			task_name = "累计消耗1万金币",
+			image = "ty_icon_jb_6y",
+			text = "100金币",
+		},
+		[2]=
+		{
+			index = 2,
+			task_level = 2,
+			type = 0,
+			task_name = "累计消耗10万金币",
+			image = "ty_icon_jb_6y",
+			text = "500金币",
+		},
+		[3]=
+		{
+			index = 3,
+			task_level = 3,
+			type = 0,
+			task_name = "累计消耗30万金币",
+			image = "ty_icon_jb_15y",
+			text = "1000金币",
+		},
+		[4]=
+		{
+			index = 4,
+			task_level = 1,
+			type = 1,
+			task_name = "商城累计充值6元",
+			image = "ty_icon_flq1",
+			text = "28福利券",
+		},
+		[5]=
+		{
+			index = 5,
+			task_level = 4,
+			type = 0,
+			task_name = "累计消耗100万金币",
+			image = "ty_icon_flq1",
+			text = "3福利券",
+		},
+		[6]=
+		{
+			index = 6,
+			task_level = 5,
+			type = 0,
+			task_name = "累计消耗300万金币",
+			image = "ty_icon_flq1",
+			text = "6福利券",
+		},
+		[7]=
+		{
+			index = 7,
+			task_level = 2,
+			type = 1,
+			task_name = "商城累计充值48元",
+			image = "ty_icon_flq1",
+			text = "188福利券",
+		},
+		[8]=
+		{
+			index = 8,
+			task_level = 6,
+			type = 0,
+			task_name = "累计消耗1000万金币",
+			image = "ty_icon_flq1",
+			text = "40福利券",
+		},
+		[9]=
+		{
+			index = 9,
+			task_level = 7,
+			type = 0,
+			task_name = "累计消耗2000万金币",
+			image = "ty_icon_flq1",
+			text = "60福利券",
+		},
+		[10]=
+		{
+			index = 10,
+			task_level = 3,
+			type = 1,
+			task_name = "商城累计充值198元",
+			image = "ty_icon_flq1",
+			text = "688福利券",
+		},
+		[11]=
+		{
+			index = 11,
+			task_level = 8,
+			type = 0,
+			task_name = "累计消耗3500万金币",
+			image = "ty_icon_flq1",
+			text = "80福利券",
+		},
+		[12]=
+		{
+			index = 12,
+			task_level = 9,
+			type = 0,
+			task_name = "累计消耗8000万金币",
+			image = "ty_icon_flq1",
+			text = "200福利券",
+		},
+		[13]=
+		{
+			index = 13,
+			task_level = 4,
+			type = 1,
+			task_name = "商城累计充值888元",
+			image = "ty_icon_flq2",
+			text = "2888福利券",
+		},
+		[14]=
+		{
+			index = 14,
+			task_level = 10,
+			type = 0,
+			task_name = "累计消耗2亿金币",
+			image = "ty_icon_flq1",
+			text = "500福利券",
+		},
+		[15]=
+		{
+			index = 15,
+			task_level = 11,
+			type = 0,
+			task_name = "累计消耗8亿金币",
+			image = "ty_icon_flq2",
+			text = "2000福利券",
+		},
+		[16]=
+		{
+			index = 16,
+			task_level = 5,
+			type = 1,
+			task_name = "商城累计充值1888元",
+			image = "ty_icon_flq2",
+			text = "5888福利券",
+		},
+		[17]=
+		{
+			index = 17,
+			task_level = 12,
+			type = 0,
+			task_name = "累计消耗20亿金币",
+			image = "ty_icon_flq2",
+			text = "8000福利券",
+		},
+		[18]=
+		{
+			index = 18,
+			task_level = 6,
+			type = 1,
+			task_name = "商城累计充值5888元",
+			image = "ty_icon_flq3",
+			text = "18888福利券",
+		},
+		[19]=
+		{
+			index = 19,
+			task_level = 13,
+			type = 0,
+			task_name = "累计消耗40亿金币",
+			image = "ty_icon_flq3",
+			text = "12000福利券",
+		},
+		[20]=
+		{
+			index = 20,
+			task_level = 7,
+			type = 1,
+			task_name = "商城累计充值10000元",
+			image = "ty_icon_flq3",
+			text = "21888福利券",
+		},
+		[21]=
+		{
+			index = 21,
+			task_level = 14,
+			type = 0,
+			task_name = "累计消耗80亿金币",
+			image = "ty_icon_flq3",
+			text = "20000福利券",
+		},
+		[22]=
+		{
+			index = 22,
+			task_level = 8,
+			type = 1,
+			task_name = "商城累计充值18888元",
+			image = "ty_icon_flq4",
+			text = "48888福利券",
+		},
+		[23]=
+		{
+			index = 23,
+			task_level = 15,
+			type = 0,
+			task_name = "累计消耗200亿金币",
+			image = "ty_icon_flq4",
+			text = "50000福利券",
+		},
+	},
+	other_data=
+	{
+		[1]=
+		{
+			line = 1,
+			cz_permiss = "actp_own_task_p_hljnh_046_hlfl_cjj",
+			ljyj_permiss = "actp_own_task_p_hljnh_046_hlfl_cjj",
+			cz_task_id = 1000698,
+			taskid = 1000697,
+			end_time = 1640620799,
+			sta_time = 1638230400,
+			condiy_key = "actp_own_task_p_hljnh_046_hlfl_cjj",
+			btn_gotoui = "game_MiniGame",
+		},
+	},
+	help_Info=
+	{
+		[1]=
+		{
+			line = 1,
+			content = "1.活动时间：11月30日08:00:00—12月27日23:59:59。",
+		},
+		[2]=
+		{
+			line = 2,
+			content = "2. 所有任务奖励必须先领取前一个任务的奖励后才能领取。",
+		},
+		[3]=
+		{
+			line = 3,
+			content = "3.充值任务只记录充值商城中的充值，不包含活动栏和推荐必买栏目。",
+		},
+		[4]=
+		{
+			line = 4,
+			content = "4.苹果大战的数据不计入活动统计。",
+		},
+		[5]=
+		{
+			line = 5,
+			content = "5.疯狂捕鱼数据只计50%。",
+		},
+		[6]=
+		{
+			line = 6,
+			content = "6.活动结束后，未领取的奖励视为自动放弃。",
+		},
+	},
+}
