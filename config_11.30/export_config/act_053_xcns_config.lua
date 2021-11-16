b45c7848ecf5d5ed632c44183087281ea280af9b
denglu <denglu@qq.com>
2021-11-16 09:57:08 +0800

    捕鱼能手，消除能手，累充返利

---
+++ config_11.30/export_config/act_053_xcns_config.lua
@@ -0,0 +1,97 @@
+return {
+	otherInfo=
+	{
+		[1]=
+		{
+			line = 1,
+			task_id = 1000688,
+			startTime = 1638230400,
+			endTime = 1638806399,
+			gotoui = "game_MiniGame",
+			downDes = "每日0点重置倍数和奖励\n统计在任意消消乐游戏中24万及以上档次消除的倍数",
+		},
+	},
+	awardInfo=
+	{
+		[1]=
+		{
+			index = 1,
+			need_num = 10,
+			award_name = "福利券",
+			award_amount = "8~50",
+			award_icon = "ty_icon_flq3",
+		},
+		[2]=
+		{
+			index = 2,
+			need_num = 20,
+			award_name = "小游戏币",
+			award_amount = "8000~5万",
+			award_icon = "ty_icon_yxb_1",
+		},
+		[3]=
+		{
+			index = 3,
+			need_num = 50,
+			award_name = "福利券",
+			award_amount = 35,
+			award_icon = "ty_icon_flq3",
+		},
+		[4]=
+		{
+			index = 4,
+			need_num = 100,
+			award_name = "福利券",
+			award_amount = "50~200",
+			award_icon = "ty_icon_flq3",
+		},
+		[5]=
+		{
+			index = 5,
+			need_num = 200,
+			award_name = "金币",
+			award_amount = "8万~30万",
+			award_icon = "ty_icon_jb_15y",
+		},
+		[6]=
+		{
+			index = 6,
+			need_num = 400,
+			award_name = "小游戏币",
+			award_amount = "18万~80万",
+			award_icon = "ty_icon_yxb_2",
+		},
+		[7]=
+		{
+			index = 7,
+			need_num = 800,
+			award_name = "福利券",
+			award_amount = "400~2000",
+			award_icon = "ty_icon_flq4",
+		},
+		[8]=
+		{
+			index = 8,
+			need_num = 1200,
+			award_name = "金币",
+			award_amount = "40万~220万",
+			award_icon = "ty_icon_jb_18y",
+		},
+		[9]=
+		{
+			index = 9,
+			need_num = 2000,
+			award_name = "金币",
+			award_amount = "80万~500万",
+			award_icon = "ty_icon_jb_30y",
+		},
+		[10]=
+		{
+			index = 10,
+			need_num = 3000,
+			award_name = "福利券",
+			award_amount = "800~3000",
+			award_icon = "ty_icon_flq4",
+		},
+	},
+}
