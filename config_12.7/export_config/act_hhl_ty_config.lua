705dd957f10b3ba33d3364af874bedd20ee2a736
denglu <denglu@qq.com>
2021-11-24 10:11:42 +0800

    加成礼包，雪球兑换，雪球排行榜

---
+++ config_12.7/export_config/act_hhl_ty_config.lua
@@ -0,0 +1,120 @@
+return {
+	config_infor=
+	{
+		[1]=
+		{
+			line = 1,
+			condiy_key = "cpl_notcjj",
+			change_type = 57,
+			cur_path = "act_021_hhl_",
+			item_key = "prop_fish_drop_act_0",
+			beginTime = 1638835200,
+			endTime = 1639411199,
+			config = {1,2,3,4,5,6,7,8,9,10},
+			GotoUI = {"game_Fishing3DHall",},
+			help = "help_info",
+		},
+		[2]=
+		{
+			line = 2,
+			condiy_key = "cpl_cjj",
+			change_type = 57,
+			cur_path = "act_021_hhl_",
+			item_key = "prop_fish_drop_act_0",
+			beginTime = 1638835200,
+			endTime = 1639411199,
+			config = {1,2,3,4,5,6,7,8,9,10},
+			GotoUI = {"game_MiniGame",},
+			help = "help_info",
+		},
+	},
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			award_name = "5万福利券",
+			award_image = "ty_icon_flq5",
+			item_cost_text = 300000,
+			type = 0,
+			condiy_key = "actp_prop_exchange_v5",
+		},
+		[2]=
+		{
+			line = 2,
+			award_name = "1万福利券",
+			award_image = "ty_icon_flq4",
+			item_cost_text = 60000,
+			type = 0,
+			condiy_key = "actp_prop_exchange_v4",
+		},
+		[3]=
+		{
+			line = 3,
+			award_name = "700万金币",
+			award_image = "pay_icon_gold8",
+			item_cost_text = 42000,
+			type = 0,
+			condiy_key = "actp_prop_exchange_v3",
+		},
+		[4]=
+		{
+			line = 4,
+			award_name = "5000福利券",
+			award_image = "ty_icon_flq3",
+			item_cost_text = 30000,
+			type = 0,
+		},
+		[5]=
+		{
+			line = 5,
+			award_name = "400万金币",
+			award_image = "pay_icon_gold7",
+			item_cost_text = 24000,
+			type = 0,
+		},
+		[6]=
+		{
+			line = 6,
+			award_name = "2000福利券",
+			award_image = "ty_icon_flq2",
+			item_cost_text = 12000,
+			type = 0,
+		},
+		[7]=
+		{
+			line = 7,
+			award_name = "150万小游戏币",
+			award_image = "ty_icon_yxb_5",
+			item_cost_text = 9000,
+			type = 0,
+		},
+		[8]=
+		{
+			line = 8,
+			award_name = "1000福利券",
+			award_image = "ty_icon_flq2",
+			item_cost_text = 6000,
+			type = 0,
+		},
+		[9]=
+		{
+			line = 9,
+			award_name = "50万小游戏币",
+			award_image = "ty_icon_yxb_4",
+			item_cost_text = 3000,
+			type = 0,
+		},
+		[10]=
+		{
+			line = 10,
+			award_name = "200福利券",
+			award_image = "ty_icon_flq1",
+			item_cost_text = 1200,
+			type = 0,
+		},
+	},
+	shop_config=
+	{
+	},
+}
