01896bc5300421a1d4a4995a777014b4c09a4d55
denglu <denglu@qq.com>
2021-11-30 15:48:43 +0800

    至尊排位

---
+++ config_12.14/export_config/zhizhun_rank_match_config.lua
@@ -0,0 +1,723 @@
+return {
+	cell_award=
+	{
+		[1]=
+		{
+			id = 1,
+			tong_1 = {5,5},
+			tong_2_3 = {5,10},
+			yin_1_3 = {2,10000},
+			huangjin_1_3 = {2,50000},
+			baijin_1_3 = {2,100000},
+			zuanshi_1_3 = {2,150000},
+			zhizhun = {2,200000},
+		},
+		[2]=
+		{
+			id = 2,
+		},
+		[3]=
+		{
+			id = 3,
+			tong_1 = {2,500},
+			tong_2_3 = {2,1000},
+			yin_1_3 = {2,15000},
+			huangjin_1_3 = {2,80000},
+			baijin_1_3 = {2,100000},
+			zuanshi_1_3 = {2,200000},
+			zhizhun = {2,300000},
+		},
+		[4]=
+		{
+			id = 4,
+			tong_1 = {2,1000},
+			tong_2_3 = {2,2000},
+			yin_1_3 = {2,5000},
+			huangjin_1_3 = {2,30000},
+			baijin_1_3 = {2,200000},
+			zuanshi_1_3 = {2,200000},
+			zhizhun = {2,200000},
+		},
+		[5]=
+		{
+			id = 5,
+			tong_1 = {4,5},
+			tong_2_3 = {4,10},
+			yin_1_3 = {2,20000},
+			huangjin_1_3 = {2,40000},
+			baijin_1_3 = {2,100000},
+			zuanshi_1_3 = {2,150000},
+			zhizhun = {2,300000},
+		},
+		[6]=
+		{
+			id = 6,
+			tong_1 = {1,1000},
+			tong_2_3 = {1,10000},
+			yin_1_3 = {1,40000},
+			huangjin_1_3 = {1,300000},
+			baijin_1_3 = {1,800000},
+			zuanshi_1_3 = {1,2000000},
+			zhizhun = {1,5000000},
+		},
+		[7]=
+		{
+			id = 7,
+		},
+		[8]=
+		{
+			id = 8,
+			tong_1 = {3,5},
+			tong_2_3 = {3,10},
+			yin_1_3 = {2,20000},
+			huangjin_1_3 = {2,100000},
+			baijin_1_3 = {2,200000},
+			zuanshi_1_3 = {2,250000},
+			zhizhun = {2,400000},
+		},
+		[9]=
+		{
+			id = 9,
+			tong_1 = {6,5},
+			tong_2_3 = {6,10},
+			yin_1_3 = {2,40000},
+			huangjin_1_3 = {2,120000},
+			baijin_1_3 = {2,200000},
+			zuanshi_1_3 = {2,300000},
+			zhizhun = {2,600000},
+		},
+		[10]=
+		{
+			id = 10,
+			tong_1 = {2,500},
+			tong_2_3 = {2,1000},
+			yin_1_3 = {2,10000},
+			huangjin_1_3 = {2,80000},
+			baijin_1_3 = {2,300000},
+			zuanshi_1_3 = {2,250000},
+			zhizhun = {2,300000},
+		},
+		[11]=
+		{
+			id = 11,
+			tong_1 = {2,500},
+			tong_2_3 = {2,1000},
+			yin_1_3 = {2,20000},
+			huangjin_1_3 = {2,50000},
+			baijin_1_3 = {2,100000},
+			zuanshi_1_3 = {2,300000},
+			zhizhun = {2,200000},
+		},
+		[12]=
+		{
+			id = 12,
+			tong_1 = {2,1500},
+			tong_2_3 = {2,3000},
+			yin_1_3 = {2,10000},
+			huangjin_1_3 = {2,50000},
+			baijin_1_3 = {2,200000},
+			zuanshi_1_3 = {2,300000},
+			zhizhun = {2,500000},
+		},
+		[13]=
+		{
+			id = 13,
+		},
+		[14]=
+		{
+			id = 14,
+			tong_1 = {1,10000},
+			tong_2_3 = {1,15000},
+			yin_1_3 = {1,60000},
+			huangjin_1_3 = {1,400000},
+			baijin_1_3 = {1,1200000},
+			zuanshi_1_3 = {1,3000000},
+			zhizhun = {1,8000000},
+		},
+		[15]=
+		{
+			id = 15,
+			tong_1 = {2,3000},
+			tong_2_3 = {2,5000},
+			yin_1_3 = {2,30000},
+			huangjin_1_3 = {2,150000},
+			baijin_1_3 = {2,300000},
+			zuanshi_1_3 = {2,500000},
+			zhizhun = {2,800000},
+		},
+		[16]=
+		{
+			id = 16,
+			tong_1 = {2,5000},
+			tong_2_3 = {2,10000},
+			yin_1_3 = {2,50000},
+			huangjin_1_3 = {2,100000},
+			baijin_1_3 = {2,400000},
+			zuanshi_1_3 = {2,400000},
+			zhizhun = {2,500000},
+		},
+		[17]=
+		{
+			id = 17,
+			tong_1 = {2,8000},
+			tong_2_3 = {2,10000},
+			yin_1_3 = {2,20000},
+			huangjin_1_3 = {2,200000},
+			baijin_1_3 = {2,500000},
+			zuanshi_1_3 = {2,600000},
+			zhizhun = {2,1000000},
+		},
+		[18]=
+		{
+			id = 18,
+			tong_1 = {2,3000},
+			tong_2_3 = {2,5000},
+			yin_1_3 = {2,50000},
+			huangjin_1_3 = {2,150000},
+			baijin_1_3 = {2,300000},
+			zuanshi_1_3 = {2,600000},
+			zhizhun = {2,600000},
+		},
+		[19]=
+		{
+			id = 19,
+		},
+		[20]=
+		{
+			id = 20,
+			tong_1 = {1,15000},
+			tong_2_3 = {1,20000},
+			yin_1_3 = {1,100000},
+			huangjin_1_3 = {1,500000},
+			baijin_1_3 = {1,2000000},
+			zuanshi_1_3 = {1,5000000},
+			zhizhun = {1,10000000},
+		},
+		[21]=
+		{
+			id = 21,
+			tong_1 = {2,10000},
+			tong_2_3 = {2,20000},
+			yin_1_3 = {2,50000},
+			huangjin_1_3 = {2,100000},
+			baijin_1_3 = {2,400000},
+			zuanshi_1_3 = {2,500000},
+			zhizhun = {2,1000000},
+		},
+		[22]=
+		{
+			id = 22,
+			tong_1 = {2,2000},
+			tong_2_3 = {2,4000},
+			yin_1_3 = {2,20000},
+			huangjin_1_3 = {2,200000},
+			baijin_1_3 = {2,300000},
+			zuanshi_1_3 = {2,600000},
+			zhizhun = {2,800000},
+		},
+		[23]=
+		{
+			id = 23,
+			tong_1 = {2,10000},
+			tong_2_3 = {2,20000},
+			yin_1_3 = {2,30000},
+			huangjin_1_3 = {2,150000},
+			baijin_1_3 = {2,500000},
+			zuanshi_1_3 = {2,800000},
+			zhizhun = {2,600000},
+		},
+		[24]=
+		{
+			id = 24,
+			tong_1 = {2,5000},
+			tong_2_3 = {2,10000},
+			yin_1_3 = {2,20000},
+			huangjin_1_3 = {2,100000},
+			baijin_1_3 = {2,500000},
+			zuanshi_1_3 = {2,400000},
+			zhizhun = {2,1000000},
+		},
+		[25]=
+		{
+			id = 25,
+			tong_1 = {2,10000},
+			tong_2_3 = {2,20000},
+			yin_1_3 = {2,80000},
+			huangjin_1_3 = {2,250000},
+			baijin_1_3 = {2,300000},
+			zuanshi_1_3 = {2,500000},
+			zhizhun = {2,1000000},
+		},
+	},
+	main=
+	{
+		[1]=
+		{
+			no = 1,
+			rank_type = "supreme_ranking_rank",
+		},
+	},
+	asset_type=
+	{
+		[1]=
+		{
+			asset_id = 1,
+			asset_type = "add_pool",
+		},
+		[2]=
+		{
+			asset_id = 2,
+			asset_type = "jing_bi",
+		},
+		[3]=
+		{
+			asset_id = 3,
+			asset_type = "prop_3d_fish_wild",
+		},
+		[4]=
+		{
+			asset_id = 4,
+			asset_type = "prop_3d_fish_frozen",
+		},
+		[5]=
+		{
+			asset_id = 5,
+			asset_type = "prop_3d_fish_lock",
+		},
+		[6]=
+		{
+			asset_id = 6,
+			asset_type = "prop_3d_fish_doubled",
+		},
+	},
+	duanwei=
+	{
+		[1]=
+		{
+			id = 1,
+			duanwei = "tong",
+			name = "青铜Ⅰ",
+			grade = 1,
+			exp = 0,
+			award_pool = 100000,
+			award_pool_gailv = 1,
+			process_cfg = {1,2,3,4,5},
+		},
+		[2]=
+		{
+			id = 2,
+			duanwei = "tong",
+			name = "青铜Ⅱ",
+			grade = 2,
+			exp = 200,
+			award_pool = 50000,
+			award_pool_gailv = 2,
+			process_cfg = {6,7,8,9,10},
+		},
+		[3]=
+		{
+			id = 3,
+			duanwei = "tong",
+			name = "青铜Ⅲ",
+			grade = 3,
+			exp = 400,
+			award_pool = 50000,
+			award_pool_gailv = 2,
+			process_cfg = {6,7,8,9,10},
+		},
+		[4]=
+		{
+			id = 4,
+			duanwei = "yin",
+			name = "白银Ⅰ",
+			grade = 1,
+			exp = 700,
+			award_pool = 300000,
+			award_pool_gailv = 2,
+			process_cfg = {11,12,13,14,15},
+		},
+		[5]=
+		{
+			id = 5,
+			duanwei = "yin",
+			name = "白银Ⅱ",
+			grade = 2,
+			exp = 1000,
+			award_pool = 250000,
+			award_pool_gailv = 2,
+			process_cfg = {11,12,13,14,15},
+		},
+		[6]=
+		{
+			id = 6,
+			duanwei = "yin",
+			name = "白银Ⅲ",
+			grade = 3,
+			exp = 1300,
+			award_pool = 250000,
+			award_pool_gailv = 2,
+			process_cfg = {11,12,13,14,15},
+		},
+		[7]=
+		{
+			id = 7,
+			duanwei = "huangjin",
+			name = "黄金Ⅰ",
+			grade = 1,
+			exp = 1700,
+			award_pool = 2000000,
+			award_pool_gailv = 3,
+			process_cfg = {16,17,18,19,20,21},
+		},
+		[8]=
+		{
+			id = 8,
+			duanwei = "huangjin",
+			name = "黄金Ⅱ",
+			grade = 2,
+			exp = 2100,
+			award_pool = 1500000,
+			award_pool_gailv = 3,
+			process_cfg = {16,17,18,19,20,21},
+		},
+		[9]=
+		{
+			id = 9,
+			duanwei = "huangjin",
+			name = "黄金Ⅲ",
+			grade = 3,
+			exp = 2500,
+			award_pool = 1500000,
+			award_pool_gailv = 3,
+			process_cfg = {16,17,18,19,20,21},
+		},
+		[10]=
+		{
+			id = 10,
+			duanwei = "baijin",
+			name = "白金Ⅰ",
+			grade = 1,
+			exp = 3000,
+			award_pool = 4000000,
+			award_pool_gailv = 3,
+			process_cfg = {22,23,24,25,26,27},
+		},
+		[11]=
+		{
+			id = 11,
+			duanwei = "baijin",
+			name = "白金Ⅱ",
+			grade = 2,
+			exp = 3600,
+			award_pool = 5000000,
+			award_pool_gailv = 3,
+			process_cfg = {22,23,24,25,26,27},
+		},
+		[12]=
+		{
+			id = 12,
+			duanwei = "baijin",
+			name = "白金Ⅲ",
+			grade = 3,
+			exp = 4200,
+			award_pool = 5000000,
+			award_pool_gailv = 3,
+			process_cfg = {22,23,24,25,26,27},
+		},
+		[13]=
+		{
+			id = 13,
+			duanwei = "zuanshi",
+			name = "钻石Ⅰ",
+			grade = 1,
+			exp = 5000,
+			award_pool = 10000000,
+			award_pool_gailv = 4,
+			process_cfg = {28,29,30,31,32,33},
+		},
+		[14]=
+		{
+			id = 14,
+			duanwei = "zuanshi",
+			name = "钻石Ⅱ",
+			grade = 2,
+			exp = 6500,
+			award_pool = 10000000,
+			award_pool_gailv = 4,
+			process_cfg = {28,29,30,31,32,33},
+		},
+		[15]=
+		{
+			id = 15,
+			duanwei = "zuanshi",
+			name = "钻石Ⅲ",
+			grade = 3,
+			exp = 8000,
+			award_pool = 10000000,
+			award_pool_gailv = 4,
+			process_cfg = {28,29,30,31,32,33},
+		},
+		[16]=
+		{
+			id = 16,
+			duanwei = "zhizhun",
+			name = "至尊",
+			grade = 1,
+			exp = 10000,
+			award_pool = 50000000,
+			award_pool_gailv = 4,
+			process_cfg = {34,35,36,37,38,39},
+		},
+	},
+	take_gailv=
+	{
+		[1]=
+		{
+			id = 1,
+			gailv_id = 1,
+			percent = 5,
+			gailv = 0,
+		},
+		[2]=
+		{
+			id = 2,
+			gailv_id = 1,
+			percent = 10,
+			gailv = 100,
+		},
+		[3]=
+		{
+			id = 3,
+			gailv_id = 1,
+			percent = 15,
+			gailv = 0,
+		},
+		[4]=
+		{
+			id = 4,
+			gailv_id = 1,
+			percent = 20,
+			gailv = 0,
+		},
+		[5]=
+		{
+			id = 5,
+			gailv_id = 2,
+			percent = 5,
+			gailv = 10,
+		},
+		[6]=
+		{
+			id = 6,
+			gailv_id = 2,
+			percent = 10,
+			gailv = 40,
+		},
+		[7]=
+		{
+			id = 7,
+			gailv_id = 2,
+			percent = 15,
+			gailv = 35,
+		},
+		[8]=
+		{
+			id = 8,
+			gailv_id = 2,
+			percent = 20,
+			gailv = 15,
+		},
+		[9]=
+		{
+			id = 9,
+			gailv_id = 3,
+			percent = 5,
+			gailv = 10,
+		},
+		[10]=
+		{
+			id = 10,
+			gailv_id = 3,
+			percent = 10,
+			gailv = 40,
+		},
+		[11]=
+		{
+			id = 11,
+			gailv_id = 3,
+			percent = 15,
+			gailv = 40,
+		},
+		[12]=
+		{
+			id = 12,
+			gailv_id = 3,
+			percent = 20,
+			gailv = 10,
+		},
+		[13]=
+		{
+			id = 13,
+			gailv_id = 4,
+			percent = 5,
+			gailv = 15,
+		},
+		[14]=
+		{
+			id = 14,
+			gailv_id = 4,
+			percent = 10,
+			gailv = 45,
+		},
+		[15]=
+		{
+			id = 15,
+			gailv_id = 4,
+			percent = 15,
+			gailv = 35,
+		},
+		[16]=
+		{
+			id = 16,
+			gailv_id = 4,
+			percent = 20,
+			gailv = 5,
+		},
+	},
+	dice_gailv=
+	{
+		[1]=
+		{
+			step_num = 1,
+			gailv = 0.015700,
+		},
+		[2]=
+		{
+			step_num = 2,
+			gailv = 0.034291,
+		},
+		[3]=
+		{
+			step_num = 3,
+			gailv = 0.151893,
+		},
+		[4]=
+		{
+			step_num = 4,
+			gailv = 0.721167,
+		},
+		[5]=
+		{
+			step_num = 5,
+			gailv = 0.055921,
+		},
+		[6]=
+		{
+			step_num = 6,
+			gailv = 0.021027,
+		},
+		[7]=
+		{
+			step_num = 7,
+			gailv = 0.011346,
+		},
+		[8]=
+		{
+			step_num = 8,
+			gailv = 0.007222,
+		},
+		[9]=
+		{
+			step_num = 9,
+			gailv = 0.005056,
+		},
+		[10]=
+		{
+			step_num = 10,
+			gailv = 0.003766,
+		},
+		[11]=
+		{
+			step_num = 11,
+			gailv = 0.002930,
+		},
+		[12]=
+		{
+			step_num = 12,
+			gailv = 0.002355,
+		},
+		[13]=
+		{
+			step_num = 13,
+			gailv = 0.001940,
+		},
+		[14]=
+		{
+			step_num = 14,
+			gailv = 0.001631,
+		},
+		[15]=
+		{
+			step_num = 15,
+			gailv = 0.001393,
+		},
+		[16]=
+		{
+			step_num = 16,
+			gailv = 0.001205,
+		},
+		[17]=
+		{
+			step_num = 17,
+			gailv = 0.001055,
+		},
+		[18]=
+		{
+			step_num = 18,
+			gailv = 0.000933,
+		},
+		[19]=
+		{
+			step_num = 19,
+			gailv = 0.000831,
+		},
+		[20]=
+		{
+			step_num = 20,
+			gailv = 0.000747,
+		},
+	},
+	base_task_id=
+	{
+		[1]=
+		{
+			no = 1,
+			task_id = 1000809,
+		},
+		[2]=
+		{
+			no = 2,
+			task_id = 1000810,
+		},
+		[3]=
+		{
+			no = 3,
+			task_id = 1000811,
+		},
+		[4]=
+		{
+			no = 4,
+			task_id = 1000812,
+		},
+		[5]=
+		{
+			no = 5,
+			task_id = 1000813,
+		},
+		[6]=
+		{
+			no = 6,
+			task_id = 1000814,
+		},
+	},
+}
