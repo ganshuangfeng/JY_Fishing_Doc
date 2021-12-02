c81ff319320552a25704d2c3ed408aeae790c96c
denglu <denglu@qq.com>
2021-12-02 11:40:44 +0800

    萌宠消消乐相关调整

---
+++ config_debug/export_config/act_022_qflb_config.lua
@@ -0,0 +1,117 @@
+return {
+	config_key=
+	{
+		[1]=
+		{
+			permission = "cpl_notcjj",
+			week_return = 1,
+			month_return = 2,
+			quarter_return = 3,
+			help_info = 1,
+		},
+		[2]=
+		{
+			permission = "cpl_cjj",
+			week_return = 4,
+			month_return = 5,
+			quarter_return = 6,
+			help_info = 2,
+		},
+	},
+	config_value=
+	{
+		[1]=
+		{
+			gift_id = 10084,
+			task_id = 78,
+			below_title = {150000,700},
+			price = 6,
+			get_award_now = 150000,
+			day_num = 7,
+			task_desc = "3D捕鱼中累计消耗<color=#FFF950>5万</color>",
+			day_award_desc = {"100福利券","锁定x1",},
+			day_award_img = {"ty_icon_flq3","3dby_btn_sd",},
+			gotoui = "game_Fishing3DHall",
+		},
+		[2]=
+		{
+			gift_id = 10085,
+			task_id = 79,
+			below_title = {2200000,3000},
+			price = 30,
+			get_award_now = 1000000,
+			day_num = 30,
+			task_desc = "3D捕鱼中累计消耗<color=#FFF950>50万</color>",
+			day_award_desc = {"100福利券","4万金币","锁定x1",},
+			day_award_img = {"ty_icon_flq3","ty_icon_jb","3dby_btn_sd",},
+			gotoui = "game_Fishing3DHall",
+		},
+		[3]=
+		{
+			gift_id = 10086,
+			task_id = 80,
+			below_title = {10500000,19800},
+			price = 198,
+			get_award_now = 6000000,
+			day_num = 90,
+			task_desc = "小游戏累计赢金<color=#FFF950>200万</color>",
+			day_award_desc = {"220福利券","5万金币","锁定x1",},
+			day_award_img = {"ty_icon_flq3","ty_icon_jb","3dby_btn_sd",},
+			gotoui = "game_MiniGame",
+		},
+		[4]=
+		{
+			gift_id = 10084,
+			task_id = 90,
+			below_title = {150000,700},
+			price = 6,
+			get_award_now = 150000,
+			day_num = 7,
+			task_desc = "萌宠消消乐累计赢金<color=#FFF950>5万</color>",
+			day_award_desc = {"100福利券","玩具锤x1",},
+			day_award_img = {"ty_icon_flq3","zjd_icon_wjc",},
+			gotoui = "game_Eliminate",
+		},
+		[5]=
+		{
+			gift_id = 10085,
+			task_id = 91,
+			below_title = {2200000,3000},
+			price = 30,
+			get_award_now = 1000000,
+			day_num = 30,
+			task_desc = "萌宠消消乐累计赢金<color=#FFF950>50万</color>",
+			day_award_desc = {"100福利券","4万金币","玩具锤x1",},
+			day_award_img = {"ty_icon_flq3","ty_icon_jb","zjd_icon_wjc",},
+			gotoui = "game_Eliminate",
+		},
+		[6]=
+		{
+			gift_id = 10086,
+			task_id = 92,
+			below_title = {10500000,19800},
+			price = 198,
+			get_award_now = 6000000,
+			day_num = 90,
+			task_desc = "萌宠消消乐累计赢金<color=#FFF950>200万</color>",
+			day_award_desc = {"220福利券","5万金币","玩具锤x1",},
+			day_award_img = {"ty_icon_flq3","ty_icon_jb","zjd_icon_wjc",},
+			gotoui = "game_Eliminate",
+		},
+	},
+	help_info=
+	{
+		[1]=
+		{
+			[1] = "1.购买后立得指定金币数，并激活赢金任务",
+			[2] = "2.任务有效期内，每天完成任务可领取1次奖励，超时未完成，视为放弃",
+			[3] = "3.不同种类的礼包可叠加购买领奖",
+		},
+		[2]=
+		{
+			[1] = "1.购买后立得指定金币数，并激活赢金任务",
+			[2] = "2.任务有效期内，每天完成任务可领取1次奖励，超时未完成，视为放弃",
+			[3] = "3.不同种类的礼包可叠加购买领奖",
+		},
+	},
+}
---
+++ config_debug/export_config/act_xrxsfl_config.lua
@@ -0,0 +1,245 @@
+return {
+	permission=
+	{
+		[1]=
+		{
+			line = 1,
+			permission = "cpl_notcjj",
+			task = {1,2,3,4,5,6},
+			cj = {8,1,2,3,4,5,6,7},
+			item_key = "prop_xrxsfl_cjq",
+			activity_exchange_type = 43,
+		},
+		[2]=
+		{
+			line = 2,
+			permission = "cpl_cjj",
+			task = {7,8,9,10,11,12},
+			cj = {16,9,10,11,12,13,14,15},
+			item_key = "prop_xrxsfl_cjq",
+			activity_exchange_type = 44,
+		},
+	},
+	task=
+	{
+		[1]=
+		{
+			ID = 1,
+			task_id = 1000352,
+			task_desc = "使用1次锁定",
+			award_desc = "抽奖次数+1",
+		},
+		[2]=
+		{
+			ID = 2,
+			task_id = 1000353,
+			task_desc = "累计捕获20条鱼",
+			award_desc = "抽奖次数+1",
+		},
+		[3]=
+		{
+			ID = 3,
+			task_id = 1000354,
+			task_desc = "累计捕获50条鱼",
+			award_desc = "抽奖次数+1",
+		},
+		[4]=
+		{
+			ID = 4,
+			task_id = 1000355,
+			task_desc = "累计捕获100条鱼",
+			award_desc = "抽奖次数+1",
+		},
+		[5]=
+		{
+			ID = 5,
+			task_id = 1000356,
+			task_desc = "等级升到3级",
+			award_desc = "抽奖次数+1",
+		},
+		[6]=
+		{
+			ID = 6,
+			task_id = 1000357,
+			task_desc = "等级升到5级",
+			award_desc = "抽奖次数+1",
+		},
+		[7]=
+		{
+			ID = 7,
+			task_id = 1000358,
+			task_desc = "任意消除1次",
+			award_desc = "抽奖次数+1",
+		},
+		[8]=
+		{
+			ID = 8,
+			task_id = 1000359,
+			task_desc = "累计消除10个独眼怪",
+			award_desc = "抽奖次数+1",
+		},
+		[9]=
+		{
+			ID = 9,
+			task_id = 1000360,
+			task_desc = "累计消除20个绿皮怪",
+			award_desc = "抽奖次数+1",
+		},
+		[10]=
+		{
+			ID = 10,
+			task_id = 1000361,
+			task_desc = "累计消除50个绿皮怪",
+			award_desc = "抽奖次数+1",
+		},
+		[11]=
+		{
+			ID = 11,
+			task_id = 1000362,
+			task_desc = "累计消除30个小黄怪",
+			award_desc = "抽奖次数+1",
+		},
+		[12]=
+		{
+			ID = 12,
+			task_id = 1000363,
+			task_desc = "累计消除80个小黄怪",
+			award_desc = "抽奖次数+1",
+		},
+	},
+	cj=
+	{
+		[1]=
+		{
+			ID = 1,
+			award_desc = "15元礼包",
+			award_img = "xrxsfl_icon_15ylb",
+			obj_key = "obj_xrxsfl_15box",
+			obj_box_exchange_id = 82,
+			value = 1,
+		},
+		[2]=
+		{
+			ID = 2,
+			award_desc = "1000鱼币",
+			award_img = "com_icon_yb",
+			prop_key = "fish_coin",
+			value = 1000,
+		},
+		[3]=
+		{
+			ID = 3,
+			award_desc = "5～50元红包",
+			award_img = "xrxsfl_icon_hb",
+			obj_key = "obj_xrxsfl_5_50box",
+			obj_box_exchange_id = 86,
+			value = 1,
+		},
+		[4]=
+		{
+			ID = 4,
+			award_desc = "金币周卡",
+			award_img = "xrxsfl_jbzk_icon_jbzk",
+			obj_key = "obj_jbzk",
+			value = 1,
+		},
+		[5]=
+		{
+			ID = 5,
+			award_desc = "50元礼包",
+			award_img = "xrxsfl_icon_50ylb",
+			obj_key = "obj_xrxsfl_50box",
+			obj_box_exchange_id = 83,
+			value = 1,
+		},
+		[6]=
+		{
+			ID = 6,
+			award_desc = "锁定*3",
+			award_img = "3dby_btn_sd",
+			prop_key = "prop_3d_fish_lock",
+			value = 3,
+		},
+		[7]=
+		{
+			ID = 7,
+			award_desc = "20元话费",
+			award_img = "com_award_icon_hfsp",
+			prop_key = "prop_web_chip_huafei",
+			value = 1,
+		},
+		[8]=
+		{
+			ID = 8,
+			award_desc = "500元京东卡",
+			award_img = "activity_icon_gift123_500jdk",
+			value = 1,
+		},
+		[9]=
+		{
+			ID = 9,
+			award_desc = "10元礼包",
+			award_img = "xrxsfl_icon_10ylb",
+			obj_key = "obj_xrxsfl_10box",
+			obj_box_exchange_id = 84,
+			value = 1,
+		},
+		[10]=
+		{
+			ID = 10,
+			award_desc = "1000金币",
+			award_img = "ty_icon_jb_15y",
+			prop_key = "jing_bi",
+			value = 1000,
+		},
+		[11]=
+		{
+			ID = 11,
+			award_desc = "5～50元红包",
+			award_img = "xrxsfl_icon_hb",
+			obj_key = "obj_xrxsfl_5_50box",
+			obj_box_exchange_id = 86,
+			value = 1,
+		},
+		[12]=
+		{
+			ID = 12,
+			award_desc = "金币周卡",
+			award_img = "xrxsfl_jbzk_icon_jbzk",
+			obj_key = "obj_jbzk",
+			value = 1,
+		},
+		[13]=
+		{
+			ID = 13,
+			award_desc = "30元礼包",
+			award_img = "xrxsfl_icon_30ylb",
+			obj_key = "obj_xrxsfl_30box",
+			obj_box_exchange_id = 85,
+			value = 1,
+		},
+		[14]=
+		{
+			ID = 14,
+			award_desc = "玩具锤*1",
+			award_img = "com_award_icon_cz1",
+			prop_key = "prop_hammer_1",
+			value = 1,
+		},
+		[15]=
+		{
+			ID = 15,
+			award_desc = "20元话费",
+			award_img = "com_award_icon_hfsp",
+			prop_key = "prop_web_chip_huafei",
+			value = 1,
+		},
+		[16]=
+		{
+			ID = 16,
+			award_desc = "500元京东卡",
+			award_img = "activity_icon_gift123_500jdk",
+			value = 1,
+		},
+	},
+}
