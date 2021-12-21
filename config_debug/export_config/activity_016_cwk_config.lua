f7b0b1a76cb63b682eb1e90f09cabaf475d58e86
denglu <denglu@qq.com>
2021-12-21 14:20:43 +0800

    提交

---
+++ config_debug/export_config/activity_016_cwk_config.lua
@@ -0,0 +1,409 @@
+return {
+	permissions=
+	{
+		[1]=
+		{
+			line = 1,
+			condi_key = "cpl_notcjj",
+			task_info = "task_info",
+			help_info = "help_info",
+		},
+		[2]=
+		{
+			line = 2,
+			condi_key = "cpl_cjj",
+			task_info = "task_info_cjj",
+			help_info = "help_info_cjj",
+		},
+	},
+	task_info=
+	{
+		[1]=
+		{
+			Index = 1,
+			task_id = 21322,
+			text = "在财神消消乐中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "csxxl",
+		},
+		[2]=
+		{
+			Index = 2,
+			task_id = 21323,
+			text = "在水浒消消乐中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "shxxl",
+		},
+		[3]=
+		{
+			Index = 3,
+			task_id = 21324,
+			text = "在萌宠消消乐中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "xxl",
+		},
+		[4]=
+		{
+			Index = 4,
+			task_id = 21325,
+			text = "在苹果大战中累计纯赢1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "zpg",
+		},
+		[5]=
+		{
+			Index = 5,
+			task_id = 21326,
+			text = "在街机打鱼中累计消耗1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "by",
+		},
+		[6]=
+		{
+			Index = 6,
+			task_id = 21327,
+			text = "在疯狂捕鱼中累计消耗1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "fkby",
+		},
+		[7]=
+		{
+			Index = 7,
+			task_id = 21328,
+			text = "在弹弹乐中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "ttl",
+		},
+		[8]=
+		{
+			Index = 8,
+			task_id = 21329,
+			text = "在敲敲乐中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "qql",
+		},
+		[9]=
+		{
+			Index = 9,
+			task_id = 21330,
+			text = "在敲敲乐财神模式中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "qql_cs",
+		},
+		[10]=
+		{
+			Index = 10,
+			task_id = 21331,
+			text = "苹果大战中累计纯赢8000万",
+			total = 80000000,
+			award = 4,
+			game_type = "zpg",
+		},
+		[11]=
+		{
+			Index = 11,
+			task_id = 21332,
+			text = "苹果大战使用500万及以上档次，种出1次金苹果",
+			total = 1,
+			award = 4,
+			game_type = "zpg",
+		},
+		[12]=
+		{
+			Index = 12,
+			task_id = 21333,
+			text = "街机打鱼使用10万以上炮倍击杀一条黄金龙",
+			total = 1,
+			award = 4,
+			game_type = "by",
+		},
+		[13]=
+		{
+			Index = 13,
+			task_id = 21334,
+			text = "敲敲乐财神模式中累计赢金8000万",
+			total = 80000000,
+			award = 4,
+			game_type = "qql_cs",
+		},
+		[14]=
+		{
+			Index = 14,
+			task_id = 21335,
+			text = "弹弹乐中累计赢金8000万",
+			total = 80000000,
+			award = 4,
+			game_type = "ttl",
+		},
+		[15]=
+		{
+			Index = 15,
+			task_id = 21336,
+			text = "萌宠消消乐使用48万及以上档次，出现1次幸运时刻",
+			total = 1,
+			award = 4,
+			game_type = "xxl",
+		},
+		[16]=
+		{
+			Index = 16,
+			task_id = 21337,
+			text = "水浒消消乐中累计赢金8000万",
+			total = 80000000,
+			award = 4,
+			game_type = "shxxl",
+		},
+		[17]=
+		{
+			Index = 17,
+			task_id = 21338,
+			text = "财神消消乐使用96万以上档次，打出1次天女散花",
+			total = 1,
+			award = 4,
+			game_type = "csxxl",
+		},
+		[18]=
+		{
+			Index = 18,
+			task_id = 21339,
+			text = "敲敲乐使用10万及以上档次，敲出2次38倍财神",
+			total = 2,
+			award = 4,
+			game_type = "qql",
+		},
+	},
+	task_info_cjj=
+	{
+		[1]=
+		{
+			Index = 1,
+			task_id = 21322,
+			text = "在财神消消乐中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "csxxl",
+		},
+		[2]=
+		{
+			Index = 2,
+			task_id = 21323,
+			text = "在水浒消消乐中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "shxxl",
+		},
+		[3]=
+		{
+			Index = 3,
+			task_id = 21324,
+			text = "在萌宠消消乐中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "xxl",
+		},
+		[4]=
+		{
+			Index = 4,
+			task_id = 21325,
+			text = "在苹果大战中累计纯赢1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "zpg",
+		},
+		[5]=
+		{
+			Index = 5,
+			task_id = 1000117,
+			text = "在龙王争霸中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "lwzb",
+		},
+		[6]=
+		{
+			Index = 6,
+			task_id = 21327,
+			text = "在疯狂捕鱼中累计消耗1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "fkby",
+		},
+		[7]=
+		{
+			Index = 7,
+			task_id = 21328,
+			text = "在弹弹乐中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "ttl",
+		},
+		[8]=
+		{
+			Index = 8,
+			task_id = 21329,
+			text = "在敲敲乐中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "qql",
+		},
+		[9]=
+		{
+			Index = 9,
+			task_id = 21330,
+			text = "在敲敲乐财神模式中累计赢金1000万",
+			total = 10000000,
+			award = 1,
+			game_type = "qql_cs",
+		},
+		[10]=
+		{
+			Index = 10,
+			task_id = 21331,
+			text = "苹果大战中累计纯赢8000万",
+			total = 80000000,
+			award = 4,
+			game_type = "zpg",
+		},
+		[11]=
+		{
+			Index = 11,
+			task_id = 21332,
+			text = "苹果大战使用500万及以上档次，种出1次金苹果",
+			total = 1,
+			award = 4,
+			game_type = "zpg",
+		},
+		[12]=
+		{
+			Index = 12,
+			task_id = 1000118,
+			text = "在龙王争霸中累计赢金8000万",
+			total = 80000000,
+			award = 4,
+			game_type = "lwzb",
+		},
+		[13]=
+		{
+			Index = 13,
+			task_id = 21334,
+			text = "敲敲乐财神模式中累计赢金8000万",
+			total = 80000000,
+			award = 4,
+			game_type = "qql_cs",
+		},
+		[14]=
+		{
+			Index = 14,
+			task_id = 21335,
+			text = "弹弹乐中累计赢金8000万",
+			total = 80000000,
+			award = 4,
+			game_type = "ttl",
+		},
+		[15]=
+		{
+			Index = 15,
+			task_id = 21336,
+			text = "萌宠消消乐使用48万及以上档次，出现1次幸运时刻",
+			total = 1,
+			award = 4,
+			game_type = "xxl",
+		},
+		[16]=
+		{
+			Index = 16,
+			task_id = 21337,
+			text = "水浒消消乐中累计赢金8000万",
+			total = 80000000,
+			award = 4,
+			game_type = "shxxl",
+		},
+		[17]=
+		{
+			Index = 17,
+			task_id = 21338,
+			text = "财神消消乐使用96万以上档次，打出1次天女散花",
+			total = 1,
+			award = 4,
+			game_type = "csxxl",
+		},
+		[18]=
+		{
+			Index = 18,
+			task_id = 21339,
+			text = "敲敲乐使用10万及以上档次，敲出2次38倍财神",
+			total = 2,
+			award = 4,
+			game_type = "qql",
+		},
+	},
+	help_info=
+	{
+		[1]=
+		{
+			id = 1,
+			text = "1.畅享卡购买后立得500万金币，剩余20天每日登陆可领40万金币。",
+		},
+		[2]=
+		{
+			id = 2,
+			text = "2.每日完成2个任务共领400福利券，每个任务每日限领1次，共领20次。",
+		},
+		[3]=
+		{
+			id = 3,
+			text = "3.任务刷新首次免费，之后每次需30000金币，次数不限。",
+		},
+		[4]=
+		{
+			id = 4,
+			text = "4.当日未完成的任务次日清零，刷新重置。",
+		},
+		[5]=
+		{
+			id = 5,
+			text = "5.任务需在购买30日内完成，时间截止后任务失效。",
+		},
+		[6]=
+		{
+			id = 6,
+		},
+	},
+	help_info_cjj=
+	{
+		[1]=
+		{
+			id = 1,
+			text = "1.畅享卡购买后立得500万金币，剩余20天每日登陆可领40万金币。",
+		},
+		[2]=
+		{
+			id = 2,
+			text = "2.每日完成2个任务共领400福利券，每个任务每日限领1次，共领20次。",
+		},
+		[3]=
+		{
+			id = 3,
+			text = "3.任务刷新首次免费，之后每次需30000金币，次数不限。",
+		},
+		[4]=
+		{
+			id = 4,
+			text = "4.当日未完成的任务次日清零，刷新重置。",
+		},
+		[5]=
+		{
+			id = 5,
+			text = "5.任务需在购买30日内完成，时间截止后任务失效。",
+		},
+	},
+}
