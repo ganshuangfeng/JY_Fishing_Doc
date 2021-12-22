b5f8ab132c7641f7a31ea13a3ee1230ce8d08f8b
denglu <denglu@qq.com>
2021-12-22 09:53:06 +0800

    捕鱼能手

---
+++ config_1.4/export_config/act_054_byns_config.lua
@@ -0,0 +1,105 @@
+return {
+	otherInfo=
+	{
+		[1]=
+		{
+			line = 1,
+			task_id = 1000665,
+			startTime = 1641254400,
+			endTime = 1641830399,
+			gotoui = "game_Fishing3DHall",
+			downDes = "每日0点重置积分和奖励\n10000及以上炮倍捕鱼可获得积分，获得积分数量=鱼的倍数",
+		},
+	},
+	awardInfo=
+	{
+		[1]=
+		{
+			index = 1,
+			need_num = 100,
+			award_name = "福利券",
+			award_amount = 5,
+			award_icon = "ty_icon_flq2",
+		},
+		[2]=
+		{
+			index = 2,
+			need_num = 500,
+			award_name = "金币",
+			award_amount = 18000,
+			award_icon = "pay_icon_gold3",
+		},
+		[3]=
+		{
+			index = 3,
+			need_num = 1000,
+			award_name = "福利券",
+			award_amount = 25,
+			award_icon = "ty_icon_flq2",
+		},
+		[4]=
+		{
+			index = 4,
+			need_num = 2000,
+			award_name = "鱼币",
+			award_amount = 50000,
+			award_icon = "com_icon_yb",
+		},
+		[5]=
+		{
+			index = 5,
+			need_num = 5000,
+			award_name = "金币",
+			award_amount = "10万~30万",
+			award_icon = "pay_icon_gold4",
+		},
+		[6]=
+		{
+			index = 6,
+			need_num = 10000,
+			award_name = "金币",
+			award_amount = "20万~50万",
+			award_icon = "pay_icon_gold5",
+		},
+		[7]=
+		{
+			index = 7,
+			need_num = 30000,
+			award_name = "福利券",
+			award_amount = "800~1500",
+			award_icon = "ty_icon_flq3",
+		},
+		[8]=
+		{
+			index = 8,
+			need_num = 80000,
+			award_name = "鱼币",
+			award_amount = "200万~500万",
+			award_icon = "com_award_icon_yb1",
+		},
+		[9]=
+		{
+			index = 9,
+			need_num = 150000,
+			award_name = "金币",
+			award_amount = "300万~800万",
+			award_icon = "pay_icon_gold6",
+		},
+		[10]=
+		{
+			index = 10,
+			need_num = 300000,
+			award_name = "福利券",
+			award_amount = "6000~12000",
+			award_icon = "ty_icon_flq4",
+		},
+		[11]=
+		{
+			index = 11,
+			need_num = 500000,
+			award_name = "福利券",
+			award_amount = "8000~20000",
+			award_icon = "ty_icon_flq5",
+		},
+	},
+}
