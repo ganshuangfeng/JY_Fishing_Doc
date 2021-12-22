0254873f214138e9ca27db990ee24eb8b588af35
denglu <denglu@qq.com>
2021-12-22 10:00:04 +0800

    欢乐敲金蛋

---
+++ config_1.4/export_config/act_ty_hlqjd_config.lua
@@ -0,0 +1,174 @@
+return {
+	config_info=
+	{
+		[1]=
+		{
+			line = 1,
+			condiy_key = "cpl_notcjj",
+			beginTime = 1641254400,
+			endTime = 1641830399,
+			config = {1,2,3},
+		},
+		[2]=
+		{
+			line = 2,
+			condiy_key = "cpl_cjj",
+			beginTime = 1641254400,
+			endTime = 1641830399,
+			config = {1,2,3},
+		},
+	},
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			condi_key = "actp_buy_gift_bag_class_hlqjd_029_hlqjd_v1",
+			task_id = {1000678,1000679,1000680},
+			shop_ids = {10578,10579,10580},
+			egg_award_ids = {1,2,3},
+			item = {"prop_brass_hammer_1","prop_silver_hammer_1","prop_gold_hammer_1"},
+		},
+		[2]=
+		{
+			line = 2,
+			condi_key = "actp_buy_gift_bag_class_hlqjd_029_hlqjd_v4",
+			task_id = {1000678,1000679,1000680},
+			shop_ids = {10581,10582,10583},
+			egg_award_ids = {4,5,6},
+			item = {"prop_brass_hammer_2","prop_silver_hammer_2","prop_gold_hammer_2"},
+		},
+		[3]=
+		{
+			line = 3,
+			condi_key = "actp_buy_gift_bag_class_hlqjd_029_hlqjd_v8",
+			task_id = {1000678,1000679,1000680},
+			shop_ids = {10584,10585,10586},
+			egg_award_ids = {7,8,9},
+			item = {"prop_brass_hammer_3","prop_silver_hammer_3","prop_gold_hammer_3"},
+		},
+	},
+	shop=
+	{
+		[1]=
+		{
+			line = 1,
+			shop_id = 10578,
+			icon = {"pay_icon_gold4","com_award_icon_cz4"},
+		},
+		[2]=
+		{
+			line = 2,
+			shop_id = 10579,
+			icon = {"pay_icon_gold3","com_award_icon_cz31"},
+		},
+		[3]=
+		{
+			line = 3,
+			shop_id = 10580,
+			icon = {"pay_icon_gold2","com_award_icon_cz2"},
+		},
+		[4]=
+		{
+			line = 4,
+			shop_id = 10581,
+			icon = {"pay_icon_gold4","com_award_icon_cz4"},
+		},
+		[5]=
+		{
+			line = 5,
+			shop_id = 10582,
+			icon = {"pay_icon_gold3","com_award_icon_cz31"},
+		},
+		[6]=
+		{
+			line = 6,
+			shop_id = 10583,
+			icon = {"pay_icon_gold2","com_award_icon_cz2"},
+		},
+		[7]=
+		{
+			line = 7,
+			shop_id = 10584,
+			icon = {"pay_icon_gold4","com_award_icon_cz4"},
+		},
+		[8]=
+		{
+			line = 8,
+			shop_id = 10585,
+			icon = {"pay_icon_gold3","com_award_icon_cz31"},
+		},
+		[9]=
+		{
+			line = 9,
+			shop_id = 10586,
+			icon = {"pay_icon_gold2","com_award_icon_cz2"},
+		},
+	},
+	egg_award=
+	{
+		[1]=
+		{
+			line = 1,
+			id = 1,
+			text = {"1","8万-50万","18万-100万"},
+			image = {"ty_icon_flq2","pay_icon_gold2","pay_icon_gold3"},
+		},
+		[2]=
+		{
+			line = 2,
+			id = 2,
+			text = {"1~5","10万-100万","25万-200万"},
+			image = {"ty_icon_flq2","pay_icon_gold3","pay_icon_gold4"},
+		},
+		[3]=
+		{
+			line = 3,
+			id = 3,
+			text = {"2~10","15万-150万","50万-300万"},
+			image = {"ty_icon_flq2","pay_icon_gold3","pay_icon_gold5"},
+		},
+		[4]=
+		{
+			line = 4,
+			id = 4,
+			text = {"2~10","15万-150万","50万-300万"},
+			image = {"ty_icon_flq2","pay_icon_gold4","pay_icon_gold5"},
+		},
+		[5]=
+		{
+			line = 5,
+			id = 5,
+			text = {"4~20","30万-200万","100万-500万"},
+			image = {"ty_icon_flq2","pay_icon_gold5","pay_icon_gold6"},
+		},
+		[6]=
+		{
+			line = 6,
+			id = 6,
+			text = {"5~30","60万-500万","180万-1000万"},
+			image = {"ty_icon_flq2","pay_icon_gold6","pay_icon_gold7"},
+		},
+		[7]=
+		{
+			line = 7,
+			id = 7,
+			text = {"4~20","30万-200万","100万-500万"},
+			image = {"ty_icon_flq2","pay_icon_gold5","pay_icon_gold6"},
+		},
+		[8]=
+		{
+			line = 8,
+			id = 8,
+			text = {"5~30","60万-500万","180万-1000万"},
+			image = {"ty_icon_flq2","pay_icon_gold6","pay_icon_gold7"},
+		},
+		[9]=
+		{
+			line = 9,
+			id = 9,
+			text = {"8~50","120万-1000万","500万-3000万"},
+			image = {"ty_icon_flq2","pay_icon_gold7","pay_icon_gold8"},
+		},
+	},
+}
