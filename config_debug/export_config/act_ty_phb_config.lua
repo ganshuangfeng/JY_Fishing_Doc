149251471f5cf21f74880c1fe7cc0d6d31b4bab8
denglu <denglu@qq.com>
2022-01-04 17:48:44 +0800

    提交

---
+++ config_debug/export_config/act_ty_phb_config.lua
@@ -0,0 +1,164 @@
+return {
+	config=
+	{
+		[1]=
+		{
+			line = 1,
+			is_on_off = 1,
+			ID = 1,
+			item_key = "prop_fish_drop_act_0",
+			act_name = "欢乐达人",
+			reward_item_key = "shop_gold_sum",
+			ext_reward_item_key = "shop_gold_sum",
+			rank_type = "ymshf_017_hldr_rank",
+			s_time = 1641859200,
+			e_time = 1643039999,
+			gotoUI = {"game_Fishing3DHall",},
+			award = {1,2,3,4,5,6,7},
+			extra_award = {8,9,10,11,12,13,14},
+			path = "act_036_dlphb",
+			condiy_key = "actp_own_task_p_drop_act_nor",
+			type_info = 100,
+			is_have_point = 1,
+			act_gift_name = "话费礼包",
+			act_gift_key = "gift_hflb",
+		},
+		[2]=
+		{
+			line = 2,
+			is_on_off = 1,
+			ID = 2,
+			item_key = "prop_fish_drop_act_0",
+			act_name = "欢乐达人",
+			reward_item_key = "shop_gold_sum",
+			ext_reward_item_key = "shop_gold_sum",
+			rank_type = "ymshf_017_hldr_rank",
+			s_time = 1641859200,
+			e_time = 1643039999,
+			gotoUI = {"game_MiniGame",},
+			award = {1,2,3,4,5,6,7},
+			extra_award = {8,9,10,11,12,13,14},
+			path = "act_036_dlphb",
+			condiy_key = "cpl_cjj",
+			type_info = 100,
+			is_have_point = 1,
+			act_gift_name = "话费礼包",
+			act_gift_key = "gift_hflb",
+		},
+	},
+	award_config=
+	{
+		[1]=
+		{
+			ID = 1,
+			award = 40000,
+			limit = {1,1,},
+		},
+		[2]=
+		{
+			ID = 2,
+			award = 15000,
+			limit = {2,2},
+		},
+		[3]=
+		{
+			ID = 3,
+			award = 5000,
+			limit = {3,3,},
+		},
+		[4]=
+		{
+			ID = 4,
+			award = 2200,
+			limit = {4,6,},
+		},
+		[5]=
+		{
+			ID = 5,
+			award = 1200,
+			limit = {7,10,},
+		},
+		[6]=
+		{
+			ID = 6,
+			award = 600,
+			limit = {11,15,},
+		},
+		[7]=
+		{
+			ID = 7,
+			award = 300,
+			limit = {16,20,},
+		},
+		[8]=
+		{
+			ID = 8,
+			award = {60000,},
+			award_img = {"ty_icon_flq2",},
+			award_txt = {"60000福利券",},
+			limit = {1,1,},
+			need_num = 2000000,
+			condition = "欢乐券达到200万",
+		},
+		[9]=
+		{
+			ID = 9,
+			award = {15000,},
+			award_img = {"ty_icon_flq2",},
+			award_txt = {"15000福利券",},
+			limit = {2,2},
+			need_num = 1200000,
+			condition = "欢乐券达到120万",
+		},
+		[10]=
+		{
+			ID = 10,
+			award = {5000,},
+			award_img = {"ty_icon_flq2",},
+			award_txt = {"5000福利券",},
+			limit = {3,3,},
+			need_num = 600000,
+			condition = "欢乐券达到60万",
+		},
+		[11]=
+		{
+			ID = 11,
+			award = {800,},
+			award_img = {"ty_icon_flq2",},
+			award_txt = {"800福利券",},
+			limit = {4,6,},
+			need_num = 300000,
+			condition = "欢乐券达到30万",
+		},
+		[12]=
+		{
+			ID = 12,
+			award = {800,},
+			award_img = {"ty_icon_flq2",},
+			award_txt = {"800福利券",},
+			limit = {7,10,},
+			need_num = 200000,
+			condition = "欢乐券达到20万",
+		},
+		[13]=
+		{
+			ID = 13,
+			award = {400,},
+			award_img = {"ty_icon_flq2",},
+			award_txt = {"400福利券",},
+			limit = {11,15,},
+			need_num = 120000,
+			condition = "欢乐券达到12万",
+		},
+		[14]=
+		{
+			ID = 14,
+			award = {200,},
+			award_img = {"ty_icon_flq2",},
+			award_txt = {"200福利券",},
+			limit = {16,20,},
+			need_num = 60000,
+			condition = "欢乐券达到6万",
+		},
+	},
+}
