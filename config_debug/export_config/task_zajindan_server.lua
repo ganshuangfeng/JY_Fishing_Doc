207385304659fdced7079f656d8523a5fd033122
denglu <denglu@qq.com>
2022-01-05 10:46:12 +0800

    提交

--- config_debug/export_config/task_zajindan_server.lua
+++ config_debug/export_config/task_zajindan_server.lua
@@ -28996,7 +28996,7 @@ return {
 			id = 1205,
 			process_id = 22153,
 			source_id = 611,
-			process = {1,2,2,5},
+			process = {3,6,6,15},
 			awards = {1679,1675,1676,1677},
 			get_award_type = "nor",
 		},
