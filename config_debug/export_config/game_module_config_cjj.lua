1d6d603082166560337ab303c575896d60aa3337
denglu <denglu@qq.com>
2022-01-17 15:40:56 +0800

    提交

--- config_debug/export_config/game_module_config_cjj.lua
+++ config_debug/export_config/game_module_config_cjj.lua
@@ -2495,9 +2495,9 @@ return {
 			key = "act_ty_jrth",
 			desc = "礼包购买+自动抽奖",
 			lua = "Act_Ty_JRTHManager",
-			is_on_off = 1,
-			enable = 1,
-			state = 1,
+			is_on_off = 0,
+			enable = 0,
+			state = 0,
 		},
 		[256]=
 		{
